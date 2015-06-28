a50fbcedcb36d3799901f42733de50f96fc03213
Che-Liang Chiou <clchiou@gmail.com>
2015-06-28 10:39:28 -0700

    Add integer interval

---
+++ garage/interval.py
@@ -0,0 +1,143 @@
+__all__ = [
+    'POS_INF',
+    'NEG_INF',
+
+    'BoundType',
+    'IntegerInterval',
+
+    'parse',
+]
+
+import re
+from enum import Enum
+
+
+POS_INF = float('+inf')
+NEG_INF = float('-inf')
+
+
+class BoundType(Enum):
+    OPEN = 'OPEN'
+    CLOSED = 'CLOSED'
+
+    def __invert__(self):
+        if self is BoundType.OPEN:
+            return BoundType.CLOSED
+        else:
+            return BoundType.OPEN
+
+
+class IntervalMixin:
+
+    def __repr__(self):
+        return 'Interval<%s>' % str(self)
+
+    def __and__(self, other):
+        return JointInterval(all, [self, other])
+
+    def __or__(self, other):
+        return JointInterval(any, [self, other])
+
+    def __xor__(self, other):
+        return (self & ~other) | (~self & other)
+
+
+class IntegerInterval(IntervalMixin):
+
+    def __init__(self, left, left_type, right, right_type):
+        assert left <= right
+        assert isinstance(left_type, BoundType)
+        assert isinstance(right_type, BoundType)
+        self.left = left
+        self.left_type = left_type
+        self.right = right
+        self.right_type = right_type
+
+    def __str__(self):
+        return ('%s%s, %s%s' % (
+            '(' if self.left_type is BoundType.OPEN else '[',
+            self.left,
+            self.right,
+            ')' if self.right_type is BoundType.OPEN else ']',
+        ))
+
+    def __bool__(self):
+        if self.left == self.right:
+            return (self.left_type is BoundType.CLOSED and
+                    self.right_type is BoundType.CLOSED)
+        elif self.left + 1 == self.right:
+            return (self.left_type is BoundType.CLOSED or
+                    self.right_type is BoundType.CLOSED)
+        else:
+            return True
+
+    def __contains__(self, item):
+        if not self:
+            return False
+        elif self.left < item < self.right:
+            return True
+        elif item == self.left:
+            return self.left_type is BoundType.CLOSED
+        elif item == self.right:
+            return self.right_type is BoundType.CLOSED
+        else:
+            return False
+
+    def __invert__(self):
+        return (
+            IntegerInterval(
+                NEG_INF, BoundType.CLOSED, self.left, ~self.left_type) |
+            IntegerInterval(
+                self.right, ~self.right_type, POS_INF, BoundType.CLOSED))
+
+
+class JointInterval(IntervalMixin):
+
+    def __init__(self, join, intervals):
+        assert join in (all, any)
+        self.join = join
+        self.intervals = intervals
+
+    def __str__(self):
+        join_str = ' | ' if self.join is any else ' & '
+        return join_str.join(map(str, self.intervals))
+
+    def __bool__(self):
+        return self.join(bool(interval) for interval in self.intervals)
+
+    def __contains__(self, item):
+        return self.join(item in interval for interval in self.intervals)
+
+    def __invert__(self):
+        join = all if self.join is any else any
+        return JointInterval(join, [~interval for interval in self.intervals])
+
+
+PATTERN_INTERVAL = re.compile(r'(\d*)-(\d*)')
+PATTERN_NUMBER = re.compile(r'\d+')
+
+
+def parse(interval_specs):
+    return JointInterval(any, list(map(_parse, interval_specs.split(','))))
+
+
+def _parse(interval_spec):
+    match = PATTERN_INTERVAL.fullmatch(interval_spec)
+    if match:
+        if match.group(1):
+            left = int(match.group(1))
+        else:
+            left = NEG_INF
+        if match.group(2):
+            right = int(match.group(2))
+        else:
+            right = POS_INF
+        return IntegerInterval(left, BoundType.CLOSED, right, BoundType.CLOSED)
+
+    match = PATTERN_NUMBER.fullmatch(interval_spec)
+    if match:
+        point = int(interval_spec)
+        return IntegerInterval(
+            point, BoundType.CLOSED, point, BoundType.CLOSED)
+
+    raise SyntaxError('Cannot parse %r' % interval_spec)
---
+++ tests/test_interval.py
@@ -0,0 +1,156 @@
+import unittest
+
+from garage.interval import POS_INF
+from garage.interval import NEG_INF
+from garage.interval import BoundType
+from garage.interval import IntegerInterval
+from garage.interval import parse
+
+
+class TestExtremeValue(unittest.TestCase):
+
+    def test_extreme_value(self):
+        self.assertLess(NEG_INF, POS_INF)
+        self.assertLess(NEG_INF, -1)
+        self.assertLess(NEG_INF, 0)
+        self.assertLess(NEG_INF, +1)
+        self.assertLess(-1, POS_INF)
+        self.assertLess(0, POS_INF)
+        self.assertLess(+1, POS_INF)
+
+
+class TestBoundType(unittest.TestCase):
+
+    def test_bound_type(self):
+        self.assertIs(BoundType.OPEN, ~BoundType.CLOSED)
+        self.assertIs(BoundType.CLOSED, ~BoundType.OPEN)
+
+
+class TestInterval(unittest.TestCase):
+
+    def test_empty_interval(self):
+        for interval in (
+                IntegerInterval(0, BoundType.CLOSED, 0, BoundType.OPEN),
+                IntegerInterval(0, BoundType.OPEN, 0, BoundType.CLOSED),
+                IntegerInterval(0, BoundType.OPEN, 0, BoundType.OPEN)):
+            self.assertFalse(interval)
+            for i in range(-3, 4):
+                self.assertNotIn(i, interval)
+
+    def test_unit_interval(self):
+        interval = IntegerInterval(0, BoundType.CLOSED, 0, BoundType.CLOSED)
+        self.assertTrue(interval)
+        self.assertIn(0, interval)
+        for i in range(1, 4):
+            self.assertNotIn(+i, interval)
+            self.assertNotIn(-i, interval)
+
+    def test_integer_interval(self):
+        interval = IntegerInterval(0, BoundType.CLOSED, 2, BoundType.OPEN)
+        self.assertNotIn(NEG_INF, interval)
+        self.assertNotIn(-1, interval)
+        self.assertIn(0, interval)
+        self.assertIn(1, interval)
+        self.assertNotIn(2, interval)
+        self.assertNotIn(POS_INF, interval)
+
+        interval = ~interval
+        self.assertIn(NEG_INF, interval)
+        self.assertIn(-1, interval)
+        self.assertNotIn(0, interval)
+        self.assertNotIn(1, interval)
+        self.assertIn(2, interval)
+        self.assertIn(POS_INF, interval)
+
+    def test_and(self):
+        p = IntegerInterval(0, BoundType.CLOSED, 4, BoundType.CLOSED)
+        q = IntegerInterval(2, BoundType.CLOSED, 6, BoundType.CLOSED)
+        interval = p & q
+        self.assertNotIn(1, interval)
+        self.assertIn(2, interval)
+        self.assertIn(3, interval)
+        self.assertIn(4, interval)
+        self.assertNotIn(5, interval)
+
+    def test_or(self):
+        p = IntegerInterval(0, BoundType.CLOSED, 4, BoundType.CLOSED)
+        q = IntegerInterval(2, BoundType.CLOSED, 6, BoundType.CLOSED)
+        interval = p | q
+        self.assertNotIn(-1, interval)
+        for i in range(0, 7):
+            self.assertIn(i, interval)
+        self.assertNotIn(7, interval)
+
+    def test_xor(self):
+        p = IntegerInterval(0, BoundType.CLOSED, 4, BoundType.CLOSED)
+        q = IntegerInterval(2, BoundType.CLOSED, 6, BoundType.CLOSED)
+        interval = p ^ q
+        self.assertNotIn(-1, interval)
+        self.assertIn(0, interval)
+        self.assertIn(1, interval)
+        self.assertNotIn(2, interval)
+        self.assertNotIn(3, interval)
+        self.assertNotIn(4, interval)
+        self.assertIn(5, interval)
+        self.assertIn(6, interval)
+        self.assertNotIn(7, interval)
+
+
+class TestParse(unittest.TestCase):
+
+    def test_unit_interval(self):
+        interval = parse('3')
+        self.assertNotIn(NEG_INF, interval)
+        self.assertNotIn(2, interval)
+        self.assertIn(3, interval)
+        self.assertNotIn(4, interval)
+        self.assertNotIn(POS_INF, interval)
+
+        interval = ~interval
+        self.assertIn(NEG_INF, interval)
+        self.assertIn(2, interval)
+        self.assertNotIn(3, interval)
+        self.assertIn(4, interval)
+        self.assertIn(POS_INF, interval)
+
+    def test_points(self):
+        interval = parse('1,3,5,7,9')
+        self.assertNotIn(0, interval)
+        self.assertNotIn(2, interval)
+        self.assertNotIn(4, interval)
+        self.assertNotIn(6, interval)
+        self.assertNotIn(8, interval)
+        self.assertNotIn(10, interval)
+        self.assertIn(1, interval)
+        self.assertIn(3, interval)
+        self.assertIn(5, interval)
+        self.assertIn(7, interval)
+        self.assertIn(9, interval)
+
+    def test_interval(self):
+        interval = parse('1-4')
+        self.assertNotIn(0, interval)
+        self.assertNotIn(5, interval)
+        for i in range(1, 5):
+            self.assertIn(i, interval)
+
+        interval = parse('-0')
+        self.assertIn(NEG_INF, interval)
+        self.assertIn(0, interval)
+        self.assertNotIn(1, interval)
+
+        interval = parse('0-')
+        self.assertIn(POS_INF, interval)
+        self.assertIn(0, interval)
+        self.assertNotIn(-1, interval)
+
+        interval = parse('-')
+        self.assertIn(POS_INF, interval)
+        self.assertIn(NEG_INF, interval)
+        self.assertIn(1, interval)
+        self.assertIn(0, interval)
+        self.assertIn(-1, interval)
+
+
+if __name__ == '__main__':
+    unittest.main()
