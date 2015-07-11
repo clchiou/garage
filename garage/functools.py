6ef9047e89fab523cf8fef61cde8496ff8201bad
Che-Liang Chiou <clchiou@gmail.com>
2015-07-11 14:50:44 -0700

    Add nondata_property

--- garage/functools.py
+++ garage/functools.py
@@ -1,5 +1,6 @@
 __all__ = [
     'memorize',
+    'nondata_property',
 ]
 
 
@@ -17,3 +18,26 @@ def memorize(method):
 
     wrapper.__doc__ = wrapped.__doc__
     return property(wrapper) if is_property else wrapper
+
+
+class nondata_property:
+    """A non-data descriptor version of property.
+
+       This is "non-data" means that instances may override it.
+    """
+
+    def __init__(self, fget=None, doc=None):
+        self.fget = fget
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, instance, _=None):
+        if instance is None:
+            return self
+        if self.fget is None:
+            raise AttributeError('unreadable attribute')
+        return self.fget(instance)
+
+    def getter(self, fget):
+        return type(self)(fget, self.__doc__)
--- tests/test_functools.py
+++ tests/test_functools.py
@@ -1,6 +1,7 @@
 import unittest
 
 from garage.functools import memorize
+from garage.functools import nondata_property
 
 
 class Foo:
@@ -35,6 +36,34 @@ class TestMemorize(unittest.TestCase):
         self.assertEqual(1, foo.prop1)
         self.assertEqual(2, foo.prop2)
         self.assertEqual(2, foo.prop2)
+
+
+class Bar:
+
+    @nondata_property
+    def prop1(self):
+        """Doc string."""
+        return 'prop1-value'
+
+    prop2 = nondata_property()
+
+
+class TestNondataProperty(unittest.TestCase):
+
+    def test_nondata_property(self):
+        # This should not raise.
+        self.assertTrue(Bar.prop1)
+        self.assertEqual('Doc string.', Bar.prop1.__doc__)
+        self.assertTrue(Bar.prop2)
+        self.assertIsNone(Bar.prop2.__doc__)
+
+        bar = Bar()
+        self.assertEqual('prop1-value', bar.prop1)
+        with self.assertRaises(AttributeError):
+            bar.prop2
+        # Override non-data descriptor.
+        bar.prop2 = 'hello'
+        self.assertEqual('hello', bar.prop2)
 
 
 if __name__ == '__main__':
