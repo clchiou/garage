8d5e132b4107c8e5eaba56e50d74563c74606ba5
Che-Liang Chiou <clchiou@gmail.com>
2015-07-25 15:44:13 -0700

    Rename to executors

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -1,21 +1,21 @@
 import unittest
 
-import garage.executor
+import garage.executors
 
 
 class TestExecutor(unittest.TestCase):
 
     def test_executor(self):
-        pool = garage.executor.WorkerPool()
+        pool = garage.executors.WorkerPool()
         self.assertEqual(0, len(pool))
 
         # No jobs, no workers are hired.
-        with garage.executor.Executor(pool, 1) as executor:
+        with garage.executors.Executor(pool, 1) as executor:
             self.assertEqual(0, len(pool))
 
         self.assertEqual(0, len(pool))
 
-        with garage.executor.Executor(pool, 1) as executor:
+        with garage.executors.Executor(pool, 1) as executor:
             f1 = executor.submit(sum, (1, 2, 3))
             f2 = executor.submit(sum, (4, 5, 6))
             self.assertEqual(0, len(pool))
