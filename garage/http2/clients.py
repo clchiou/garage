1235ea19aac72c86d2d2e6d16b35af849cdd28ac
Che-Liang Chiou <clchiou@gmail.com>
2015-08-13 23:16:43 -0700

    Fix kwargs arguments to requests

--- garage/http2/clients.py
+++ garage/http2/clients.py
@@ -28,13 +28,42 @@ LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
 
+_REQUEST_ARG_NAMES = frozenset(
+    'headers files data params auth cookies hooks json'.split()
+)
+
+
+_SEND_ARG_NAMES = frozenset(
+    'verify proxies stream cert timeout allow_redirects'.split()
+)
+
+
+_ALL_ARG_NAMES = _REQUEST_ARG_NAMES | _SEND_ARG_NAMES
+
+
 class HttpError(Exception):
     pass
 
 
+def _check_kwargs(kwargs, arg_names):
+    names = set(kwargs) - arg_names
+    if names:
+        raise TypeError('not expect these keyword arguments: %s' %
+                        ', '.join(sorted(names)))
+
+
 def _make_method(method):
     def http_method(self, uri, **kwargs):
-        return self.send(Request(method, uri, **kwargs))
+        _check_kwargs(kwargs, _ALL_ARG_NAMES)
+        req_kwargs = {
+            key: arg for key, arg in kwargs.items()
+            if key in _REQUEST_ARG_NAMES
+        }
+        send_kwargs = {
+            key: arg for key, arg in kwargs.items()
+            if key in _SEND_ARG_NAMES
+        }
+        return self.send(Request(method, uri, **req_kwargs), **send_kwargs)
     return http_method
 
 
@@ -62,14 +91,15 @@ class Client(_ClientMixin):
     def headers(self):
         return self._session.headers
 
-    def send(self, request):
+    def send(self, request, **kwargs):
         LOG.debug('%s %s', request.method, request.uri)
+        _check_kwargs(kwargs, _SEND_ARG_NAMES)
         rreq = request._make_request()
         retry = self._retry_policy()
         retry_count = 0
         while True:
             try:
-                return self._send(request, rreq, retry_count)
+                return self._send(request, rreq, kwargs, retry_count)
             except BaseException:
                 backoff = next(retry, None)
                 if backoff is None:
@@ -77,7 +107,7 @@ class Client(_ClientMixin):
             self._sleep(backoff)
             retry_count += 1
 
-    def _send(self, request, rreq, retry_count):
+    def _send(self, request, rreq, kwargs, retry_count):
         try:
             with self._rate_limit:
                 if retry_count:
@@ -86,7 +116,7 @@ class Client(_ClientMixin):
                 if LOG.isEnabledFor(logging.DEBUG):
                     for name, value in rreq.headers.items():
                         LOG.debug('<<< %s: %s', name, value)
-                rrep = self._session.send(rreq.prepare())
+                rrep = self._session.send(rreq.prepare(), **kwargs)
                 if LOG.isEnabledFor(logging.DEBUG):
                     for name, value in rrep.headers.items():
                         LOG.debug('>>> %s: %s', name, value)
@@ -117,9 +147,9 @@ class ForwardingClient(_ClientMixin):
     def headers(self):
         return self.client.headers
 
-    def send(self, request):
+    def send(self, request, **kwargs):
         request = self.on_request(request)
-        response = self.client.send(request)
+        response = self.client.send(request, **kwargs)
         response = self.on_response(request, response)
         return response
 
@@ -136,6 +166,7 @@ class Request:
     """A thin wrapper of requests.Request."""
 
     def __init__(self, method, uri, **kwargs):
+        _check_kwargs(kwargs, _REQUEST_ARG_NAMES)
         self.method = method
         self.uri = uri
         self.kwargs = kwargs
