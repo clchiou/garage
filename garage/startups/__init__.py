ae800d1553f4c1f3183bb35737364192c2e327a6
Che-Liang Chiou <clchiou@gmail.com>
2015-09-03 09:37:51 -0700

    Factor out startups.logging but maintain startup order

--- garage/startups/__init__.py
+++ garage/startups/__init__.py
@@ -13,15 +13,10 @@ __all__ = [
     'PARSE',
     'PARSER',
     'init',
-    'init_logging',
 ]
-
-import logging
-import threading
 
 from startup import startup
 
-import garage
 from garage.functools import run_once
 
 
@@ -29,29 +24,6 @@ ARGS = 'args'
 ARGV = 'argv'
 PARSE = 'parse'
 PARSER = 'parser'
-
-VERBOSE = __name__ + '#verbose'
-
-
-LOG_FORMAT = '%(asctime)s %(threadName)s %(levelname)s %(name)s: %(message)s'
-
-
-def add_arguments(parser: PARSER, verbose: VERBOSE) -> PARSE:
-    group = parser.add_argument_group(garage.__name__)
-    group.add_argument(
-        '-v', '--verbose', action='count', default=verbose,
-        help='verbose output')
-
-
-def configure(args: ARGS):
-    if args.verbose == 0:
-        level = logging.WARNING
-    elif args.verbose == 1:
-        level = logging.INFO
-    else:
-        level = logging.DEBUG
-    logging.basicConfig(level=level, format=LOG_FORMAT)
-    threading.current_thread().name = garage.__name__ + '#main'
 
 
 def parse_argv(parser: PARSER, argv: ARGV, _: PARSE) -> ARGS:
@@ -61,11 +33,3 @@ def parse_argv(parser: PARSER, argv: ARGV, _: PARSE) -> ARGS:
 @run_once
 def init():
     startup(parse_argv)
-
-
-@run_once
-def init_logging(verbose=0):
-    init()
-    startup.set(VERBOSE, verbose)
-    startup(add_arguments)
-    startup(configure)
---
+++ garage/startups/logging.py
@@ -0,0 +1,51 @@
+__all__ = [
+    'init',
+]
+
+import logging
+import threading
+
+from startup import startup
+
+from garage.functools import run_once
+
+import garage
+import garage.startups
+from garage.startups import ARGS, PARSE, PARSER
+
+
+VERBOSE = __name__ + '#verbose'
+
+
+LOG_FORMAT = '%(asctime)s %(threadName)s %(levelname)s %(name)s: %(message)s'
+
+
+def add_arguments(parser: PARSER, verbose: VERBOSE) -> PARSE:
+    group = parser.add_argument_group(garage.__name__)
+    group.add_argument(
+        '-v', '--verbose', action='count', default=verbose,
+        help='verbose output')
+
+
+def configure(args: ARGS):
+    if args.verbose == 0:
+        level = logging.WARNING
+    elif args.verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.DEBUG
+    logging.basicConfig(level=level, format=LOG_FORMAT)
+    threading.current_thread().name = garage.__name__ + '#main'
+
+
+@run_once
+def init(verbose=0):
+    garage.startups.init()
+
+    # XXX: Hack for manipulating startup order.
+    add_arguments.__module__ = garage.__name__
+    configure.__module__ = garage.__name__
+
+    startup.set(VERBOSE, verbose)
+    startup(add_arguments)
+    startup(configure)
