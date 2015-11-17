16f560f61080b578d61a8f03ed53d933b11fe85c
Che-Liang Chiou <clchiou@gmail.com>
2015-11-16 20:52:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'garage',
-    description = 'My personal python modules',
+    description = 'Python modules developed in the garage',
     license = 'MIT',
     packages = find_packages(),
     install_requires = [
-        'PyYAML',
-        'lxml',
-        'requests',
-        'startup',
+        # These modules are optional but nice to have.
+        #'PyYAML',
+        #'lxml',
+        #'requests',
+        #'startup',
     ],
 )
