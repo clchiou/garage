2c046e9b09a04c9916c8de8e2c52fe743d74abe6
Che-Liang Chiou <clchiou@gmail.com>
2015-12-28 22:09:10 -0800

    Add http.services.Service

--- py/garage/garage/http/services.py
+++ py/garage/garage/http/services.py
@@ -1,15 +1,124 @@
 __all__ = [
+    'ServiceError',
+    'EndpointNotFound',
+    'VersionNotSupported',
+    'Service',
     'Version',
 ]
 
 import re
 from collections import namedtuple
+from http import HTTPStatus
+
+from http2 import HttpError
+
+from garage import asserts
+
+
+class ServiceError(Exception):
+    pass
+
+
+class EndpointNotFound(ServiceError):
+    pass
+
+
+class VersionNotSupported(ServiceError):
+    pass
+
+
+class Service:
+
+    def __init__(self, version):
+        self._path = b'/'
+        self.version = version
+        self.policies = []
+        self.endpoints = {}
+        self.parse = None
+        self.serialize = None
+
+    @property
+    def path(self):
+        return self._path.decode('ascii')
+
+    @path.setter
+    def path(self, new_path):
+        self._path = new_path.encode('ascii')
+        if self._path[-1] != b'/':
+            self._path += b'/'
+
+    def add_policies(self, policy):
+        self.policies.append(policy)
+
+    def add_endpoint(self, name, endpoint):
+        name = name.encode('ascii')
+        asserts.precond(name not in self.endpoints)
+        self.endpoints[name] = endpoint
+
+    async def __call__(self, http_request, http_response):
+        try:
+            endpoint = self.dispatch(http_request)
+            await self.call_endpoint(endpoint, http_request, http_response)
+        except EndpointNotFound:
+            raise HttpError(HTTPStatus.NOT_FOUND)
+        except VersionNotSupported:
+            raise HttpError(HTTPStatus.BAD_REQUEST)
+
+    def dispatch(self, http_request):
+        path = http_request.headers.get(b':path')
+        if path is None:
+            raise EndpointNotFound(None)
+        version, name = parse_path(self._path, path)
+        if version is None:
+            raise EndpointNotFound(path)
+        endpoint = self.endpoints.get(name)
+        if endpoint is None:
+            raise EndpointNotFound(path)
+        if not self.version.is_compatible_with(version):
+            raise VersionNotSupported(version)
+        return endpoint
+
+    async def call_endpoint(self, endpoint, http_request, http_response):
+        for policy in self.policies:
+            await policy(http_request.headers)
+
+        request = await http_request.body
+        if self.parse:
+            request = await self.parse(http_request.headers, request)
+
+        response = await endpoint(request)
+        if self.serialize:
+            response = await self.serialize(http_request.headers, response)
+
+        http_response.headers[b':status'] = b'200'
+        http_response.write(response)
+        http_response.close()
+
+
+def parse_path(root, path):
+    if not path.startswith(root):
+        return None, None
+    i = path.find(b'/', len(root))
+    if i == -1:
+        return None, None
+    try:
+        version = Version.parse(path[len(root):i])
+    except ValueError:
+        return None, None
+    j = path.find(b'?', i)
+    if j == -1:
+        j = path.find(b'#', i)
+    if j == -1:
+        name = path[i+1:]
+    else:
+        name = path[i+1:j]
+    return version, name
 
 
 class Version(namedtuple('Version', 'major minor patch')):
 
     # Should I forbid leading zeros?
-    PATTERN_VERSION = re.compile(r'(\d+)\.(\d+)\.(\d+)')
+    PATTERN_VERSION = re.compile(br'(\d+)\.(\d+)\.(\d+)')
 
     @classmethod
     def parse(cls, version):
--- py/garage/tests/http/test_services.py
+++ py/garage/tests/http/test_services.py
@@ -6,24 +6,24 @@ from garage.http.services import Version
 class VersionTest(unittest.TestCase):
 
     def test_parse(self):
-        self.assertTupleEqual((0, 0, 0), Version.parse('0.0.0'))
-        self.assertTupleEqual((0, 0, 0), Version.parse('000.000.000'))
-        self.assertTupleEqual((1, 2, 3), Version.parse('001.002.003'))
+        self.assertTupleEqual((0, 0, 0), Version.parse(b'0.0.0'))
+        self.assertTupleEqual((0, 0, 0), Version.parse(b'000.000.000'))
+        self.assertTupleEqual((1, 2, 3), Version.parse(b'001.002.003'))
         with self.assertRaises(ValueError):
-            Version.parse('0.0.c')
+            Version.parse(b'0.0.c')
         with self.assertRaises(ValueError):
-            Version.parse('1.2')
+            Version.parse(b'1.2')
         with self.assertRaises(ValueError):
-            Version.parse('1.')
+            Version.parse(b'1.')
 
     def test_str(self):
         self.assertEqual('0.0.0', str(Version(0, 0, 0)))
         self.assertEqual('1.2.3', str(Version(1, 2, 3)))
 
     def test_is_compatible_with(self):
-        v0_1_0 = Version.parse('0.1.0')
-        v0_2_0 = Version.parse('0.2.0')
-        v1_0_0 = Version.parse('1.0.0')
+        v0_1_0 = Version.parse(b'0.1.0')
+        v0_2_0 = Version.parse(b'0.2.0')
+        v1_0_0 = Version.parse(b'1.0.0')
 
         self.assertTrue(v0_1_0.is_compatible_with(v0_2_0))
         self.assertTrue(v0_2_0.is_compatible_with(v0_1_0))
@@ -33,11 +33,11 @@ class VersionTest(unittest.TestCase):
 
     def test_newer(self):
         versions = [
-            Version.parse('0.1.0'),
-            Version.parse('0.1.1'),
-            Version.parse('0.2.0'),
-            Version.parse('0.10.0'),
-            Version.parse('1.0.0'),
+            Version.parse(b'0.1.0'),
+            Version.parse(b'0.1.1'),
+            Version.parse(b'0.2.0'),
+            Version.parse(b'0.10.0'),
+            Version.parse(b'1.0.0'),
         ]
         for v0, v1 in zip(versions, versions[1:]):
             self.assertGreater(v1, v0)
