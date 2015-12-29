b9fc1562c93ecc0dd5380e1ceda2c2675bff9c77
Che-Liang Chiou <clchiou@gmail.com>
2015-12-28 23:51:34 -0800

    Simplify service

--- py/garage/garage/http/services.py
+++ py/garage/garage/http/services.py
@@ -30,22 +30,11 @@ class VersionNotSupported(ServiceError):
 class Service:
 
     def __init__(self, version):
-        self._path = b'/'
         self.version = version
         self.policies = []
         self.endpoints = {}
         self.parse = None
         self.serialize = None
-
-    @property
-    def path(self):
-        return self._path.decode('ascii')
-
-    @path.setter
-    def path(self, new_path):
-        self._path = new_path.encode('ascii')
-        if self._path[-1] != b'/':
-            self._path += b'/'
 
     def add_policies(self, policy):
         self.policies.append(policy)
@@ -58,18 +47,19 @@ class Service:
     async def __call__(self, http_request, http_response):
         try:
             endpoint = self.dispatch(http_request)
-            await self.call_endpoint(endpoint, http_request, http_response)
         except EndpointNotFound:
             raise HttpError(HTTPStatus.NOT_FOUND)
         except VersionNotSupported:
             raise HttpError(HTTPStatus.BAD_REQUEST)
+        await self.call_endpoint(endpoint, http_request, http_response)
 
     def dispatch(self, http_request):
         path = http_request.headers.get(b':path')
         if path is None:
             raise EndpointNotFound(None)
-        version, name = parse_path(self._path, path)
-        if version is None:
+        try:
+            version, name = parse_path(path)
+        except ValueError:
             raise EndpointNotFound(path)
         endpoint = self.endpoints.get(name)
         if endpoint is None:
@@ -89,30 +79,23 @@ class Service:
         response = await endpoint(request)
         if self.serialize:
             response = await self.serialize(http_request.headers, response)
+        asserts.postcond(isinstance(response, bytes))
 
         http_response.headers[b':status'] = b'200'
         await http_response.write(response)
         await http_response.close()
 
 
-def parse_path(root, path):
-    if not path.startswith(root):
-        return None, None
-    i = path.find(b'/', len(root))
-    if i == -1:
-        return None, None
-    try:
-        version = Version.parse(path[len(root):i])
-    except ValueError:
-        return None, None
-    j = path.find(b'?', i)
-    if j == -1:
-        j = path.find(b'#', i)
-    if j == -1:
-        name = path[i+1:]
-    else:
-        name = path[i+1:j]
-    return version, name
+PATTERN_PATH = re.compile(br'/(\d+\.\d+\.\d+)/([a-zA-Z0-9_\-.]+)')
+
+
+def parse_path(path):
+    match = PATTERN_PATH.match(path)
+    if not match:
+        raise ValueError(path)
+    version = Version.parse(match.group(1))
+    endpoint = match.group(2)
+    return version, endpoint
 
 
 class Version(namedtuple('Version', 'major minor patch')):
