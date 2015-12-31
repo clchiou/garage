a0231521c8293f38694ec689f620e8a77c02f151
Che-Liang Chiou <clchiou@gmail.com>
2015-12-30 20:18:37 -0800

    Add TLS setup to HTTP/2 server sample

--- samples/http2/hello_world.py
+++ samples/http2/hello_world.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import socket
+import ssl
 import sys
 
 import http2
@@ -9,7 +10,7 @@ from garage.http.services import Service
 
 
 if len(sys.argv) < 2:
-    print('Usage: %s port' % sys.argv[0])
+    print('Usage: %s port [server.crt server.key]' % sys.argv[0])
     sys.exit(1)
 
 logging.basicConfig()
@@ -23,9 +24,20 @@ service.add_endpoint('hello-world', hello_world)
 
 loop = asyncio.get_event_loop()
 
+if len(sys.argv) >= 4:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context.load_cert_chain(sys.argv[2], sys.argv[3])
+    if ssl.HAS_ALPN:
+        ssl_context.set_alpn_protocols(['h2'])
+    else:
+        assert ssl.HAS_NPN
+        ssl_context.set_npn_protocols(['h2'])
+else:
+    ssl_context = None
+
 server = loop.run_until_complete(loop.create_server(
     lambda: http2.Protocol(lambda: service),
-    '127.0.0.1', int(sys.argv[1]),
+    host='127.0.0.1', port=int(sys.argv[1]), ssl=ssl_context,
 ))
 
 server.sockets[0].setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
