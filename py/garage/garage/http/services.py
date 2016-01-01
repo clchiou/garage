54f3e8aa598ee33d6016e1b6c14dd162302b7596
Che-Liang Chiou <clchiou@gmail.com>
2015-12-31 22:36:52 -0800

    Remove ServiceHub, which is not quite useful at the moment

--- py/garage/garage/http/services.py
+++ py/garage/garage/http/services.py
@@ -2,13 +2,11 @@ __all__ = [
     'ServiceError',
     'EndpointNotFound',
     'VersionNotSupported',
-    'ServiceHub',
     'Service',
 ]
 
 import logging
 import re
-from collections import namedtuple
 from http import HTTPStatus
 
 from http2 import HttpError
@@ -29,61 +27,6 @@ class EndpointNotFound(ServiceError):
 
 class VersionNotSupported(ServiceError):
     pass
-
-
-class ServiceHub:
-
-    def __init__(self):
-        self.services = {}
-
-    def add_service(self, service):
-        LOG.info('register service %s version %d',
-                 service.name, service.version)
-        name = service.name.encode('ascii')
-        services = self.services.setdefault(name, [])
-        for i in range(len(services)):
-            asserts.precond(service.version != services[i].version)
-            if service.version < services[i].version:
-                services.insert(i, service)
-                break
-        else:
-            services.append(service)
-
-    async def __call__(self, http_request, http_response):
-        path = http_request.headers.get(b':path')
-        if path is None:
-            raise HttpError(HTTPStatus.BAD_REQUEST)
-        service, endpoint = call_dispatch(self.dispatch, path)
-        await service.call_endpoint(endpoint, http_request, http_response)
-
-    PATTERN_SERVICE = re.compile(
-        br'/([a-zA-Z0-9_\-.]+)/(\d+)/([a-zA-Z0-9_\-.]+)')
-
-    def dispatch(self, path):
-        match = self.PATTERN_SERVICE.match(path)
-        if not match:
-            raise EndpointNotFound(path)
-        service_name = match.group(1)
-        version = int(match.group(2))
-        endpoint_name = match.group(3)
-
-        services = self.services.get(service_name)
-        if services is None:
-            raise EndpointNotFound(path)
-
-        for service in services:
-            if service.version < version:
-                continue
-            endpoint = service.endpoints.get(endpoint_name)
-            if endpoint is None:
-                raise EndpointNotFound(path)
-            LOG.info('dispatch %s to %s/%d/%s',
-                     path.decode('ascii'),
-                     service.name, service.version,
-                     endpoint_name.decode('ascii'))
-            return service, endpoint
-
-        raise VersionNotSupported(version)
 
 
 class Service:
@@ -111,7 +54,12 @@ class Service:
         path = http_request.headers.get(b':path')
         if path is None:
             raise HttpError(HTTPStatus.BAD_REQUEST)
-        endpoint = call_dispatch(self.dispatch, path)
+        try:
+            endpoint = self.dispatch(path)
+        except EndpointNotFound:
+            raise HttpError(HTTPStatus.NOT_FOUND)
+        except VersionNotSupported:
+            raise HttpError(HTTPStatus.BAD_REQUEST)
         await self.call_endpoint(endpoint, http_request, http_response)
 
     PATTERN_ENDPOINT = re.compile(br'/(\d+)/([a-zA-Z0-9_\-.]+)')
@@ -151,12 +99,3 @@ class Service:
         http_response.headers[b':status'] = b'200'
         await http_response.write(response)
         await http_response.close()
-
-
-def call_dispatch(dispatch, path):
-    try:
-        return dispatch(path)
-    except EndpointNotFound:
-        raise HttpError(HTTPStatus.NOT_FOUND)
-    except VersionNotSupported:
-        raise HttpError(HTTPStatus.BAD_REQUEST)
