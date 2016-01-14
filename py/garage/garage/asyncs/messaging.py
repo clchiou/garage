33a5e8e77ead81419105e757d240c9e4dfd3554d
Che-Liang Chiou <clchiou@gmail.com>
2016-01-14 04:43:48 -0800

    Add asyncs.messaging

---
+++ py/garage/garage/asyncs/messaging.py
@@ -0,0 +1,119 @@
+__all__ = [
+    'MessagingError',
+    'ServiceState',
+    'ServiceMixin',
+    'remote_call',
+    'serve_one',
+    'serve_forever',
+]
+
+import asyncio
+import enum
+import json  # TODO: Make serialization format pluggable.
+import logging
+
+from garage import asserts
+
+import nanomsg as nn
+
+
+LOG = logging.getLogger(__name__)
+
+
+class MessagingError(Exception):
+    pass
+
+
+class ServiceState(enum.Enum):
+
+    INITIALIZING = 'INITIALIZING'
+    INITIALIZED = 'INITIALIZED'
+
+    STARTING = 'STARTING'
+    STARTED = 'STARTED'
+
+    STOPPING = 'STOPPING'
+    STOPPED = 'STOPPED'
+
+
+class ServiceMixin:
+    """Non-restartable service mixin."""
+
+    def __init__(self, serve, name='?', close=None, *, loop=None):
+        self.__name = name
+        self.__state = ServiceState.INITIALIZING
+        self.__server = None
+        self.__serve = serve
+        self.__close = close
+        self.__loop = loop
+        self.__state = ServiceState.INITIALIZED
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__name, self.__state.value)
+
+    async def start(self):
+        asserts.precond(self.__state is ServiceState.INITIALIZED)
+        LOG.info('%r: start', self)
+        self.__state = ServiceState.STARTING
+        self.__server = self.__loop.create_task(self.__serve())
+        self.__state = ServiceState.STARTED
+
+    async def stop(self):
+        asserts.precond(self.__state is ServiceState.STARTED)
+        LOG.info('%r: stop', self)
+        self.__state = ServiceState.STOPPING
+        if self.__close:
+            await self.__close()
+        done, pending = await asyncio.wait(
+            [self.__server],
+            loop=self.__loop,
+            #timeout=GRACEFUL_PERIOD,  # TODO: Set graceful period.
+        )
+        for fut in done:
+            try:
+                fut.result()
+            except Exception:
+                LOG.exception('%r: err when stopping service', self)
+        if pending:
+            LOG.warning('%r: leave %d pending tasks and exit',
+                        self, len(pending))
+        self.__server = None
+        self.__state = ServiceState.STOPPED
+
+
+async def remote_call(sock, method, request):
+    message = {'method': method, 'request': request}
+    await sock.send(json.dumps(message).encode('ascii'))
+    with await sock.recv() as response:
+        return json.loads(bytes(response.as_memoryview()).decode('ascii'))
+
+
+async def serve_forever(sock, methods, name='<?>'):
+    while True:
+        try:
+            await serve_one(sock, methods)
+        except nn.Closed:
+            break
+        except MessagingError:
+            LOG.warning('%s: invalid message', name, exc_info=True)
+        except Exception:
+            LOG.exception('%s: err when processing message', name)
+    LOG.info('%s: exit', name)
+
+
+async def serve_one(sock, methods):
+    with await sock.recv() as msg:
+        message = json.loads(bytes(msg.as_memoryview()).decode('ascii'))
+
+    method_name = message.get('method')
+    request = message.get('request')
+    if not (method_name and request):
+        raise MessagingError('invalid message %r', message)
+
+    method = methods.get(method_name)
+    if not method:
+        raise MessagingError('method not found %r', method_name)
+
+    response = await method(request)
+
+    await sock.send(json.dumps(response).encode('ascii'))
