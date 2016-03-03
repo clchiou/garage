c368b2bf6c6a490dfe395fa2dc0fc23721fa416a
Che-Liang Chiou <clchiou@gmail.com>
2016-03-03 00:27:11 -0800

    Rewrite asyncs.processes

--- py/garage/garage/asyncs/processes.py
+++ py/garage/garage/asyncs/processes.py
@@ -1,144 +1,59 @@
 __all__ = [
-    'Throne',
     'process',
-    'until_closed',
 ]
 
 import asyncio
 import functools
-import logging
 
 from garage import asserts
 
-from . import queues
-from .futures import awaiting
+
+class ProcessExit(Exception):
+    pass
 
 
-LOG = logging.getLogger(__name__)
-
-
-class Throne:
-    """A throne represents a continuous service provided by processes.
-       When a process dies, a new process is (immediately) created to
-       continue the service.
-
-       The process is dead, long live the process!
-    """
-
-    def __init__(self, *, loop=None):
-        self.proc = None
-        self._throne = awaiting.replaceable(loop=loop)
-
-    async def __aenter__(self):
-        await self._throne.__aenter__()
-        return self
-
-    async def __aexit__(self, *exc_info):
-        if self.proc:
-            self.proc.inbox.close()
-        return await self._throne.__aexit__(*exc_info)
-
-    async def dethrone(self):
-        """Dethrone the current process.
-
-           This is blocking - as dethroning all kings.
-        """
-        asserts.precond(self.proc)
-        proc, self.proc = self.proc, None
-        proc.inbox.close()
-        await self._throne.remove()
-        return proc
-
-    def throne(self, proc):
-        """Make the new process come to the throne.
-
-           This is non-blocking.
-        """
-        asserts.precond(proc)
-        self._throne.set(proc.task)
-        self.proc = proc
-        return self.proc
-
-
-def process(coro_func=None, *, make_queue=None, loop=None):
+def process(coro_func=None, *, loop=None):
     """Decorator to mark processes."""
     if coro_func is None:
-        return functools.partial(process, make_queue=make_queue, loop=loop)
+        return functools.partial(process, loop=loop)
     # NOTE: A coroutine object is not a coroutine function!
     asserts.precond(not asyncio.iscoroutine(coro_func))
-    return ProcessFactory(coro_func, make_queue, loop=loop)
-
-
-class ProcessFactory:
-
-    def __init__(self, coro_func, make_queue, *, loop=None):
-        self.coro_func = coro_func
-        self.closed_as_normal_exit = True
-        self.make_queue = make_queue
-        self.loop = loop
-
-    def __call__(self, *args, **kwargs):
-        return self.make(args, kwargs)
-
-    def make(self, args, kwargs, **options):
-        for opt in ('closed_as_normal_exit', 'make_queue', 'loop'):
-            options.setdefault(opt, getattr(self, opt))
-        return Process(self.coro_func, args, kwargs, **options)
+    return Process(coro_func, loop=loop)
 
 
 class Process:
-    """A process is just a asyncio.Task with an inbox queue.
-
-       The task is intended to be the only consumer of the inbox and so
-       when the task is done, the inbox queue is closed.  If you need
-       something like a shared inbox, you should create it separately.
+    """A process is an asyncio.Task plus a `stop` future object, and
+       I find this much more easier to work with than Task.cancel().
     """
 
-    def __init__(self, coro_func, args, kwargs, *,
-                 closed_as_normal_exit=True,
-                 make_queue=None,
-                 loop=None):
+    def __init__(self, coro_func, *, loop=None):
+        self.coro_func = coro_func
         self.loop = loop
-        inbox = make_queue() if make_queue else queues.Queue(loop=self.loop)
-        # Schedule the task while avoiding circular reference - don't
-        # hold `self` or `self.inbox` but just plain `inbox`.
-        self.task = asyncio.ensure_future(
-            coro_func(inbox, *args, **kwargs), loop=self.loop)
-        self.task.add_done_callback(lambda _: inbox.close(graceful=False))
-        if closed_as_normal_exit:
-            self.task.add_done_callback(_silence_closed)
-        self.inbox = inbox
 
-    def stop(self, graceful=True):
-        self.inbox.close(graceful=graceful)
-
-    def __await__(self):
-        return self.task.__await__()
-
-    async def __aenter__(self):
-        self._context_manager = awaiting(self.task, loop=self.loop)
-        await self._context_manager.__aenter__()
-        return self
-
-    async def __aexit__(self, *exc_info):
-        self.inbox.close()
-        return await self._context_manager.__aexit__(*exc_info)
+    def __call__(self, *args, **kwargs):
+        stop_flag = asyncio.Event(loop=self.loop)
+        exit_fut = asyncio.ensure_future(exit(stop_flag), loop=self.loop)
+        task = asyncio.ensure_future(
+            self.coro_func(exit_fut, *args, **kwargs),
+            loop=self.loop,
+        )
+        task.add_done_callback(_silence_process_exit)
+        task.add_done_callback(lambda fut: exit_fut.cancel())
+        task.stop = stop_flag.set  # Monkey patch task.
+        return task
 
 
-def until_closed(inbox, *, closed_as_normal_exit=True, loop=None):
-    """Wrap inbox.until_closed() in an awaiting context manager."""
-    cxtmgr = awaiting(inbox.until_closed(), cancel_on_exit=True, loop=loop)
-    if closed_as_normal_exit:
-        cxtmgr.silence_exc_type = queues.Closed
-    return cxtmgr
+async def exit(stop_flag):
+    await stop_flag.wait()
+    raise ProcessExit
 
 
-def _silence_closed(future):
+def _silence_process_exit(future):
     #
-    # Hack for silencing queues.Closed of a finished future!
+    # Hack for silencing ProcessExit of a finished future!
     #
     # You could alternatively use a wrapper coroutine to silence the
-    # queues.Closed exception, but then `repr(future)` would show the
+    # ProcessExit exception, but then `repr(future)` would show the
     # location of the wrapper rather than the original coroutine, which
     # is quite inconvenient when debugging.
     #
@@ -151,7 +66,7 @@ def _silence_closed(future):
     asserts.precond(hasattr(future, '_exception'))
 
     # 1. Don't alter states of futures that we don't want to silence.
-    if not isinstance(future._exception, queues.Closed):
+    if not isinstance(future._exception, ProcessExit):
         return
 
     # 2. Call future.exception() to silence traceback logger.
--- py/garage/tests/asyncs/test_processes.py
+++ py/garage/tests/asyncs/test_processes.py
@@ -12,56 +12,32 @@ class MyException(Exception):
 
 
 @process
-async def recv_messages(inbox, flag, messages):
-    await flag.wait()
-    while True:
-        messages.append(await inbox.get())
-
-
-@process
-async def raises(inbox):
+async def raises(exit):
     raise MyException
 
 
 @process
-async def until_closed(inbox):
-    await inbox.until_closed()
+async def until_exit(exit):
+    await exit
 
 
 class ProcessesTest(unittest.TestCase):
 
     @synchronous
-    async def test_process(self):
-        flag = asyncio.Event()
-        messages = []
-        proc = recv_messages(flag, messages)
-        await proc.inbox.put(1)
-        await proc.inbox.put(2)
-        await proc.inbox.put(3)
-        proc.inbox.close()
-        self.assertListEqual([], messages)
-        flag.set()
-        await proc.task
-        self.assertTrue(proc.task.done())
-        self.assertListEqual([1, 2, 3], messages)
+    async def test_stop(self):
+        proc = until_exit()
+        self.assertFalse(proc.done())
+        proc.stop()
+        await proc
+        self.assertTrue(proc.done())
 
     @synchronous
     async def test_raises(self):
         proc = raises()
+        self.assertFalse(proc.done())
         with self.assertRaises(MyException):
-            await proc.task
-
-    @synchronous
-    async def test_context(self):
-        for make_proc in [until_closed, raises]:
-            # Let context manager do await.
-            async with make_proc() as proc:
-                pass
-            self.assertTrue(proc.task.done())
-            # Context manager won't (?) do await.
-            async with make_proc() as proc:
-                await asyncio.sleep(0.01)
-            self.assertTrue(proc.task.done())
+            await proc
+        self.assertTrue(proc.done())
 
 
 if __name__ == '__main__':
