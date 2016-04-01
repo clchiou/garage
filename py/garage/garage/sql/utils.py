a7de25a6badc695f8ff9a147268a4618520dcd26
Che-Liang Chiou <clchiou@gmail.com>
2016-03-31 18:12:13 -0700

    Add ensure_only_one_row to sql.utils

--- py/garage/garage/sql/utils.py
+++ py/garage/garage/sql/utils.py
@@ -1,6 +1,14 @@
 __all__ = [
+    'ensure_only_one_row',
     'insert_or_ignore',
 ]
+
+
+def ensure_only_one_row(rows):
+    row = rows.fetchone()
+    if row is None or rows.fetchone() is not None:
+        raise KeyError
+    return row
 
 
 def insert_or_ignore(conn, table, values):
