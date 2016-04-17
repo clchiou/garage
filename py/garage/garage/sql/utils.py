d3fc9414effb4c49104cc4a0888872d9eb4c20a9
Che-Liang Chiou <clchiou@gmail.com>
2016-04-16 19:16:24 -0700

    Add a hack for appending "IF NOT EXISTS" clause to "CREATE INDEX"

--- py/garage/garage/sql/utils.py
+++ py/garage/garage/sql/utils.py
@@ -1,7 +1,21 @@
 __all__ = [
+    'add_if_not_exists_clause',
     'ensure_only_one_row',
     'insert_or_ignore',
 ]
+
+from garage import asserts
+
+from sqlalchemy.schema import CreateIndex
+
+
+def add_if_not_exists_clause(index, engine):
+    # `sqlalchemy.Index.create()` does not take `checkfirst` for reasons
+    # that I am unaware of, and here is a hack for sidestep that.
+    stmt = str(CreateIndex(index).compile(engine))
+    stmt = stmt.replace('CREATE INDEX', 'CREATE INDEX IF NOT EXISTS', 1)
+    asserts.postcond('IF NOT EXISTS' in stmt, stmt)
+    return stmt
 
 
 def ensure_only_one_row(rows):
