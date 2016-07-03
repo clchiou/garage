3deffc39e1a489255272c35f7171b7e85942b108
Che-Liang Chiou <clchiou@gmail.com>
2016-07-02 23:52:11 -0700

    Fix node/nodejs name conflict on Ubuntu systems

--- shipyard/shipyard/host/node/build.py
+++ shipyard/shipyard/host/node/build.py
@@ -3,7 +3,11 @@
 from pathlib import Path
 
 from foreman import define_parameter, decorate_rule
-from shipyard import install_packages
+from shipyard import (
+    ensure_file,
+    execute,
+    install_packages,
+)
 
 
 (define_parameter('npm_prefix')
@@ -16,7 +20,11 @@ from shipyard import install_packages
 @decorate_rule('//base:build')
 def install(parameters):
     """Set up host-only environment for Node.js."""
-    if not Path('/usr/bin/nodejs').exists():
+    if not Path('/usr/bin/node').exists():
         install_packages(['nodejs', 'npm'])
         contents = 'prefix = %s\n' % parameters['npm_prefix'].absolute()
         (Path.home() / '.npmrc').write_text(contents)
+        # Ubuntu systems use `nodejs` rather than `node` :(
+        if not Path('/usr/bin/node').exists():
+            ensure_file('/usr/bin/nodejs')
+            execute('sudo ln --symbolic nodejs node'.split(), cwd='/usr/bin')
