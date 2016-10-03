cdb67d4048e442898502c70f3b37022b562ad799
Che-Liang Chiou <clchiou@gmail.com>
2016-10-03 00:28:24 -0700

    Add find_include_dirs to buildtools

--- py/buildtools/buildtools/__init__.py
+++ py/buildtools/buildtools/__init__.py
@@ -3,6 +3,7 @@
 __all__ = [
     'register_subcommands',
     'add_cplusplus_suffix',
+    'find_include_dirs',
     'read_pkg_config',
     'make_bdist_zipapp',
     'make_copy_files',
@@ -14,6 +15,7 @@ import hashlib
 import os
 import os.path
 import stat
+import sys
 import tempfile
 from subprocess import check_call, check_output
 
@@ -42,6 +44,23 @@ def add_cplusplus_suffix(suffix):
     if not suffix.startswith('.'):
         raise ValueError('suffix is not started with ".": %s' % suffix)
     unixccompiler.UnixCCompiler.src_extensions.append(suffix)
+
+
+def find_include_dirs(argv=None):
+    if argv is None:
+        argv = sys.argv
+    include_dirs = []
+    for i, arg in enumerate(argv):
+        if arg == '-I' or arg == '--include-dirs':
+            dirs = argv[i + 1]
+        elif arg.startswith('-I'):
+            dirs = arg[len('-I'):]
+        elif arg.startswith('--include-dirs='):
+            dirs = arg[len('--include-dirs='):]
+        else:
+            continue
+        include_dirs.extend(dirs.split(':'))
+    return include_dirs
 
 
 PackageConfig = namedtuple(
