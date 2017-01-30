91b484bb4d18464a0f0c1c4ff179b0bdbf366e0b
Che-Liang Chiou <clchiou@gmail.com>
2017-01-30 01:57:01 -0800

    Wrap curio.timeout_after for non-positive duration

--- py/garage/garage/asyncs/utils.py
+++ py/garage/garage/asyncs/utils.py
@@ -1,5 +1,6 @@
 __all__ = [
     'synchronous',
+    'timeout_after',
 ]
 
 from functools import wraps
@@ -15,3 +16,29 @@ def synchronous(coro_func):
     def wrapper(*args, **kwargs):
         return curio.run(coro_func(*args, **kwargs))
     return wrapper
+
+
+def timeout_after(seconds, coro=None):
+    """Wrap curio.timeout_after for non-positive seconds argument."""
+    if seconds is None or seconds <= 0:
+        if coro is None:
+            return _TimeoutNever()
+        else:
+            return coro
+    else:
+        return curio.timeout_after(seconds, coro=coro)
+
+
+class _TimeoutNever:
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *_):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        pass
