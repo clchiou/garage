b1335c02518a1a628a06f64822d2a68de44237d5
Che-Liang Chiou <clchiou@gmail.com>
2017-02-06 20:53:08 -0800

    Add TaskStackTest

--- py/garage/tests/asyncs/test_asyncs.py
+++ py/garage/tests/asyncs/test_asyncs.py
@@ -41,5 +41,25 @@ class SpawnTest(unittest.TestCase):
         self.assertEqual('CANCELLED', task.state)
 
 
+class TaskStackTest(unittest.TestCase):
+
+    @synchronous
+    async def test_task_stack(self):
+
+        results = []
+
+        async def func(data):
+            try:
+                await curio.sleep(10)
+            finally:
+                results.append(data)
+
+        async with asyncs.TaskStack() as stack:
+            await stack.spawn(func(1))
+            await stack.spawn(func(2))
+
+        self.assertEqual([2, 1], results)
+
+
 if __name__ == '__main__':
     unittest.main()
