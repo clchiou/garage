ff476b33c26a9067e6ac64b2c161d29b0febea33
Che-Liang Chiou <clchiou@gmail.com>
2017-03-01 21:55:32 -0500

    Add unit tests for write_to and write_packed_to

--- py/capnptools/examples/tests/test_books.py
+++ py/capnptools/examples/tests/test_books.py
@@ -1,32 +1,55 @@
 import unittest
 
+import os
+import tempfile
+
 from examples import books
 
 
 class BooksTest(unittest.TestCase):
 
+    BOOK = {
+        'title': 'Moby-Dick; or, The Whale',
+        'authors': ['Herman Melville'],
+    }
+
     def test_builder(self):
+
         book = books.MallocMessageBuilder().init_root(books.Book)
-        book.title = 'Moby-Dick; or, The Whale'
-        book.authors = ['Herman Melville']
-        self.assertEqual(
-            {
-                'title': 'Moby-Dick; or, The Whale',
-                'authors': ['Herman Melville'],
-            },
-            book._as_dict(),
-        )
+        book.title = self.BOOK['title']
+        book.authors = self.BOOK['authors']
+        self.assertEqual(self.BOOK, book._as_dict())
 
         book = book._as_reader()
-        self.assertEqual('Moby-Dick; or, The Whale', book.title)
-        self.assertEqual(['Herman Melville'], book.authors._as_dict())
-        self.assertEqual(
-            {
-                'title': 'Moby-Dick; or, The Whale',
-                'authors': ['Herman Melville'],
-            },
-            book._as_dict(),
-        )
+        self.assertEqual(self.BOOK['title'], book.title)
+        self.assertEqual(self.BOOK['authors'], book.authors._as_dict())
+        self.assertEqual(self.BOOK, book._as_dict())
+
+    def test_write(self):
+
+        builder = books.MallocMessageBuilder()
+        book = builder.init_root(books.Book)
+        book.title = self.BOOK['title']
+        book.authors = self.BOOK['authors']
+
+        for read_cls, write_func in [
+                ('StreamFdMessageReader', 'write_to'),
+                ('PackedFdMessageReader', 'write_packed_to')]:
+
+            with self.subTest(read_cls=read_cls, write_func=write_func):
+                fd, path = tempfile.mkstemp()
+                try:
+                    getattr(builder, write_func)(fd)
+                    os.close(fd)
+
+                    fd = os.open(path, os.O_RDONLY)
+                    reader = getattr(books, read_cls)(fd)
+                    book = reader.get_root(books.Book)
+                    self.assertEqual(self.BOOK, book._as_dict())
+
+                finally:
+                    os.unlink(path)
+                    os.close(fd)
 
 
 if __name__ == '__main__':
