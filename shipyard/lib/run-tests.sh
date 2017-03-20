64a525d25d7d09f0773f3e43cbca0cb1e54bfa66
Che-Liang Chiou <clchiou@gmail.com>
2017-03-20 02:45:07 -0400

    Deprecate lib and shipyard

--- shipyard/lib/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o xtrace
-
-# HACK: Prevent unittest from loading shipyard/__init__.py (which will
-# raise import errors due to path issues).
-python3 -m unittest discover -s tests
--- shipyard/shipyard/py/requests/build.py
+++
@@ -1,9 +0,0 @@
-"""Install requests."""
-
-from shipyard import py
-
-
-py.define_pip_package(
-    package_name='requests',
-    version='2.10.0',
-)
