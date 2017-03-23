b84ec77ea76195e82612d8cc22f1582b78fa3540
Che-Liang Chiou <clchiou@gmail.com>
2017-03-22 22:56:21 -0400

    Start redesigning ops tool

--- py/ops/setup.py
+++ py/ops/setup.py
@@ -5,12 +5,15 @@ import buildtools
 
 setup(
     name = 'ops',
-    description = 'Operation tools',
+    description = 'Operations tool',
     cmdclass = {
         'bdist_zipapp': buildtools.make_bdist_zipapp(main='ops:main'),
     },
     packages = [
         'ops',
         'ops.pods',
+    ],
+    install_requires = [
+        'garage',
     ],
 )
