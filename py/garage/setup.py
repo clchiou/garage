3deb17003e9b80d53a23a83ca94e95f4e6814dd9
Che-Liang Chiou <clchiou@gmail.com>
2017-03-22 22:13:39 -0400

    Add bdist_zipapp to garage/setup.py

--- py/garage/setup.py
+++ py/garage/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+import buildtools
+
 
 setup(
     name = 'garage',
     description = 'Python modules developed in the garage',
     license = 'MIT',
+    cmdclass = {
+        'bdist_zipapp': buildtools.make_bdist_zipapp(main_optional=True),
+    },
     packages = find_packages(exclude=['tests*']),
     extras_require = {
         'asyncs': ['curio'],
