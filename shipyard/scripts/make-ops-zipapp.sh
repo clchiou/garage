628f17bbaea525b7b9dc16ed9d60abaed35001a6
Che-Liang Chiou <clchiou@gmail.com>
2017-03-24 20:46:25 -0400

    Add make-ops-zipapp.sh for easier packaging ops tool

---
+++ shipyard/scripts/make-ops-zipapp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Bundle together ops tool and its dependencies in one zip app (using
+# this script is easier than doing it in a builder).
+
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+if [[ "${#}" -lt 1 ]]; then
+  show "usage: ${PROG} output"
+  exit 1
+fi
+
+readonly OUTPUT="$(realpath "${1}")"
+
+set -o xtrace
+
+# ops
+pushd "${ROOT}/py/ops"
+python3 setup.py build bdist_zipapp --output "${OUTPUT}"
+popd
+
+# garage
+pushd "${ROOT}/py/garage"
+python3 setup.py build bdist_zipapp --output "${OUTPUT}"
+popd
+
+# startup
+pushd "${ROOT}/py/startup"
+# NOTE: startup/setup.py does not support bdist_zipapp
+zip --grow -r "${OUTPUT}" startup.py
+popd
