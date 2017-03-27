01801f6a1751da9a4a9e69b5de10b3900c9d0f62
Che-Liang Chiou <clchiou@gmail.com>
2017-03-26 22:27:08 -0400

    Polish make-ops-zipapp.sh

--- shipyard/scripts/make-ops-zipapp.sh
+++ shipyard/scripts/make-ops-zipapp.sh
@@ -6,26 +6,40 @@
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 if [[ "${#}" -lt 1 ]]; then
-  show "usage: ${PROG} OUTPUT"
+  show "usage: ${PROG} [PYTHON3] OUTPUT"
   exit 1
+fi
+
+# Add buildtools to PYTHONPATH
+export PYTHONPATH="${PYTHONPATH:-}${PYTHONPATH:+:}${ROOT}/py/buildtools"
+
+if [[ "${#}" -gt 1 ]]; then
+  readonly PYTHON3="${1}"
+  shift
 fi
 
 readonly OUTPUT="$(realpath "${1}")"
 
 set -o xtrace
 
+rm -f "${OUTPUT}"
+
 # ops
 pushd "${ROOT}/py/ops"
-python3 setup.py build bdist_zipapp --output "${OUTPUT}"
+rm -rf build  # Clean up any previous build
+"${PYTHON3:-python3}" setup.py build bdist_zipapp --output "${OUTPUT}"
+rm -rf build
 popd
 
 # garage
 pushd "${ROOT}/py/garage"
-python3 setup.py build bdist_zipapp --output "${OUTPUT}"
+rm -rf build  # Clean up any previous build
+"${PYTHON3:-python3}" setup.py build bdist_zipapp --output "${OUTPUT}"
+rm -rf build
 popd
 
 # startup
 pushd "${ROOT}/py/startup"
-# NOTE: startup/setup.py does not support bdist_zipapp
+# startup/setup.py does not support bdist_zipapp at the moment
 zip --grow -r "${OUTPUT}" startup.py
 popd
