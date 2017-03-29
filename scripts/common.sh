6354a4e261c48951579facd5eb711676810f44a2
Che-Liang Chiou <clchiou@gmail.com>
2017-03-29 01:00:04 -0400

    Add helper functions to scripts/common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,10 +9,6 @@ show() {
   echo "${@}" 1>&2
 }
 
-info() {
-  show "${PROG}: ${FUNCNAME[1]}: ${@}"
-}
-
 abort() {
   show "${PROG}: ${FUNCNAME[1]}: ${@}"
   exit 1
@@ -21,3 +17,23 @@ abort() {
 # ROOT is /path/to/garage
 readonly ROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")"
 [[ -d "${ROOT}/.git" ]] || abort "not git repo: ${ROOT}"
+
+# Helper functions
+
+info() {
+  show "${PROG}: ${FUNCNAME[1]}: ${@}"
+}
+
+# Use this if you don't want to enable xtrace
+log_and_run() {
+  show "+ ${@}"
+  "${@}"
+}
+
+ensure_file() {
+  [[ -f "${1}" ]] || abort "not a file: ${1}"
+}
+
+ensure_directory() {
+  [[ -d "${1}" ]] || abort "not a directory: ${1}"
+}
