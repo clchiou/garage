ffd1bcfc59b019168e82ecfb1851e8784c875ad6
Che-Liang Chiou <clchiou@gmail.com>
2017-04-05 23:27:13 -0400

    Add `--recursive` to scripts.cp

--- py/garage/garage/scripts.py
+++ py/garage/garage/scripts.py
@@ -340,8 +340,10 @@ def apt_get_install(packages, *, only_missing=True):
     execute(cmd)
 
 
-def cp(src, dst, *, preserve=('mode',)):
+def cp(src, dst, *, recursive=False, preserve=('mode',)):
     cmd = ['cp', '--force']
+    if recursive:
+        cmd.append('--recursive')
     if preserve:
         cmd.append('--preserve=%s' % ','.join(preserve))
     cmd.append(src)
