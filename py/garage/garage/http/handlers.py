cd906cc0727e04b9f6dca46b654401a91f8b25de
Che-Liang Chiou <clchiou@gmail.com>
2017-04-29 16:45:46 -0400

    Add handlers.parse_request

--- py/garage/garage/http/handlers.py
+++ py/garage/garage/http/handlers.py
@@ -2,9 +2,16 @@
 
 __all__ = [
     'ApiEndpointHandler',
+    'UriPath',
+    'parse_request',
 ]
 
+from pathlib import PurePosixPath as UriPath
+import urllib.parse
+
 import http2
+
+from . import servers
 
 
 class ApiEndpointHandler:
@@ -27,3 +34,32 @@ class ApiEndpointHandler:
         headers.extend(self.make_response_headers(request.headers))
         await stream.submit_response(
             http2.Response(headers=headers, body=output))
+
+
+def parse_request(request) -> urllib.parse.SplitResult:
+
+    if not request.scheme:
+        raise servers.ClientError(http2.Status.BAD_REQUEST)
+
+    if not request.path:
+        raise servers.ClientError(http2.Status.BAD_REQUEST)
+
+    authority = request.authority
+    if not authority:
+        for header, value in request.headers:
+            if header != b'Host':
+                continue
+            if authority:
+                msg = 'duplicate "Host" header: %r, %r' % (authority, value)
+                raise servers.ClientError(
+                    http2.Status.BAD_REQUEST, internal_message=msg)
+            authority = value
+    if not authority:
+        raise servers.ClientError(http2.Status.BAD_REQUEST)
+
+    try:
+        uri = b'%s://%s%s' % (request.scheme.value, authority, request.path)
+        result = urllib.parse.urlsplit(uri.decode('ascii'))
+        return result._replace(path=UriPath(result.path))
+    except Exception as exc:
+        raise servers.ClientError(http2.Status.BAD_REQUEST) from exc
