1d2e1ec8e15d0d20c585a8380287db64e69e3f08
Che-Liang Chiou <clchiou@gmail.com>
2017-07-12 17:18:47 -0400

    Correct "graceful period" to "grace period"

--- py/garage/garage/asyncs/servers.py
+++ py/garage/garage/asyncs/servers.py
@@ -18,14 +18,14 @@ LOG = logging.getLogger(__name__)
 
 
 # The root node of the supervisor tree
-async def serve(graceful_exit, graceful_period, make_server_funcs):
+async def serve(graceful_exit, grace_period, make_server_funcs):
     okay = NOT_OKAY
     async with asyncs.TaskStack() as servers:
         LOG.info('start servers: pid=%d', os.getpid())
         for make_server in make_server_funcs:
             await servers.spawn(make_server())
         # Also spawn default signal handler
-        await servers.spawn(signal_handler(graceful_exit, graceful_period))
+        await servers.spawn(signal_handler(graceful_exit, grace_period))
         # Now let's wait for the servers...
         server_task = await asyncs.select(servers)
         # When one server exits, normally or not, we bring down all
@@ -42,7 +42,7 @@ async def serve(graceful_exit, graceful_period, make_server_funcs):
     return okay
 
 
-async def signal_handler(graceful_exit, graceful_period):
+async def signal_handler(graceful_exit, grace_period):
     # Exploit the fact that when one of the server task exits, the init
     # task will bring down all other server tasks
     async with curio.SignalQueue(signal.SIGINT, signal.SIGTERM) as sigqueue:
@@ -55,8 +55,8 @@ async def signal_handler(graceful_exit, graceful_period):
             return
         else:
             raise AssertionError('unknown signal: %s' % sig)
-        async with curio.ignore_after(graceful_period):
+        async with curio.ignore_after(grace_period):
             sig = await sigqueue.get()
             LOG.info('receive signal again: %s', sig)
             return
-        LOG.info('exceed graceful period %f', graceful_period)
+        LOG.info('exceed grace period %f', grace_period)
--- py/garage/garage/startups/asyncs/servers.py
+++ py/garage/garage/startups/asyncs/servers.py
@@ -11,16 +11,16 @@ from garage.asyncs import Event, servers
 
 class GracefulExitComponent(components.Component):
 
-    GRACEFUL_PERIOD = 5  # Unit: seconds
+    GRACE_PERIOD = 5  # Unit: seconds
 
     provide = components.make_fqname_tuple(__name__, 'graceful_exit')
 
     def add_arguments(self, parser):
         group = parser.add_argument_group(servers.__name__)
         group.add_argument(
-            '--graceful-period',
-            default=self.GRACEFUL_PERIOD, type=int,
-            help="""set graceful period when terminating servers
+            '--grace-period',
+            default=self.GRACE_PERIOD, type=int,
+            help="""set grace period when terminating servers
                     (default to %(default)s seconds)
                  """)
 
@@ -43,6 +43,6 @@ class ServerContainerComponent(components.Component):
         return functools.partial(
             servers.serve,
             require.graceful_exit,
-            require.args.graceful_period,
+            require.args.grace_period,
             require.make_server,
         )
