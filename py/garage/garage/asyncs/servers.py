df5f5ad8a0a855d221d0b4b7d33f2a6d36598b2e
Che-Liang Chiou <clchiou@gmail.com>
2017-07-30 18:07:38 -0400

    Rewrite internals of garage.asyncs.servers

--- py/garage/garage/asyncs/servers.py
+++ py/garage/garage/asyncs/servers.py
@@ -19,32 +19,61 @@ LOG = logging.getLogger(__name__)
 
 # The root node of the supervisor tree
 async def serve(graceful_exit, grace_period, make_server_funcs):
+
     okay = NOT_OKAY
+
     async with asyncs.TaskStack() as servers:
+
         LOG.info('start servers: pid=%d', os.getpid())
         for make_server in make_server_funcs:
             await servers.spawn(make_server())
-        # Also spawn default signal handler
-        await servers.spawn(signal_handler(graceful_exit, grace_period))
-        # Now let's wait for the servers...
-        server_task = await asyncs.select(servers)
+        all_tasks = list(servers)
+
+        # Also spawn default signal handler.
+        signal_handler_task = await asyncs.spawn(
+            signal_handler(graceful_exit, grace_period),
+            daemon=True,
+        )
+        all_tasks.append(signal_handler_task)
+
         # When one server exits, normally or not, we bring down all
-        # other servers
-        try:
-            await server_task.join()
-            LOG.info('server exit: %r', server_task)
-            okay = OKAY
-        except curio.TaskError:
-            LOG.exception('server crash: %r', server_task)
-        LOG.info('stop servers')
-        # TaskStack will cancel all the remaining tasks
+        # other servers.  But if it exits normally, we initiate a
+        # graceful exit.
+        server = await asyncs.select(all_tasks)
+        if server is signal_handler_task:
+            pass
+        elif server.exception:
+            LOG.error('server crash: %r', server, exc_info=server.exception)
+        else:
+            LOG.info('notify graceful exit')
+            graceful_exit.set()
+            async with curio.ignore_after(grace_period) as timeout:
+                okay = await wait_servers(servers)
+            if timeout.expired:
+                LOG.info('exceed grace period %f', grace_period)
+
+        # When we leave this block, TaskStack will cancel all the
+        # remaining tasks.
+
     LOG.info('exit')
+    return okay
+
+
+async def wait_servers(servers):
+    okay = OKAY
+    for server in servers:
+        await server.wait()
+        okay &= not server.exception
+        if server.exception:
+            LOG.error('server crash: %r', server, exc_info=server.exception)
+        else:
+            LOG.info('server exit: %r', server)
     return okay
 
 
 async def signal_handler(graceful_exit, grace_period):
     # Exploit the fact that when one of the server task exits, the init
-    # task will bring down all other server tasks
+    # task will bring down all other server tasks.
     async with curio.SignalQueue(signal.SIGINT, signal.SIGTERM) as sigqueue:
         sig = await sigqueue.get()
         LOG.info('receive signal: %s', sig)
