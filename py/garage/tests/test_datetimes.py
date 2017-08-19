7f5487589e0599ad97345ded9e0a7974b5229e52
Che-Liang Chiou <clchiou@gmail.com>
2017-08-19 15:14:54 -0400

    Not test stdlib in garage/tests/test_datetimes.py
    The test will fail inside a container because the timezone is UTC and
    we can't (or shouldn't) change it in a unit test.
    
    And it's probably not necessary to test stdlib's (implicit) behavior in
    our unit test.

--- py/garage/tests/test_datetimes.py
+++ py/garage/tests/test_datetimes.py
@@ -17,16 +17,11 @@ class DatetimesTest(unittest.TestCase):
     def test_fromtimestamp(self):
         zero = datetime(1970, 1, 1, 0, 0)
 
-        # We handle timezone correctly (I guess?).
+        # We do not use the implicit local timezone like stdlib (which
+        # will surprise you).
         dt = datetimes.utcfromtimestamp(0)
         self.assertEqual(zero, dt.replace(tzinfo=None))
         self.assertEqual(0, dt.timestamp())
-
-        # But stdlib's doesn't.
-        dt = datetime.fromtimestamp(0)
-        self.assertNotEqual(zero, dt)  # Incorrect value.
-        dt = datetime.utcfromtimestamp(0)
-        self.assertNotEqual(0, dt.timestamp())  # Incorrect timestamp.
 
 
 if __name__ == '__main__':
