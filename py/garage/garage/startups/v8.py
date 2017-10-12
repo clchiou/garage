8eeb36b7b4967c376924833bee12739b3024cdaf
Che-Liang Chiou <clchiou@gmail.com>
2017-10-12 02:58:29 -0400

    Rewrite V8Component

--- py/garage/garage/startups/v8.py
+++ py/garage/garage/startups/v8.py
@@ -3,7 +3,6 @@ __all__ = [
 ]
 
 from garage import components
-from garage.threads.utils import make_get_thread_local
 
 from v8 import V8
 
@@ -12,12 +11,7 @@ class V8Component(components.Component):
 
     require = components.EXIT_STACK
 
-    provide = components.make_fqname_tuple(__name__, 'get_v8_isolate')
+    provide = components.make_fqname_tuple(__name__, 'v8')
 
     def make(self, require):
-        exit_stack = require.exit_stack
-        v8 = exit_stack.enter_context(V8())
-        return make_get_thread_local(
-            'v8_isolate',
-            lambda: exit_stack.enter_context(v8.isolate()),
-        )
+        return require.exit_stack.enter_context(V8())
