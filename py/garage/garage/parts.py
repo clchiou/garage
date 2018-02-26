51eb12b39434b9d948cd6693e31457d817d6534b
Che-Liang Chiou <clchiou@gmail.com>
2018-02-26 00:06:45 +0000

    Prototype garage.parts.assemble

--- py/garage/garage/parts.py
+++ py/garage/garage/parts.py
@@ -1,12 +1,13 @@
 """Parts of a process.
 
 Here you may:
-  * Define list of part names that a module produces, even recursively.
-  * Refer to these part names from another module.
-  * Search and assemble parts.
+  * Define list of part names that a module may make, even recursively.
+  * Refer to parts by name.
+  * Register maker of parts.
+  * Assemble parts.
 
 The `parts` module will replace the `components` module as the way to
-assemble a process.
+assemble pieces of a process.
 
 What about the name "parts"?  It is said that a component is usually
 self-contained but a part might be not; so these pieces are called parts
@@ -15,14 +16,21 @@ easier to type "part" than "component").
 """
 
 __all__ = [
+    'AUTO',
     'PartList',
-    'auto',
+    'assemble',
+    'register_maker',
 ]
+
+import inspect
+from collections import defaultdict
 
 from garage.assertions import ASSERT
 
+from startup import Startup
 
-auto = object()
+
+AUTO = object()
 
 
 def _assert_name(name):
@@ -34,6 +42,7 @@ def _assert_name(name):
     return name
 
 
+# Make it a subclass of str so that `startup` accepts it.
 class PartName(str):
     """Represent a part's name.
 
@@ -76,7 +85,25 @@ class PartName(str):
 
 
 class PartList:
-    """List of part names."""
+    """List of part names.
+
+    To create a part list, you provide the module name and a list of
+    entries where each entry is either a name or another part list:
+        # Assume __name__ == 'some.module'
+        PartList(__name__, [
+            ('name_1', AUTO),
+            ('name_2', 'some_part_name'),
+            ('sub_list', another_part_list),
+        ])
+
+    Then you may refer to these part names like this:
+        assert part_list.name_1 == 'some.module:name_1'
+        assert part_list.name_2 == 'some.module:some_part_name'
+
+    NOTE: If another part list is referred in the list, it will be
+    copied and rebased (i.e., all part names will be nested under the
+    new part list).
+    """
 
     def __repr__(self):
         return '<%s.%s 0x%x %s>' % (
@@ -91,7 +118,7 @@ class PartList:
         self._attr_names = []
         for attr_name, entry in entries:
             _assert_name(attr_name)
-            if entry is auto:
+            if entry is AUTO:
                 value = PartName(module_name, attr_name)
             elif isinstance(entry, PartList):
                 value = entry._rebase(module_name, attr_name)
@@ -112,3 +139,167 @@ class PartList:
             part_list.__dict__[attr_name] = value._rebase(module_name, prefix)
             part_list._attr_names.append(attr_name)
         return part_list
+
+
+# Table of output part name -> maker -> list of input part names.
+_MAKER_TABLE = defaultdict(dict)
+
+
+def register_maker(maker):
+    """Register a part maker function."""
+    return _register_maker(_MAKER_TABLE, maker)
+
+
+def _register_maker(maker_table, maker):
+
+    sig = inspect.signature(maker)
+
+    input_part_names = []
+    for parameter in sig.parameters.values():
+        if parameter.annotation is sig.empty:
+            # We should probably not err out here because maker could be
+            # a wrapper, and this parameter is bound to some default (if
+            # not, eventually an error will be raised when this maker is
+            # called).
+            continue
+        if isinstance(parameter.annotation, list):
+            ASSERT(
+                len(parameter.annotation) == 1,
+                'expect `[x]`-form annotation, not: %r', parameter.annotation,
+            )
+            part_name = parameter.annotation[0]
+        else:
+            part_name = parameter.annotation
+        input_part_names.append(ASSERT.type_of(part_name, str))
+
+    ASSERT(
+        sig.return_annotation is not sig.empty,
+        'expect maker be annotated on its return value: %r', maker,
+    )
+    if isinstance(sig.return_annotation, tuple):
+        for output_part_name in sig.return_annotation:
+            ASSERT.type_of(output_part_name, str)
+            maker_table[output_part_name][maker] = input_part_names
+    else:
+        ASSERT.type_of(sig.return_annotation, str)
+        maker_table[sig.return_annotation][maker] = input_part_names
+
+    return maker
+
+
+def assemble(part_names, *, input_parts=None, selected_makers=None):
+    """Assemble parts.
+
+    For each part name, it searches registered part maker (recursively)
+    and assembles the (sub-)parts.
+
+    You may provide parts that no maker are registered for via the
+    `input_parts` argument.
+
+    If there are multiple registered makers for a part, either one of
+    the following will happen:
+      * Some makers are selected by the caller.
+      * All makers are selected by the caller.
+      * An error is raised.
+    The `selected_makers` argument looks like this:
+        selected_makers = {
+            part_name: [maker, ...],  # Some makers are selected.
+            another_part_name: all,  # All makers are selected.
+        }
+
+    NOTE: It is an error when both an input part is provided and a maker
+    is registered for that part name.
+    """
+    return _assemble(
+        _MAKER_TABLE,
+        part_names,
+        input_parts or {},
+        selected_makers or {},
+    )
+
+
+def _assemble(maker_table, part_names, input_parts, selected_makers):
+    startup = Startup()
+    sources = find_sources(
+        part_names,
+        input_parts,
+        maker_table, selected_makers,
+    )
+    for maker, pair in sources:
+        if maker:
+            startup(maker)
+        else:
+            startup.set(pair[0], pair[1])
+    return startup.call()
+
+
+def find_sources(part_names, input_parts, maker_table, selected_makers):
+    """For each part name, find its source recursively.
+
+    A source is either a maker, or a part provided by the caller.
+    """
+
+    part_names = set(part_names)
+
+    seen_part_names = set()
+
+    yielded_makers = set()
+
+    def maybe_yield_maker(maker, input_part_names):
+        if maker not in yielded_makers:
+            part_names.update(input_part_names)
+            yielded_makers.add(maker)
+            yield maker, None
+
+    while part_names:
+
+        part_name = part_names.pop()
+        if part_name in seen_part_names:
+            continue
+
+        seen_part_names.add(part_name)
+
+        # No maker is registered for this part; try input parts.
+        if part_name not in maker_table:
+            ASSERT(
+                part_name in input_parts,
+                'expect part %s from caller', part_name,
+            )
+            yield None, (part_name, input_parts[part_name])
+            continue
+
+        # Try registered maker(s).
+        ASSERT(
+            part_name not in input_parts,
+            'expect part %s by maker, not from caller', part_name,
+        )
+        maker_to_input_part_names = maker_table[part_name]
+
+        # Easy, only one maker is registered for this part.
+        if len(maker_to_input_part_names) == 1:
+            yield from maybe_yield_maker(
+                *next(iter(maker_to_input_part_names.items())))
+            continue
+
+        # It is getting complex as multiple makers are registered for
+        # this part.
+        selected = selected_makers.get(part_name)
+        ASSERT(
+            selected is not None,
+            'expect caller to select maker(s) for %s', part_name,
+        )
+
+        # Okay, the caller wants them all.
+        if selected is all:
+            for maker, input_part_names in maker_to_input_part_names.items():
+                yield from maybe_yield_maker(maker, input_part_names)
+            continue
+
+        # Try the selected makers.
+        for maker in selected:
+            input_part_names = maker_to_input_part_names.get(maker)
+            ASSERT(
+                input_part_names is not None,
+                'expect maker to be registered: %r', maker,
+            )
+            yield from maybe_yield_maker(maker, input_part_names)
--- py/garage/tests/test_parts.py
+++ py/garage/tests/test_parts.py
@@ -1,5 +1,8 @@
 import unittest
 
+import functools
+from collections import defaultdict
+
 from garage import parts
 
 
@@ -24,18 +27,22 @@ class PartsTest(unittest.TestCase):
     def test_part_list(self):
 
         plist1 = parts.PartList('a.b.c', [
-            ('x', parts.auto),
+            ('x', parts.AUTO),
             ('y', 'Y'),
         ])
 
         plist2 = parts.PartList('d.e.f', [
             ('pl1', plist1),
-            ('z', parts.auto),
+            ('z', parts.AUTO),
         ])
 
         plist3 = parts.PartList('g.h.i', [
             ('pl1', plist1),
             ('pl2', plist2),
+        ])
+
+        plist4 = parts.PartList('j.k.l', [
+            ('pl3', plist3),
         ])
 
         self.assertEqual('a.b.c:x', plist1.x)
@@ -49,6 +56,276 @@ class PartsTest(unittest.TestCase):
         self.assertEqual('g.h.i:pl1.Y', plist3.pl1.y)
         self.assertEqual('g.h.i:pl2.pl1.x', plist3.pl2.pl1.x)
         self.assertEqual('g.h.i:pl2.pl1.Y', plist3.pl2.pl1.y)
+
+        self.assertEqual('j.k.l:pl3.pl2.pl1.x', plist4.pl3.pl2.pl1.x)
+        self.assertEqual('j.k.l:pl3.pl2.pl1.Y', plist4.pl3.pl2.pl1.y)
+
+    def test_register_maker_on_wrapper(self):
+
+        plist = parts.PartList('foo', [('x', parts.AUTO), ('y', parts.AUTO)])
+
+        def func(x: plist.x, arg) -> plist.y:
+            pass
+
+        f1 = functools.partial(func, arg=None)
+
+        @functools.wraps(func)
+        def f2(x):
+            pass
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, func)
+        parts._register_maker(maker_table, f1)
+        parts._register_maker(maker_table, f2)
+        self.assertEqual(
+            {plist.y: {func: [plist.x], f1: [plist.x], f2: [plist.x]}},
+            maker_table,
+        )
+
+    def test_find_sources(self):
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_sub_part_1)
+        parts._register_maker(maker_table, make_sub_part_2)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assert_sources_equal(
+            [
+                (make_final_part, None),
+                (make_sub_part_1, None),
+                (make_sub_part_2, None),
+            ],
+            parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {},
+            ),
+        )
+
+        pattern = r'expect part a.b.c:some_part from caller'
+        with self.assertRaisesRegex(AssertionError, pattern):
+            list(parts.find_sources(
+                [PLIST.some_part],
+                {},
+                maker_table,
+                {},
+            ))
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_all_sub_parts)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assert_sources_equal(
+            [
+                (make_all_sub_parts, None),
+                (make_final_part, None),
+            ],
+            parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {},
+            ),
+        )
+
+    def test_find_sources_with_input_parts(self):
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assert_sources_equal(
+            [
+                (make_final_part, None),
+                (None, (PLIST.sub_part_1, 'hello')),
+                (None, (PLIST.sub_part_2, 'world')),
+            ],
+            parts.find_sources(
+                [PLIST.final_part],
+                {PLIST.sub_part_1: 'hello', PLIST.sub_part_2: 'world'},
+                maker_table,
+                {},
+            ),
+        )
+
+        pattern = r'expect part a.b.c:sub_part_2 from caller'
+        with self.assertRaisesRegex(AssertionError, pattern):
+            list(parts.find_sources(
+                [PLIST.final_part],
+                {PLIST.sub_part_1: 'hello'},
+                maker_table,
+                {},
+            ))
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_final_part)
+        parts._register_maker(maker_table, make_sub_part_1)
+
+        pattern = r'expect part a.b.c:sub_part_1 by maker, not from caller'
+        with self.assertRaisesRegex(AssertionError, pattern):
+            list(parts.find_sources(
+                [PLIST.final_part],
+                {PLIST.sub_part_1: 'hello', PLIST.sub_part_2: 'world'},
+                maker_table,
+                {},
+            ))
+
+    def test_find_sources_with_selected_makers(self):
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_sub_part_1)
+        parts._register_maker(maker_table, make_sub_part_1_another)
+        parts._register_maker(maker_table, make_sub_part_2)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assert_sources_equal(
+            [
+                (make_final_part, None),
+                (make_sub_part_1_another, None),
+                (make_sub_part_2, None),
+            ],
+            parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {PLIST.sub_part_1: [make_sub_part_1_another]},
+            ),
+        )
+
+        self.assert_sources_equal(
+            [
+                (make_final_part, None),
+                (make_sub_part_1, None),
+                (make_sub_part_1_another, None),
+                (make_sub_part_2, None),
+            ],
+            parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {PLIST.sub_part_1: all},
+            ),
+        )
+
+        pattern = r'expect caller to select maker\(s\) for a.b.c:sub_part_1'
+        with self.assertRaisesRegex(AssertionError, pattern):
+            list(parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {},
+            ))
+
+        pattern = (
+            r'expect maker to be registered: '
+            r'<function make_sub_part_1_yet_another at.*>'
+        )
+        with self.assertRaisesRegex(AssertionError, pattern):
+            list(parts.find_sources(
+                [PLIST.final_part],
+                {},
+                maker_table,
+                {PLIST.sub_part_1: [make_sub_part_1_yet_another]},
+            ))
+
+    def assert_sources_equal(self, expect, actual):
+        key = lambda blob: (str(blob[0]), blob[1])
+        self.assertEqual(sorted(expect, key=key), sorted(actual, key=key))
+
+    def test_assemble(self):
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_sub_part_1)
+        parts._register_maker(maker_table, make_sub_part_2)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assertEqual(
+            {
+                PLIST.sub_part_1: 'part from make_sub_part_1',
+                PLIST.sub_part_2: 'part from make_sub_part_2',
+                PLIST.final_part: 'part from make_final_part',
+            },
+            parts._assemble(
+                maker_table,
+                [PLIST.final_part],
+                {},
+                {},
+            ),
+        )
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_all_sub_parts)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assertEqual(
+            {
+                PLIST.sub_part_1: 'part 1 from make_all_sub_parts',
+                PLIST.sub_part_2: 'part 2 from make_all_sub_parts',
+                PLIST.final_part: 'part from make_final_part',
+            },
+            parts._assemble(
+                maker_table,
+                [PLIST.final_part],
+                {},
+                {},
+            ),
+        )
+
+        maker_table = defaultdict(dict)
+        parts._register_maker(maker_table, make_sub_part_1)
+        parts._register_maker(maker_table, make_final_part)
+
+        self.assertEqual(
+            {
+                PLIST.sub_part_1: 'part from make_sub_part_1',
+                PLIST.sub_part_2: 'part from input',
+                PLIST.final_part: 'part from make_final_part',
+            },
+            parts._assemble(
+                maker_table,
+                [PLIST.final_part],
+                {PLIST.sub_part_2: 'part from input'},
+                {},
+            ),
+        )
+
+
+PLIST = parts.PartList('a.b.c', [
+    ('sub_part_1', parts.AUTO),
+    ('sub_part_2', parts.AUTO),
+    ('final_part', parts.AUTO),
+    ('some_part', parts.AUTO),
+])
+
+
+def make_sub_part_1() -> PLIST.sub_part_1:
+    return 'part from make_sub_part_1'
+
+
+def make_sub_part_1_another() -> PLIST.sub_part_1:
+    return 'part from make_sub_part_1_another'
+
+
+def make_sub_part_1_yet_another() -> PLIST.sub_part_1:
+    return 'part from make_sub_part_1_yet_another'
+
+
+def make_sub_part_2() -> PLIST.sub_part_2:
+    return 'part from make_sub_part_2'
+
+
+def make_all_sub_parts() -> (PLIST.sub_part_1, PLIST.sub_part_2):
+    return (
+        'part 1 from make_all_sub_parts',
+        'part 2 from make_all_sub_parts',
+    )
+
+
+def make_final_part(
+        sub_part_1: [PLIST.sub_part_1],
+        sub_part_2: PLIST.sub_part_2,
+        ) -> PLIST.final_part:
+    return 'part from make_final_part'
 
 
 if __name__ == '__main__':
