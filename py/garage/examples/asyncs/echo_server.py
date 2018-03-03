294779654ba5424c1fbf1a359ff38c6ad977c81c
Che-Liang Chiou <clchiou@gmail.com>
2018-03-03 10:44:54 +0000

    Rewrite examples/asyncs/echo_server.py under new apps framework

--- py/garage/examples/asyncs/echo_server.py
+++ py/garage/examples/asyncs/echo_server.py
@@ -1,42 +1,33 @@
-"""Simple echo server."""
+"""Simple TCP echo server."""
 
-from functools import partial
+import functools
 import logging
 
 import curio
 
-from garage import cli
-from garage import components
-from garage.asyncs.utils import make_server_socket, serve
-from garage.startups.asyncs.servers import (
-    GracefulExitComponent,
-    ServerContainerComponent,
-)
+from garage import apps
+from garage import parameters
+from garage import parts
+from garage.asyncs import utils
+from garage.partdefs.asyncs import servers
 
 
 LOG = logging.getLogger(__name__)
 
 
-class ServerComponent(components.Component):
+PARAMS = parameters.get(__name__)
+PARAMS.port = parameters.define(25000, 'set port')
 
-    require = (components.ARGS, GracefulExitComponent.provide.graceful_exit)
 
-    provide = ServerContainerComponent.require.make_server
-
-    def add_arguments(self, parser):
-        group = parser.add_argument_group(__name__)
-        group.add_argument(
-            '--port', default=25000, type=int,
-            help="""set port (default to %(default)s)""")
-
-    def make(self, require):
-        return partial(
-            serve,
-            require.graceful_exit,
-            partial(make_server_socket, ('', require.args.port)),
-            handle,
-            logger=LOG,
-        )
+@parts.register_maker
+def make_server(
+        graceful_exit: servers.PARTS.graceful_exit) -> servers.PARTS.server:
+    return utils.serve(
+        graceful_exit,
+        functools.partial(utils.make_server_socket, ('', PARAMS.port.get())),
+        handle,
+        logger=LOG,
+    )
 
 
 async def handle(client_sock, client_addr):
@@ -47,12 +38,12 @@ async def handle(client_sock, client_addr):
     LOG.info('close connection to: %s', client_addr)
 
 
-@cli.command('echo-server')
-@cli.component(ServerContainerComponent)
-@cli.component(ServerComponent)
-def main(serve: ServerContainerComponent.provide.serve):
+@apps.with_prog('echo-server')
+@apps.with_selected_makers({servers.PARTS.server: all})
+@apps.using_parts(serve=servers.PARTS.serve)
+def main(_, serve):
     return 0 if curio.run(serve()) else 1
 
 
 if __name__ == '__main__':
-    main()
+    apps.run(main)
