64688bd9c0d7558bdbfceeb0f4159ce677938e2e
Che-Liang Chiou <clchiou@gmail.com>
2018-03-04 07:36:33 +0000

    Fix and rewrite garage.partdefs.http.servers

--- py/garage/garage/partdefs/http/servers.py
+++ py/garage/garage/partdefs/http/servers.py
@@ -11,16 +11,19 @@ from garage.partdefs.asyncs import servers
 
 def define_parts(module_name):
     return parts.PartList(module_name, [
-        ('server', parts.AUTO),
+        ('handler', parts.AUTO),
+        ('logger', parts.AUTO),
     ])
 
 
-def define_params(*, host='', port=8443):
+def define_params(*, host='', port=80):
     params = parameters.define_namespace('create async http(s) server')
     params.host = parameters.define(host, 'let server bind to this address')
     params.port = parameters.define(port, 'let server listen to this port')
     params.certificate = parameters.define('', 'set server TLS certificate')
     params.private_key = parameters.define('', 'set server TLS private key')
+    params.enable_client_authentication = parameters.define(
+        False, 'enable authenticate client certificate')
     return params
 
 
@@ -28,7 +31,9 @@ def define_maker(part_list, params):
 
     def make_server(
             graceful_exit: servers.PARTS.graceful_exit,
-        ) -> part_list.server:
+            handler: part_list.handler,
+            logger: part_list.logger,
+        ) -> servers.PARTS.server:
 
         make_server_socket = functools.partial(
             garage.asyncs.utils.make_server_socket,
@@ -37,24 +42,23 @@ def define_maker(part_list, params):
 
         certificate = params.certificate.get()
         private_key = params.private_key.get()
+        client_authentication = params.enable_client_authentication.get()
         if certificate and private_key:
             make_ssl_context = functools.partial(
                 http2.make_ssl_context,
                 certificate,
                 private_key,
+                client_authentication=client_authentication,
             )
         else:
             make_ssl_context = None
 
-        async def server(handler, *, logger=None):
-            return await garage.asyncs.utils.serve(
-                graceful_exit,
-                make_server_socket,
-                handler,
-                make_ssl_context=make_ssl_context,
-                logger=logger,
-            )
-
-        return server
+        return garage.asyncs.utils.serve(
+            graceful_exit,
+            make_server_socket,
+            handler,
+            make_ssl_context=make_ssl_context,
+            logger=logger,
+        )
 
     return make_server
