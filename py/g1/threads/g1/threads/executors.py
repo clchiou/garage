d89594644e5c3ad8b66206cc905ee2175656b39f
Che-Liang Chiou <clchiou@gmail.com>
2019-02-08 14:05:56 +0000

    Improve executor graceful shutdown

--- py/g1/threads/g1/threads/executors.py
+++ py/g1/threads/g1/threads/executors.py
@@ -6,8 +6,6 @@ import itertools
 import logging
 import os
 
-from g1.bases import timers
-from g1.bases.assertions import ASSERT
 from g1.threads import actors
 from g1.threads import futures
 from g1.threads import queues
@@ -21,19 +19,18 @@ class Executor:
 
     def __init__(
         self,
-        max_executors=None,
+        max_executors=0,
         *,
-        name_prefix=None,
+        name_prefix='',
         daemon=None,
     ):
 
-        if max_executors is None:
+        if max_executors <= 0:
             # Use this because Executor is often used to parallelize I/O
             # instead of computationally-heavy tasks.
-            max_executors = (os.cpu_count() or 1) * 8
-        ASSERT.greater(max_executors, 0)
+            max_executors = max(os.cpu_count(), 1) * 8
 
-        if name_prefix is None:
+        if not name_prefix:
             names = (
                 'executor-%02d' % self._COUNTER()
                 for _ in range(max_executors)
@@ -56,8 +53,8 @@ class Executor:
     def __enter__(self):
         return self
 
-    def __exit__(self, *_):
-        self.shutdown()
+    def __exit__(self, exc_type, *_):
+        self.shutdown(graceful=not exc_type)
 
     def submit(self, func, *args, **kwargs):
         future = futures.Future()
@@ -71,9 +68,17 @@ class Executor:
         items = self.queue.close(graceful)
         if items:
             LOG.warning('drop %d tasks', len(items))
-        timer = timers.make(timeout)
-        for stub in self.stubs:
-            exc = stub.future.get_exception(timer.get_timeout())
-            if exc:
-                LOG.error('actor crash: %r', stub, exc_info=exc)
+        if graceful:
+            self._join(timeout)
         return items
+
+    def _join(self, timeout):
+        stubs = {stub.future: stub for stub in self.stubs}
+        queue = futures.CompletionQueue(stubs)
+        queue.close()
+        for f in queue.as_completed(timeout):
+            exc = f.get_exception()
+            if exc:
+                LOG.error('executor crash: %r', stubs[f], exc_info=exc)
+        if queue:
+            LOG.warning('not join %d executor', len(queue))
--- py/g1/threads/tests/test_executors.py
+++ py/g1/threads/tests/test_executors.py
@@ -1,5 +1,7 @@
 import unittest
 
+import threading
+
 from g1.threads import executors
 from g1.threads import queues
 
@@ -17,6 +19,101 @@ class ExecutorsTest(unittest.TestCase):
             executor.submit(inc, 1)
         for stub in executor.stubs:
             self.assertTrue(stub.future.is_completed())
+
+    def test_shutdown_graceful(self):
+        executor = executors.Executor(4)
+        event1 = threading.Event()
+        event2 = threading.Event()
+        try:
+
+            start_barrier = threading.Barrier(3)
+
+            def func():
+                start_barrier.wait()
+                event1.wait()
+
+            f1 = executor.submit(func)
+            f2 = executor.submit(func)
+            f3 = executor.submit(event2.wait)
+
+            start_barrier.wait()
+
+            for stub in executor.stubs:
+                self.assertFalse(stub.future.is_completed())
+
+            event2.set()
+            self.assertTrue(f3.get_result(timeout=1))
+
+            with self.assertLogs(executors.__name__) as cm:
+                items = executor.shutdown(graceful=True, timeout=0.001)
+
+            self.assertEqual(len(cm.output), 1)
+            self.assertRegex(cm.output[0], r'not join 2 executor')
+
+            self.assertFalse(f1.is_completed())
+            self.assertFalse(f2.is_completed())
+            self.assertEqual(items, [])
+
+            counts = {True: 0, False: 0}
+            for stub in executor.stubs:
+                counts[stub.future.is_completed()] += 1
+            self.assertEqual(counts, {True: 2, False: 2})
+
+            event1.set()
+
+            self.assertIsNone(f1.get_result(timeout=1))
+            self.assertIsNone(f2.get_result(timeout=1))
+
+            for stub in executor.stubs:
+                self.assertTrue(stub.future.is_completed())
+
+        finally:
+            event1.set()
+            event2.set()
+            executor.shutdown()
+
+    def test_shutdown_not_graceful(self):
+        executor = executors.Executor(2)
+        event = threading.Event()
+        try:
+
+            start_barrier = threading.Barrier(3)
+
+            def func():
+                start_barrier.wait()
+                event.wait()
+
+            f1 = executor.submit(func)
+            f2 = executor.submit(func)
+            f3 = executor.submit(event.wait)
+
+            start_barrier.wait()
+
+            for stub in executor.stubs:
+                self.assertFalse(stub.future.is_completed())
+
+            with self.assertLogs(executors.__name__) as cm:
+                items = executor.shutdown(graceful=False)
+
+            self.assertEqual(len(cm.output), 1)
+            self.assertRegex(cm.output[0], r'drop 1 tasks')
+
+            self.assertFalse(f1.is_completed())
+            self.assertFalse(f2.is_completed())
+            self.assertFalse(f3.is_completed())
+            self.assertEqual([m.future for m in items], [f3])
+
+            event.set()
+
+            self.assertIsNone(f1.get_result(timeout=1))
+            self.assertIsNone(f2.get_result(timeout=1))
+
+            for stub in executor.stubs:
+                self.assertTrue(stub.future.is_completed())
+
+        finally:
+            event.set()
+            executor.shutdown()
 
 
 def inc(x):
