7d6eaa2dfe5b7c20345ab905b587a73d13bf84a6
Che-Liang Chiou <clchiou@gmail.com>
2019-02-14 04:38:56 +0000

    Add define_executor for convenience

--- py/g1/threads/g1/threads/parts.py
+++ py/g1/threads/g1/threads/parts.py
@@ -1,6 +1,38 @@
 from g1.apps import bases
+from g1.apps import labels
 from g1.apps import parameters
+from g1.apps import utils
 from g1.threads import executors
+
+
+def define_executor(module_path=None, **kwargs):
+    """Define an executor under ``module_path``."""
+
+    module_path = module_path or executors.__name__
+
+    module_labels = labels.make_labels(
+        module_path,
+        'executor_params',
+        'executor',
+    )
+
+    utils.depend_parameter_for(
+        module_labels.executor_params,
+        parameters.define(
+            module_path,
+            make_executor_params(**kwargs),
+        ),
+    )
+
+    utils.define_maker(
+        make_executor,
+        {
+            'params': module_labels.executor_params,
+            'return': module_labels.executor,
+        },
+    )
+
+    return module_labels
 
 
 def make_executor_params(
