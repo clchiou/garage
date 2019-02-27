cf19536e080f3e0c3f3cd1f146edae24a6ece397
Che-Liang Chiou <clchiou@gmail.com>
2019-02-27 08:33:08 +0000

    Add g1.bases.classes.{nondata_property,memorizing_property}

--- py/g1/bases/g1/bases/classes.py
+++ py/g1/bases/g1/bases/classes.py
@@ -2,7 +2,13 @@
 
 __all__ = [
     'SingletonMeta',
+    'memorizing_property',
+    'nondata_property',
 ]
+
+import functools
+
+from .assertions import ASSERT
 
 
 class SingletonMeta(type):
@@ -15,3 +21,41 @@ class SingletonMeta(type):
         except AttributeError:
             instance = cls.__instance = super().__call__(*args, **kwargs)
         return instance
+
+
+class nondata_property:
+    """Non-data descriptor variant of property.
+
+    See: https://docs.python.org/3/howto/descriptor.html#descriptor-protocol
+    """
+
+    def __init__(self, fget=None, doc=None):
+        self.fget = fget
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, type=None):  # pylint: disable=redefined-builtin
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError('unreadable attribute')
+        return self.fget(obj)
+
+
+def memorizing_property(func):
+    """Non-data property decorator that memorizes its value.
+
+    It stores the value to the instance by function's name; so ``func``
+    cannot be a nameless entity such as a lambda function.
+    """
+    ASSERT.not_equal(func.__name__, '<lambda>')
+
+    @nondata_property
+    @functools.wraps(func)
+    def wrapper(self):
+        value = func(self)
+        setattr(self, func.__name__, value)
+        return value
+
+    return wrapper
--- py/g1/bases/tests/test_classes.py
+++ py/g1/bases/tests/test_classes.py
@@ -22,5 +22,98 @@ class SingletonMetaTest(unittest.TestCase):
         self.assertEqual(xs, [1])
 
 
+class NondataPropertyTest(unittest.TestCase):
+
+    def test_nondata_property(self):
+
+        class Test:
+
+            @classes.nondata_property
+            def nondata(self):  # pylint: disable=no-self-use
+                """Doc string."""
+                return 1
+
+            @property
+            def data(self):
+                return 2
+
+        self.assertEqual(Test.nondata.__doc__, 'Doc string.')
+
+        t0 = Test()
+        self.assertEqual(t0.nondata, 1)
+        self.assertEqual(t0.data, 2)
+
+        t0.nondata = 3
+        with self.assertRaises(AttributeError):
+            t0.data = 4
+        self.assertEqual(t0.nondata, 3)
+        self.assertEqual(t0.data, 2)
+
+        # Not affected by t0.
+        t1 = Test()
+        self.assertEqual(t1.nondata, 1)
+        self.assertEqual(t1.data, 2)
+
+    def test_memorizing_property(self):
+
+        called = []
+
+        class Test:
+
+            def __init__(self, x):
+                self.x = x
+
+            @classes.memorizing_property
+            def p0(self):
+                """Doc string."""
+                called.append(('p0', self.x))
+                return self.x + 1
+
+            @classes.memorizing_property
+            def p1(self):
+                called.append(('p1', self.x))
+                return self.x + 2
+
+        self.assertEqual(Test.p0.__doc__, 'Doc string.')
+
+        t0 = Test(0)
+        t1 = Test(1)
+        self.assertEqual(called, [])
+
+        for _ in range(10):
+            self.assertEqual(t0.p0, 1)
+            self.assertEqual(called, [('p0', 0)])
+        self.assertIn('p0', t0.__dict__)
+        self.assertNotIn('p1', t0.__dict__)
+        self.assertNotIn('p0', t1.__dict__)
+        self.assertNotIn('p1', t1.__dict__)
+
+        for _ in range(10):
+            self.assertEqual(t0.p1, 2)
+            self.assertEqual(called, [('p0', 0), ('p1', 0)])
+        self.assertIn('p0', t0.__dict__)
+        self.assertIn('p1', t0.__dict__)
+        self.assertNotIn('p0', t1.__dict__)
+        self.assertNotIn('p1', t1.__dict__)
+
+        self.assertEqual(t0.p0, 1)
+        self.assertEqual(t0.p1, 2)
+        self.assertEqual(called, [('p0', 0), ('p1', 0)])
+
+        self.assertEqual(called, [('p0', 0), ('p1', 0)])
+        for _ in range(10):
+            self.assertEqual(t1.p0, 2)
+            self.assertEqual(called, [('p0', 0), ('p1', 0), ('p0', 1)])
+        for _ in range(10):
+            self.assertEqual(t1.p1, 3)
+            self.assertEqual(
+                called, [('p0', 0), ('p1', 0), ('p0', 1), ('p1', 1)]
+            )
+        self.assertIn('p0', t0.__dict__)
+        self.assertIn('p1', t0.__dict__)
+        self.assertIn('p0', t1.__dict__)
+        self.assertIn('p1', t1.__dict__)
+
+
 if __name__ == '__main__':
     unittest.main()
