52592e505843cb4b7c140b4728b92366f691c054
Che-Liang Chiou <clchiou@gmail.com>
2019-09-12 08:11:10 +0000

    Add script to bundle ctr into a zipapp

--- py/g1/containers/setup.py
+++ py/g1/containers/setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+from g1.devtools import buildtools
+
 setup(
     name='g1.containers',
     entry_points={
         'console_scripts': [
             'ctr = g1.containers.apps:run',
         ],
+    },
+    cmdclass={
+        'bdist_zipapp':
+        buildtools.make_bdist_zipapp(main='g1.containers.apps:run'),
     },
     packages=[
         'g1.containers',
---
+++ shipyard2/scripts/make-ctr-zipapp.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Bundle together ``ctr`` and its dependencies in one zipapp.
+#
+# TODO: Eventually we should migrate this process to a build rule.
+#
+
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+if [[ "${#}" -lt 1 ]]; then
+  show "usage: ${PROG} [PYTHON3] OUTPUT"
+  exit 1
+fi
+
+# Add buildtools to PYTHONPATH.
+PYTHONPATH="${PYTHONPATH:-}${PYTHONPATH:+:}${ROOT}/py/g1/devtools/buildtools"
+# TODO: Remove this once we fully migrated to g1.devtools.buildtools.
+PYTHONPATH="${PYTHONPATH}:${ROOT}/py/buildtools"
+export PYTHONPATH
+
+if [[ "${#}" -gt 1 ]]; then
+  readonly PYTHON3="${1}"
+  readonly USE_PYTHON3=(--python "${PYTHON3}")
+  shift
+else
+  readonly PYTHON3=python3
+  readonly USE_PYTHON3=()
+fi
+
+readonly OUTPUT="$(realpath "${1}")"
+[[ -e "${OUTPUT}" ]] && abort "refuse to overwrite: ${OUTPUT}"
+
+package() {
+  show "goto: ${1}"
+  cd "${ROOT}/py/${1}"
+  rm -rf build  # Clean up previous build.
+  trace_exec "${PYTHON3}" setup.py \
+    build \
+    bdist_zipapp "${USE_PYTHON3[@]}" --output "${OUTPUT}"
+  rm -rf build
+}
+
+package "g1/apps"
+package "g1/bases"
+package "g1/containers"
+package "startup"
