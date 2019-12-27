9ce60512a04192348cac5bcccf0bc91c11689e85
Che-Liang Chiou <clchiou@gmail.com>
2019-12-27 13:30:00 +0000

    Prototype shipyard2 build script

---
+++ shipyard2/scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+# XXX Because sudo does not find venv python3, as a workaround, we
+# export all of build's dependencies (we could make sudo find venv
+# python3, but I think it is better to let sudo find distro python3).
+DEPS=(
+  "${ROOT}/py/foreman"
+  "${ROOT}/py/g1/apps"
+  "${ROOT}/py/g1/bases"
+  "${ROOT}/py/g1/containers"
+  "${ROOT}/py/startup"
+)
+for dep in "${DEPS[@]}"; do
+  PYTHONPATH="${PYTHONPATH:-}${PYTHONPATH:+:}${dep}"
+done
+export PYTHONPATH
+
+exec sudo --preserve-env=PYTHONPATH "${HERE}/build" "${@}"
