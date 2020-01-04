b4ac6aa83a897d335af38c1a72fae6d99f5db4f1
Che-Liang Chiou <clchiou@gmail.com>
2020-01-04 12:51:07 +0000

    Add //third-party/nng

---
+++ shipyard2/rules/third-party/nng/build.py
@@ -0,0 +1,44 @@
+"""Build nng from source."""
+
+import logging
+
+import foreman
+
+from g1 import scripts
+
+import shipyard2.rules.bases
+
+LOG = logging.getLogger(__name__)
+
+shipyard2.rules.bases.define_git_repo(
+    'https://github.com/nanomsg/nng.git',
+    'v1.2.3',
+)
+
+shipyard2.rules.bases.define_distro_packages([
+    'cmake',
+    'gcc',
+    'g++',
+    'ninja-build',
+])
+
+
+@foreman.rule
+@foreman.rule.depend('//bases:build')
+@foreman.rule.depend('git-clone')
+@foreman.rule.depend('install')
+def build(parameters):
+    src_path = parameters['//bases:drydock'] / foreman.get_relpath()
+    build_dir_path = src_path / 'build'
+    if build_dir_path.exists():
+        LOG.info('skip: build nng')
+        return
+    LOG.info('build nng')
+    scripts.mkdir(build_dir_path)
+    with scripts.using_cwd(build_dir_path):
+        scripts.run(['cmake', '-G', 'Ninja', '..'])
+        scripts.run(['ninja'])
+        # Skip `ninja test` for now.
+        with scripts.using_sudo():
+            scripts.run(['ninja', 'install'])
+            scripts.run(['ldconfig'])
