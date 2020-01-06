c713aaa7bba90c7613ba84e967dfc224219abe70
Che-Liang Chiou <clchiou@gmail.com>
2020-01-06 07:47:19 +0000

    Comment on container process model

--- py/g1/containers/g1/containers/__init__.py
+++ py/g1/containers/g1/containers/__init__.py
@@ -35,6 +35,24 @@ The design borrows many ideas from rkt, but should be simpler than rkt
 because we do not intend to make our container as general-purposed and
 versatile as rkt.  However, due to lack of expertise and development
 time, our container will be less secure than Docker or rkt.
+
+NOTE: Our container process model is a mix of Docker's and rkt's:
+
+* Docker does not have a notion of pods; so we compare Docker containers
+  with our pods.
+
+* A Docker container consists of layers of images, which are merged into
+  one overlay filesystem, and all processes are run inside the same
+  container; our pods follow the same design with Docker in this aspect.
+
+* In Docker you need to launch and monitor processes yourself; whereas
+  our pods integrate with systemd, and will do this for you.
+
+* Both rkt pods and our pods integrate with systemd.
+
+* While a rkt pod also has multiple images, these images are not merged
+  into an overlay filesystem; instead, each image and its processes are
+  inside separate containers.  Our pods differ from rkt in this aspect.
 """
 
 import logging
