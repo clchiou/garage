b1892a93a8efc0b67451318d8b406cbd12599007
Che-Liang Chiou <clchiou@gmail.com>
2020-01-09 16:42:35 +0000

    Correct comments on CTR_PARAMS_PATH workaround

--- shipyard2/scripts/builder.sh
+++ shipyard2/scripts/builder.sh
@@ -2,12 +2,12 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-# XXX We require BUILDER_PARAMS_PATH to work around this issue:
-# builder and ctr need the same set of parameters to work together
-# correctly.  We decide to use a common parameter file since passing
-# around `--parameter` command-line flags is not quite maintainable.
-[[ -n "${BUILDER_PARAMS_PATH:-}" ]] || abort "expect BUILDER_PARAMS_PATH"
-ensure_file "${BUILDER_PARAMS_PATH}"
+# XXX We require CTR_PARAMS_PATH to work around this issue: builder and
+# ctr need the same set of parameters (because we make builder import
+# g1.containers codes).  We decide to use a common parameter file since
+# tossing around `--parameter` command-line flags is not maintainable.
+[[ -n "${CTR_PARAMS_PATH:-}" ]] || abort "expect CTR_PARAMS_PATH"
+ensure_file "${CTR_PARAMS_PATH}"
 
 # XXX Because sudo does not find venv python3, as a workaround, we
 # export all of builder's dependencies (we could make sudo find venv
@@ -26,8 +26,9 @@ for dep in "${DEPS[@]}"; do
 done
 export PYTHONPATH
 
-# XXX Preserve BUILDER_PARAMS_PATH because builder calls ctr.
-exec sudo --preserve-env=BUILDER_PARAMS_PATH,PYTHONPATH \
+# XXX Preserve CTR_PARAMS_PATH because builder does not just import
+# g1.containers, but also subprocess ctr.
+exec sudo --preserve-env=CTR_PARAMS_PATH,PYTHONPATH \
   python3 -m shipyard2.builders \
-  --parameter-file "${BUILDER_PARAMS_PATH#*.}" "${BUILDER_PARAMS_PATH}" \
+  --parameter-file "${CTR_PARAMS_PATH#*.}" "${CTR_PARAMS_PATH}" \
   "${@}"
--- shipyard2/scripts/ctr.sh
+++ shipyard2/scripts/ctr.sh
@@ -2,12 +2,12 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-# XXX We require BUILDER_PARAMS_PATH to work around this issue:
-# builder and ctr need the same set of parameters to work together
-# correctly.  We decide to use a common parameter file since passing
-# around `--parameter` command-line flags is not quite maintainable.
-[[ -n "${BUILDER_PARAMS_PATH:-}" ]] || abort "expect BUILDER_PARAMS_PATH"
-ensure_file "${BUILDER_PARAMS_PATH}"
+# XXX We require CTR_PARAMS_PATH to work around this issue: builder and
+# ctr need the same set of parameters (because we make builder import
+# g1.containers codes).  We decide to use a common parameter file since
+# tossing around `--parameter` command-line flags is not maintainable.
+[[ -n "${CTR_PARAMS_PATH:-}" ]] || abort "expect CTR_PARAMS_PATH"
+ensure_file "${CTR_PARAMS_PATH}"
 
 # XXX Because sudo does not find venv python3, as a workaround, we
 # export all of ctr's dependencies (we could make sudo find venv
@@ -26,5 +26,5 @@ export PYTHONPATH
 
 exec sudo --preserve-env=PYTHONPATH \
   python3 -m g1.containers \
-  --parameter-file "${BUILDER_PARAMS_PATH#*.}" "${BUILDER_PARAMS_PATH}" \
+  --parameter-file "${CTR_PARAMS_PATH#*.}" "${CTR_PARAMS_PATH}" \
   "${@}"
