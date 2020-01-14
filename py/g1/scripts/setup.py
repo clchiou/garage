b983b6955c98b6aa43662a6ee8b1e0d756855a03
Che-Liang Chiou <clchiou@gmail.com>
2020-01-14 13:42:46 +0000

    Fix missing build-time dependency for make-ctr-zipapp.sh

--- py/g1/scripts/setup.py
+++ py/g1/scripts/setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+from g1.devtools import buildtools
+
 setup(
     name='g1.scripts',
+    cmdclass={
+        'bdist_zipapp': buildtools.make_bdist_zipapp(main_optional=True),
+    },
     packages=[
         'g1.scripts',
     ],
--- shipyard2/rules/py/g1/scripts/build.py
+++ shipyard2/rules/py/g1/scripts/build.py
@@ -1,6 +1,9 @@
 import shipyard2.rules.pythons
 
 shipyard2.rules.pythons.define_package(
+    host_deps=[
+        '//py/g1/devtools/buildtools:build',
+    ],
     deps=[
         '//py/g1/bases:build',
     ],
