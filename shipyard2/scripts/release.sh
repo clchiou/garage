723a22b4597e91d322fe295233034ab8d5f128e4
Che-Liang Chiou <clchiou@gmail.com>
2020-01-22 10:38:21 +0000

    Tweak PYTHONPATH in scripts/release.sh

--- shipyard2/scripts/release.sh
+++ shipyard2/scripts/release.sh
@@ -5,9 +5,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 readonly DEPS=(
   "$(realpath "${HERE}/..")"  # shipyard2.
   "${ROOT}/py/foreman"
-  "${ROOT}/py/g1/bases"
-  "${ROOT}/py/g1/scripts"
-  "${ROOT}/py/startup"
 )
 for dep in "${DEPS[@]}"; do
   PYTHONPATH="${PYTHONPATH:-}${PYTHONPATH:+:}${dep}"
