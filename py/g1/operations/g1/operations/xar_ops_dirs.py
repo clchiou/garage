53204e11e102e40b9a665e88028479495d33ece7
Che-Liang Chiou <clchiou@gmail.com>
2020-01-28 08:52:26 +0000

    Prototype g1.operations.xar_ops_dirs

---
+++ py/g1/operations/g1/operations/xar_ops_dirs.py
@@ -0,0 +1,203 @@
+__all__ = [
+    'XarBundleDir',
+    'make_xar_ops_dirs',
+]
+
+import logging
+from pathlib import Path
+
+import g1.files
+from g1.bases import classes
+from g1.bases.assertions import ASSERT
+from g1.containers import scripts as ctr_scripts
+from g1.texts import jsons
+
+from . import bases
+from . import models
+from . import repos
+
+LOG = logging.getLogger(__name__)
+
+
+class XarBundleDir(repos.BundleDirInterface):
+
+    def __init__(self, path):
+        self.path_unchecked = path
+
+    def _check(self):
+        ASSERT.predicate(self.path_unchecked, Path.is_dir)
+        deploy_instruction = jsons.load_dataobject(
+            models.XarDeployInstruction,
+            ASSERT.predicate(
+                self.path_unchecked / \
+                models.BUNDLE_DEPLOY_INSTRUCTION_FILENAME,
+                Path.is_file,
+            )
+        )
+        if deploy_instruction.is_zipapp():
+            ASSERT.predicate(
+                self.path_unchecked / models.XAR_BUNDLE_ZIPAPP_FILENAME,
+                Path.is_file,
+            )
+        else:
+            ASSERT.predicate(
+                self.path_unchecked / models.XAR_BUNDLE_IMAGE_FILENAME,
+                Path.is_file,
+            )
+        return deploy_instruction
+
+    def check(self):
+        self._check()
+
+    # XXX: This annotation works around pylint no-member false errors.
+    deploy_instruction: models.XarDeployInstruction
+
+    @classes.memorizing_property
+    def deploy_instruction(self):  # pylint: disable=function-redefined
+        return self._check()
+
+    @property
+    def name(self):
+        return self.deploy_instruction.name
+
+    @property
+    def version(self):
+        return self.deploy_instruction.version
+
+    @property
+    def zipapp_path(self):
+        ASSERT.true(self.deploy_instruction.is_zipapp())
+        return self.path / models.XAR_BUNDLE_ZIPAPP_FILENAME
+
+    @property
+    def image_path(self):
+        ASSERT.false(self.deploy_instruction.is_zipapp())
+        return self.path / models.XAR_BUNDLE_IMAGE_FILENAME
+
+    def install(self):
+        if self.deploy_instruction.is_zipapp():
+            LOG.info('xars install zipapp: %s %s', self.name, self.version)
+            bases.copy_exec(
+                self.zipapp_path,
+                bases.get_zipapp_target_path(self.deploy_instruction.name),
+            )
+        else:
+            LOG.info('xars install xar: %s %s', self.name, self.version)
+            ctr_scripts.ctr_import_image(self.image_path)
+            ctr_scripts.ctr_install_xar(
+                self.deploy_instruction.name,
+                self.deploy_instruction.exec_relpath,
+                self.deploy_instruction.image,
+            )
+        return True
+
+    def uninstall(self):
+        if self.deploy_instruction.is_zipapp():
+            LOG.info('xars uninstall zipapp: %s %s', self.name, self.version)
+            path = bases.get_zipapp_target_path(self.deploy_instruction.name)
+            if path.exists():
+                path.unlink()
+        else:
+            LOG.info('xars uninstall xar: %s %s', self.name, self.version)
+            ctr_scripts.ctr_uninstall_xar(self.deploy_instruction.name)
+            ctr_scripts.ctr_remove_image(self.deploy_instruction.image)
+        return True
+
+
+class XarOpsDir(repos.OpsDirInterface):
+
+    @staticmethod
+    def _load_metadata(xar_ops_dir_path):
+        return jsons.load_dataobject(
+            models.XarMetadata,
+            xar_ops_dir_path / models.OPS_DIR_METADATA_FILENAME,
+        )
+
+    def __init__(self, path):
+        self.path_unchecked = path
+
+    def init(self):
+        bases.make_dir(self.path_unchecked)
+
+    def check(self):
+        ASSERT.predicate(self.path_unchecked, Path.is_dir)
+
+    # XXX: This annotation works around pylint no-member false errors.
+    _metadata_path: Path
+
+    @classes.memorizing_property
+    def _metadata_path(self):  # pylint: disable=function-redefined
+        return self.path / models.OPS_DIR_METADATA_FILENAME
+
+    # XXX: This annotation works around pylint no-member false errors.
+    metadata: models.XarMetadata
+
+    @classes.memorizing_property
+    def metadata(self):  # pylint: disable=function-redefined
+        return self._load_metadata(self.path)
+
+    @property
+    def zipapp_target_path(self):
+        ASSERT.true(self.metadata.is_zipapp())
+        return bases.get_zipapp_target_path(self.metadata.name)
+
+    def cleanup(self):
+        if self._metadata_path.exists():
+            self._metadata_path.unlink()
+        ASSERT.predicate(self.path, g1.files.is_empty_dir)
+
+    def check_invariants(self, active_ops_dirs):
+        self.check()
+        ASSERT.predicate(self._metadata_path, Path.is_file)
+        for ops_dir in active_ops_dirs:
+            metadata = self._load_metadata(ops_dir.path)
+            ASSERT(
+                metadata.name != self.metadata.name,
+                'expect unique xar name: {}',
+                self.metadata.name,
+            )
+
+    @staticmethod
+    def get_ops_dir_name(name, version):
+        return '%s_%s' % (name, version)
+
+    def init_from_bundle_dir(self, bundle_dir):
+        deploy_instruction = bundle_dir.deploy_instruction
+        jsons.dump_dataobject(
+            models.XarMetadata(
+                name=deploy_instruction.name,
+                version=deploy_instruction.version,
+                image=deploy_instruction.image,
+            ),
+            self._metadata_path,
+        )
+        bases.set_file_attrs(self._metadata_path)
+
+    def activate(self):
+        pass  # Nothing here.
+
+    def deactivate(self):
+        pass  # Nothing here.
+
+    def uninstall(self):
+        name = self.metadata.name
+        version = self.metadata.version
+        if self.metadata.is_zipapp():
+            LOG.info('xars uninstall zipapp: %s %s', name, version)
+            path = bases.get_zipapp_target_path(name)
+            if path.exists():
+                path.unlink()
+        else:
+            LOG.info('xars uninstall xar: %s %s', name, version)
+            ctr_scripts.ctr_uninstall_xar(name)
+            ctr_scripts.ctr_remove_image(self.metadata.image)
+        return True
+
+
+def make_xar_ops_dirs():
+    return repos.OpsDirs(
+        models.REPO_XARS_DIR_NAME,
+        bases.get_repo_path() / models.REPO_XARS_DIR_NAME,
+        bundle_dir_type=XarBundleDir,
+        ops_dir_type=XarOpsDir,
+    )
---
+++ py/g1/operations/tests/test_xar_ops_dirs.py
@@ -0,0 +1,277 @@
+import unittest
+import unittest.mock
+
+import g1.files
+from g1.containers import models as ctr_models
+from g1.operations import models
+from g1.operations import xar_ops_dirs
+from g1.texts import jsons
+
+from tests import fixtures
+
+
+class TestCaseBase(fixtures.TestCaseBase):
+
+    XAR_DEPLOY_INSTRUCTION = models.XarDeployInstruction(
+        name='dummy-xar',
+        version='0.0.1',
+        exec_relpath='usr/local/bin/dummy-xar',
+        image=ctr_models.PodConfig.Image(tag='some-tag'),
+    )
+
+    XAR_METADATA = models.XarMetadata(
+        name=XAR_DEPLOY_INSTRUCTION.name,
+        version=XAR_DEPLOY_INSTRUCTION.version,
+        image=XAR_DEPLOY_INSTRUCTION.image,
+    )
+
+    ZIPAPP_DEPLOY_INSTRUCTION = models.XarDeployInstruction(
+        name='dummy-zipapp',
+        version='0.0.2',
+        exec_relpath=None,
+        image=None,
+    )
+
+    ZIPAPP_METADATA = models.XarMetadata(
+        name=ZIPAPP_DEPLOY_INSTRUCTION.name,
+        version=ZIPAPP_DEPLOY_INSTRUCTION.version,
+        image=ZIPAPP_DEPLOY_INSTRUCTION.image,
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.ctr_scripts_mock = unittest.mock.patch(
+            xar_ops_dirs.__name__ + '.ctr_scripts'
+        ).start()
+
+    def tearDown(self):
+        unittest.mock.patch.stopall()
+        super().tearDown()
+
+    def make_xar_bundle(self):
+        self.test_bundle_dir_path.mkdir()
+        jsons.dump_dataobject(
+            self.XAR_DEPLOY_INSTRUCTION,
+            self.test_bundle_dir_path / \
+            models.BUNDLE_DEPLOY_INSTRUCTION_FILENAME,
+        )
+        (self.test_bundle_dir_path / models.XAR_BUNDLE_IMAGE_FILENAME).touch()
+        bundle_dir = xar_ops_dirs.XarBundleDir(self.test_bundle_dir_path)
+        bundle_dir.check()
+        return bundle_dir
+
+    def make_zipapp_bundle(self):
+        self.test_bundle_dir_path.mkdir()
+        jsons.dump_dataobject(
+            self.ZIPAPP_DEPLOY_INSTRUCTION,
+            self.test_bundle_dir_path / \
+            models.BUNDLE_DEPLOY_INSTRUCTION_FILENAME,
+        )
+        (self.test_bundle_dir_path / models.XAR_BUNDLE_ZIPAPP_FILENAME).touch()
+        bundle_dir = xar_ops_dirs.XarBundleDir(self.test_bundle_dir_path)
+        bundle_dir.check()
+        return bundle_dir
+
+
+class XarBundleDirTest(TestCaseBase):
+
+    def test_xar_bundle_dir(self):
+        bundle_dir = self.make_xar_bundle()
+        self.assertEqual(
+            bundle_dir.deploy_instruction,
+            self.XAR_DEPLOY_INSTRUCTION,
+        )
+        self.assertEqual(
+            bundle_dir.image_path,
+            self.test_bundle_dir_path / models.XAR_BUNDLE_IMAGE_FILENAME,
+        )
+        with self.assertRaises(AssertionError):
+            bundle_dir.zipapp_path  # pylint: disable=pointless-statement
+
+    def test_zipapp_bundle_dir(self):
+        bundle_dir = self.make_zipapp_bundle()
+        self.assertEqual(
+            bundle_dir.deploy_instruction,
+            self.ZIPAPP_DEPLOY_INSTRUCTION,
+        )
+        self.assertEqual(
+            bundle_dir.zipapp_path,
+            self.test_bundle_dir_path / models.XAR_BUNDLE_ZIPAPP_FILENAME,
+        )
+        with self.assertRaises(AssertionError):
+            bundle_dir.image_path  # pylint: disable=pointless-statement
+
+    def test_install_xar(self):
+        bundle_dir = self.make_xar_bundle()
+        bundle_dir.install()
+        self.ctr_scripts_mock.ctr_import_image.assert_called_once_with(
+            bundle_dir.image_path,
+        )
+        self.ctr_scripts_mock.ctr_install_xar.assert_called_once_with(
+            self.XAR_DEPLOY_INSTRUCTION.name,
+            self.XAR_DEPLOY_INSTRUCTION.exec_relpath,
+            self.XAR_DEPLOY_INSTRUCTION.image,
+        )
+
+    def test_install_zipapp(self):
+        self.assertTrue(g1.files.is_empty_dir(self.test_zipapp_dir_path))
+        bundle_dir = self.make_zipapp_bundle()
+        bundle_dir.install()
+        self.assertEqual(
+            self.list_dir(self.test_zipapp_dir_path),
+            [self.ZIPAPP_DEPLOY_INSTRUCTION.name],
+        )
+
+    def test_uninstall_xar(self):
+        bundle_dir = self.make_xar_bundle()
+        bundle_dir.uninstall()
+        self.ctr_scripts_mock.ctr_uninstall_xar.assert_called_once_with(
+            self.XAR_METADATA.name,
+        )
+        self.ctr_scripts_mock.ctr_remove_image.assert_called_once_with(
+            self.XAR_METADATA.image,
+        )
+
+    def test_uninstall_zipapp(self):
+        self.assertTrue(g1.files.is_empty_dir(self.test_zipapp_dir_path))
+        bundle_dir = self.make_zipapp_bundle()
+        bundle_dir.install()
+        self.assertEqual(
+            self.list_dir(self.test_zipapp_dir_path),
+            [self.ZIPAPP_DEPLOY_INSTRUCTION.name],
+        )
+        bundle_dir.uninstall()
+        self.assertTrue(g1.files.is_empty_dir(self.test_zipapp_dir_path))
+
+
+class XarOpsDirTest(TestCaseBase):
+
+    def make_ops_dir(self):
+        bundle_dir = xar_ops_dirs.XarBundleDir(self.test_bundle_dir_path)
+        bundle_dir.check()
+        ops_dir = xar_ops_dirs.XarOpsDir(self.test_ops_dir_path)
+        ops_dir.init()
+        ops_dir.check()
+        ops_dir.init_from_bundle_dir(bundle_dir)
+        return ops_dir
+
+    @property
+    def test_metadata_path(self):
+        return self.test_ops_dir_path / 'metadata'
+
+    def test_init(self):
+        ops_dir = xar_ops_dirs.XarOpsDir(self.test_ops_dir_path)
+        with self.assertRaises(AssertionError):
+            ops_dir.check()
+        ops_dir.init()
+        ops_dir.check()
+        self.assertTrue(g1.files.is_empty_dir(ops_dir.path))
+
+    def do_test_cleanup(self, ops_dir):
+        self.assertFalse(g1.files.is_empty_dir(self.test_ops_dir_path))
+        ops_dir.cleanup()
+        self.assertTrue(g1.files.is_empty_dir(self.test_ops_dir_path))
+
+    def test_cleanup_xar(self):
+        self.make_xar_bundle()
+        self.do_test_cleanup(self.make_ops_dir())
+
+    def test_cleanup_zipapp(self):
+        self.make_zipapp_bundle()
+        self.do_test_cleanup(self.make_ops_dir())
+
+    def do_test_init_from_bundle_dir(self):
+        ops_dir = self.make_ops_dir()
+        self.assertEqual(self.list_dir(self.test_ops_dir_path), ['metadata'])
+        self.assertTrue(self.test_metadata_path.is_file())
+        self.assertEqual(ops_dir._metadata_path, self.test_metadata_path)
+        return ops_dir
+
+    def test_init_from_bundle_dir_xar(self):
+        self.make_xar_bundle()
+        ops_dir = self.do_test_init_from_bundle_dir()
+        self.assertEqual(ops_dir.metadata, self.XAR_METADATA)
+        with self.assertRaises(AssertionError):
+            ops_dir.zipapp_target_path  # pylint: disable=pointless-statement
+
+    def test_init_from_bundle_dir_zipapp(self):
+        self.make_zipapp_bundle()
+        ops_dir = self.do_test_init_from_bundle_dir()
+        self.assertEqual(ops_dir.metadata, self.ZIPAPP_METADATA)
+        self.assertEqual(
+            ops_dir.zipapp_target_path,
+            self.test_zipapp_dir_path / self.ZIPAPP_METADATA.name,
+        )
+
+    def test_uninstall_xar(self):
+        self.make_xar_bundle()
+        ops_dir = self.make_ops_dir()
+        ops_dir.uninstall()
+        self.ctr_scripts_mock.ctr_uninstall_xar.assert_called_once_with(
+            self.XAR_METADATA.name,
+        )
+        self.ctr_scripts_mock.ctr_remove_image.assert_called_once_with(
+            self.XAR_METADATA.image,
+        )
+
+    def test_uninstall_zipapp(self):
+        self.assertTrue(g1.files.is_empty_dir(self.test_zipapp_dir_path))
+
+        bundle_dir = self.make_zipapp_bundle()
+        bundle_dir.install()
+        self.assertEqual(
+            self.list_dir(self.test_zipapp_dir_path),
+            [self.ZIPAPP_DEPLOY_INSTRUCTION.name],
+        )
+
+        ops_dir = self.make_ops_dir()
+        ops_dir.uninstall()
+        self.assertTrue(g1.files.is_empty_dir(self.test_zipapp_dir_path))
+
+
+class InvarianceTest(TestCaseBase):
+
+    XAR_DEPLOY_INSTRUCTION_2 = models.XarDeployInstruction(
+        name='dummy-xar',
+        version='0.0.2',
+        exec_relpath='usr/local/bin/dummy-xar',
+        image=ctr_models.PodConfig.Image(tag='some-tag'),
+    )
+
+    def test_check_invariants(self):
+        self.test_bundle_dir_path.mkdir()
+        b1_path = self.test_bundle_dir_path / 'bundle-1'
+        b2_path = self.test_bundle_dir_path / 'bundle-2'
+        for path, deploy_instruction in (
+            (b1_path, self.XAR_DEPLOY_INSTRUCTION),
+            (b2_path, self.XAR_DEPLOY_INSTRUCTION_2),
+        ):
+            path.mkdir()
+            jsons.dump_dataobject(
+                deploy_instruction,
+                path / models.BUNDLE_DEPLOY_INSTRUCTION_FILENAME,
+            )
+            (path / models.XAR_BUNDLE_IMAGE_FILENAME).touch()
+        b1 = xar_ops_dirs.XarBundleDir(b1_path)
+        b2 = xar_ops_dirs.XarBundleDir(b2_path)
+        (self.test_repo_path / 'v1' / 'xars').mkdir(parents=True)
+        ops_dirs = xar_ops_dirs.make_xar_ops_dirs()
+        ops_dirs.init()
+        with ops_dirs.listing_ops_dirs() as actual:
+            self.assertEqual(actual, [])
+        self.assertTrue(ops_dirs.install(b1))
+        with ops_dirs.listing_ops_dirs() as actual:
+            self.assertEqual(len(actual), 1)
+            self.assertEqual(actual[0].metadata, self.XAR_METADATA)
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'expect unique xar name: dummy-xar',
+        ):
+            ops_dirs.install(b2)
+        with ops_dirs.listing_ops_dirs() as actual:
+            self.assertEqual(len(actual), 1)
+            self.assertEqual(actual[0].metadata, self.XAR_METADATA)
+
+
+if __name__ == '__main__':
+    unittest.main()
