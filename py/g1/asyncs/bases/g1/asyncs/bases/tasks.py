27ae0a311b34fac5a3e5d1d20f400e2678c13c2a
Che-Liang Chiou <clchiou@gmail.com>
2020-03-03 10:18:24 +0000

    Rename log_on_error to log_error and privatize join_and_log_on_error

--- py/g1/asyncs/bases/g1/asyncs/bases/tasks.py
+++ py/g1/asyncs/bases/g1/asyncs/bases/tasks.py
@@ -8,7 +8,6 @@ __all__ = [
     'as_completed',
     'get_all_tasks',
     'get_current_task',
-    'join_and_log_on_error',
     'joining',
     'spawn',
     'spawn_onto_stack',
@@ -43,9 +42,9 @@ class CompletionQueue:
     implementation, and thus may be more efficient.
     """
 
-    def __init__(self, *, always_cancel=False, log_on_error=True):
+    def __init__(self, *, always_cancel=False, log_error=True):
         self._always_cancel = always_cancel
-        self._log_on_error = log_on_error
+        self._log_error = log_error
         self._gate = locks.Gate()
         self._completed = collections.deque()
         self._uncompleted = set()
@@ -72,10 +71,9 @@ class CompletionQueue:
             for task in tasks:
                 task.cancel()
         for task in tasks:
-            if self._log_on_error:
-                await join_and_log_on_error(task)
-            else:
-                await task.join()
+            await task.join()
+            if self._log_error:
+                _log_task_error(task)
 
     def is_closed(self):
         return self._closed
@@ -168,11 +166,11 @@ async def as_completed(tasks):
         num_tasks -= 1
 
 
-async def join_and_log_on_error(task):
-    exc = await task.get_exception()
+def _log_task_error(task):
+    exc = task.get_exception_nonblocking()
     if not exc:
-        pass
-    elif isinstance(exc, Cancelled):
+        return
+    if isinstance(exc, Cancelled):
         LOG.debug('task is cancelled: %r', task, exc_info=exc)
     else:
         LOG.error('task error: %r', task, exc_info=exc)
@@ -184,14 +182,14 @@ class joining:
     * If the code nested inside the context raises (including
       BaseException such as task cancellation) or always_cancel is set
       to True, the task is cancelled.
-    * Then the task is joined, and if log_on_error is set to True (which
-      is the default), task error is logged.
+    * Then the task is joined, and if log_error is set to True (which is
+      the default), task error is logged.
     """
 
-    def __init__(self, task, *, always_cancel=False, log_on_error=True):
+    def __init__(self, task, *, always_cancel=False, log_error=True):
         self._task = task
         self._always_cancel = always_cancel
-        self._log_on_error = log_on_error
+        self._log_error = log_error
 
     async def __aenter__(self):
         return self._task
@@ -199,10 +197,9 @@ class joining:
     async def __aexit__(self, exc_type, *_):
         if exc_type or self._always_cancel:
             self._task.cancel()
-        if self._log_on_error:
-            await join_and_log_on_error(self._task)
-        else:
-            await self._task.join()
+        await self._task.join()
+        if self._log_error:
+            _log_task_error(self._task)
 
 
 def spawn(awaitable):
