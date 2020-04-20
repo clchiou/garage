f1e6e61e5845693dc83131718fe4a9c77a60f346
Che-Liang Chiou <clchiou@gmail.com>
2020-04-20 05:35:32 +0000

    Break Client class into smaller classes for extending method interface

--- py/g1/messaging/g1/messaging/reqrep/clients.py
+++ py/g1/messaging/g1/messaging/reqrep/clients.py
@@ -15,14 +15,13 @@ from . import utils
 class Client:
 
     def __init__(self, request_type, response_type, wiredata):
-        self._request_type = request_type
-        self._response_type = response_type
-        self._wiredata = wiredata
-        # You may (optionally) use context to close socket on exit.
         self.socket = nng.asyncs.Socket(nng.Protocols.REQ0)
+        self.transceive = Transceiver(self.socket, response_type, wiredata)
         self.m = collections.Namespace(
-            **{name: _make_transceiver(self, name)
-               for name in request_type.m}
+            **{
+                name: Method(name, request_type, self.transceive)
+                for name in request_type.m
+            }
         )
 
     __repr__ = classes.make_repr('{self.socket!r}')
@@ -34,24 +33,35 @@ class Client:
     def __exit__(self, *args):
         return self.socket.__exit__(*args)
 
-    async def transceive(self, request):
-        with nng.asyncs.Context(ASSERT.not_none(self.socket)) as context:
+
+class Transceiver:
+
+    def __init__(self, socket, response_type, wiredata):
+        self._socket = socket
+        self._response_type = response_type
+        self._wiredata = wiredata
+
+    async def __call__(self, request):
+        with nng.asyncs.Context(ASSERT.not_none(self._socket)) as context:
             await context.send(self._wiredata.to_lower(request))
             wire_response = await context.recv()
             return self._wiredata.to_upper(self._response_type, wire_response)
 
 
-def _make_transceiver(self, name):
+class Method:
 
-    make = self._request_type.m[name]
+    def __init__(self, name, request_type, transceive):
+        self._name = name
+        self._request_type = request_type
+        self._transceive = transceive
 
-    async def transceive(**kwargs):
-        response = await self.transceive(
-            self._request_type(args=make(**kwargs))
+    async def __call__(self, **kwargs):
+        response = await self._transceive(
+            self._request_type(
+                args=self._request_type.m[self._name](**kwargs)
+            )
         )
         if response.error is not None:
             raise utils.select(response.error)[1]
         else:
-            return getattr(response.result, name)
-
-    return transceive
+            return getattr(response.result, self._name)
