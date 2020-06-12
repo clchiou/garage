c9112a624c634c284024dff0a7af2261ee56772d
Che-Liang Chiou <clchiou@gmail.com>
2020-06-12 09:07:40 +0000

    Add more timestamp helpers to g1.bases.datetimes

--- py/g1/bases/g1/bases/datetimes.py
+++ py/g1/bases/g1/bases/datetimes.py
@@ -1,9 +1,58 @@
+"""Extension of standard library's datetime.
+
+A common pitfall of using datetime to store a timestamps is not setting
+timezone to UTC, which is then default to the local timezone.  This
+produces wrong results when converting datetime-represented timestamp
+to/from a number-represented timestamp.  Specifically, when the local
+timezone is not UTC, ``datetime.fromtimestamp(0)`` does not return
+1970-01-01, and ``datetime(1970, 1, 1).timestamp()`` does not return 0.
+All timestamp helpers of this module will set timezone to UTC.
+"""
+
 __all__ = [
+    'fromisoformat',
+    'make_timestamp',
+    'timestamp_date',
     'utcfromtimestamp',
     'utcnow',
 ]
 
 import datetime
+
+
+def fromisoformat(string):
+    """Parse a timestamp with datetime.fromisoformat."""
+    timestamp = datetime.datetime.fromisoformat(string)
+    if timestamp.tzinfo is None:
+        return timestamp.replace(tzinfo=datetime.timezone.utc)
+    else:
+        return timestamp.astimezone(datetime.timezone.utc)
+
+
+def make_timestamp(
+    year, month, day, hour=0, minute=0, second=0, microsecond=0
+):
+    """Make a datetime-represented timestamp."""
+    return datetime.datetime(
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second,
+        microsecond,
+        datetime.timezone.utc,
+    )
+
+
+def timestamp_date(timestamp):
+    """Keep only the date part of a datetime-represented timestamp."""
+    return datetime.datetime(
+        year=timestamp.year,
+        month=timestamp.month,
+        day=timestamp.day,
+        tzinfo=datetime.timezone.utc,
+    )
 
 
 def utcfromtimestamp(timestamp):
@@ -21,4 +70,4 @@ def utcnow():
     Unlike stdlib's ``utcnow``, this also sets ``tzinfo`` to UTC;
     without this, ``timestamp()`` will return incorrect number.
     """
-    return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    return datetime.datetime.now(datetime.timezone.utc)
--- py/g1/bases/tests/test_datetimes.py
+++ py/g1/bases/tests/test_datetimes.py
@@ -7,8 +7,44 @@ from g1.bases import datetimes
 
 class DatetimesTest(unittest.TestCase):
 
+    def test_fromisoformat(self):
+        self.assertEqual(
+            datetimes.fromisoformat('2000-01-02T03:04:05'),
+            datetime.datetime(
+                2000, 1, 2, 3, 4, 5, tzinfo=datetime.timezone.utc
+            ),
+        )
+        self.assertEqual(
+            datetimes.fromisoformat('2000-01-01T08:00:00+08:00'),
+            datetime.datetime(
+                2000, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc
+            ),
+        )
+
+    def test_make_timestamp(self):
+        dt = datetimes.make_timestamp(1970, 1, 1, 2, 3, 4)
+        self.assertEqual(dt, datetimes.utcfromtimestamp(7200 + 180 + 4))
+        self.assertEqual(
+            dt,
+            datetime.datetime(
+                1970, 1, 1, 2, 3, 4, tzinfo=datetime.timezone.utc
+            ),
+        )
+
+    def test_timestamp_date(self):
+        dt = datetimes.utcfromtimestamp(7200 + 180 + 4)
+        self.assertEqual(
+            dt.replace(tzinfo=None), datetime.datetime(1970, 1, 1, 2, 3, 4)
+        )
+        self.assertEqual(dt.timestamp(), 7200 + 180 + 4)
+        dt = datetimes.timestamp_date(dt)
+        self.assertEqual(
+            dt.replace(tzinfo=None), datetime.datetime(1970, 1, 1, 0, 0, 0)
+        )
+        self.assertEqual(dt.timestamp(), 0)
+
     def test_utcfromtimestamp(self):
-        zero = datetime.datetime(1970, 1, 1, 0, 0)
+        zero = datetime.datetime(1970, 1, 1)
         dt = datetimes.utcfromtimestamp(0)
         self.assertEqual(zero, dt.replace(tzinfo=None))
         self.assertEqual(0, dt.timestamp())
