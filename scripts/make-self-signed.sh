268570b47595545927790d473d73838d6e7a6f1d
Che-Liang Chiou <clchiou@gmail.com>
2020-10-16 23:40:50 +0000

    Make make-self-signed.sh to generate DH parameters file and PEM file

--- scripts/make-self-signed.sh
+++ scripts/make-self-signed.sh
@@ -4,16 +4,20 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-if [[ "${#}" -ne 2 ]]; then
-  echo "usage: ${PROG} output_key output_crt"
+if [[ "${#}" -ne 4 ]]; then
+  echo "usage: ${PROG} output_key output_crt output_dh output_pem"
   exit 1
 fi
 
 readonly OUTPUT_KEY="${1}"
 readonly OUTPUT_CRT="${2}"
+readonly OUTPUT_DH="${3}"
+readonly OUTPUT_PEM="${4}"
 
 [[ ! -e "${OUTPUT_KEY}" ]] || abort "refuse to overwrite: ${OUTPUT_KEY}"
 [[ ! -e "${OUTPUT_CRT}" ]] || abort "refuse to overwrite: ${OUTPUT_CRT}"
+[[ ! -e "${OUTPUT_DH}" ]] || abort "refuse to overwrite: ${OUTPUT_DH}"
+[[ ! -e "${OUTPUT_PEM}" ]] || abort "refuse to overwrite: ${OUTPUT_PEM}"
 
 export TZ=UTC
 
@@ -31,3 +35,10 @@ trace_exec \
 
 trace_exec \
   openssl x509 -in "${OUTPUT_CRT}" -noout -text
+
+# Strangely, if you put `-out` before 4096, openssl will output to
+# stdout rather than the output file you specify.
+trace_exec \
+  openssl dhparam -out "${OUTPUT_DH}" 4096
+
+cat "${OUTPUT_CRT}" "${OUTPUT_KEY}" "${OUTPUT_DH}" > "${OUTPUT_PEM}"
