88bdaa6a595f162629e7eee5bc4fdcbdca68c47b
Che-Liang Chiou <clchiou@gmail.com>
2020-11-12 01:52:38 +0000

    Extend utils to accept connectable, not just Engine object

--- py/g1/databases/g1/databases/utils.py
+++ py/g1/databases/g1/databases/utils.py
@@ -6,26 +6,33 @@ __all__ = [
 
 import contextlib
 
+import sqlalchemy.engine
 from sqlalchemy.schema import CreateIndex
 
 from g1.bases.assertions import ASSERT
 
 
-def add_if_not_exists_clause(index, engine):
+def add_if_not_exists_clause(index, connectable):
     """Add "IF NOT EXISTS" clause to create index statement.
 
     I don't know why but ``sqlalchemy.Index.create()`` does not take a
     ``checkfirst`` argument like the rest of others.
     """
-    stmt = str(CreateIndex(index).compile(engine))
+    stmt = str(CreateIndex(index).compile(connectable))
     stmt = stmt.replace('CREATE INDEX', 'CREATE INDEX IF NOT EXISTS', 1)
     ASSERT.in_('IF NOT EXISTS', stmt)
     return stmt
 
 
 @contextlib.contextmanager
-def executing(engine, statement):
-    with engine.connect() as conn:
+def executing(connectable, statement):
+    if isinstance(connectable, sqlalchemy.engine.Connection):
+        # Do NOT close Connection object that caller passes to us.
+        ctx = contextlib.nullcontext(connectable)
+    else:
+        ctx = connectable.connect()
+    with ctx as conn:
+        # ResultProxy does not implement __enter__ and __exit__.
         result = conn.execute(statement)
         try:
             yield result
@@ -35,8 +42,8 @@ def executing(engine, statement):
 
 # I don't know why but SQLAlchemy only make this available in ORM, not
 # in core.
-def one_or_none(engine, statement):
-    with executing(engine, statement) as result:
+def one_or_none(connectable, statement):
+    with executing(connectable, statement) as result:
         row = result.fetchone()
         ASSERT.none(result.fetchone())
         return row
