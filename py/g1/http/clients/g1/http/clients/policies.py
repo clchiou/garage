fe4a3ecf3fc39ad3c298fb4dd1708b1ecbaaf924
Che-Liang Chiou <clchiou@gmail.com>
2021-04-10 07:05:18 +0000

    Prototype circuit breaker

--- py/g1/http/clients/g1/http/clients/policies.py
+++ py/g1/http/clients/g1/http/clients/policies.py
@@ -1,5 +1,8 @@
 __all__ = [
     'Unavailable',
+    # Circuit breaker.
+    'TristateBreakers',
+    'NO_BREAK',
     # Rate limit.
     'unlimited',
     'TokenBucket',
@@ -8,14 +11,234 @@ __all__ = [
     'ExponentialBackoff',
 ]
 
+import collections
+import enum
+import logging
 import time
 
+from g1.asyncs.bases import locks
 from g1.asyncs.bases import timers
+from g1.bases import collections as g1_collections
 from g1.bases.assertions import ASSERT
+
+LOG = logging.getLogger(__name__)
 
 
 class Unavailable(Exception):
-    """When rate limit is exceeded."""
+    """When rate limit is exceeded or circuit breaker disconnects."""
+
+
+class CircuitBreaker:
+
+    async def __aenter__(self):
+        raise NotImplementedError
+
+    async def __aexit__(self, exc_type, exc, traceback):
+        raise NotImplementedError
+
+    def notify_success(self):
+        raise NotImplementedError
+
+    def notify_failure(self):
+        raise NotImplementedError
+
+
+class CircuitBreakers:
+
+    def get(self, key: str) -> CircuitBreaker:
+        raise NotImplementedError
+
+
+@enum.unique
+class _States(enum.Enum):
+    GREEN = enum.auto()
+    YELLOW = enum.auto()
+    RED = enum.auto()
+
+
+class _EventLog:
+    """Record when events happened."""
+
+    def __init__(self, capacity):
+        self._log = collections.deque(maxlen=capacity)
+
+    def add(self, t):
+        if self._log:
+            ASSERT.greater(t, self._log[-1])
+        self._log.append(t)
+
+    def count(self, t0=None):
+        """Return number of events after ``t0``."""
+        if t0 is None:
+            return len(self._log)
+        for i, t in enumerate(self._log):
+            if t >= t0:
+                return len(self._log) - i
+        return 0
+
+    def clear(self):
+        self._log.clear()
+
+
+class TristateBreaker(CircuitBreaker):
+    """Tristate circuit breaker.
+
+    It operates in three states:
+
+    * GREEN: This is the initial state.  When in this state, it lets all
+      requests pass through.  When there are ``failure_threshold``
+      failures consecutively in the last ``failure_period`` seconds, it
+      changes the state to RED.
+
+    * YELLOW: When in this state, it only lets one request pass through
+      at a time.  When there are ``success_threshold`` successes
+      consecutively, it changes the state to GREEN.  When there is a
+      failure, it changes the state to RED.
+
+    * RED: When in this state, it lets no requests pass through.  After
+      ``failure_timeout`` seconds, it changes the state to YELLOW.
+    """
+
+    def __init__(
+        self,
+        *,
+        key,
+        failure_threshold,
+        failure_period,
+        failure_timeout,
+        success_threshold,
+    ):
+        self._key = key
+        self._failure_threshold = ASSERT.greater(failure_threshold, 0)
+        self._failure_period = ASSERT.greater(failure_period, 0)
+        self._failure_timeout = ASSERT.greater(failure_timeout, 0)
+        self._success_threshold = ASSERT.greater(success_threshold, 0)
+        self._state = _States.GREEN
+        # When state is GREEN, _event_log records failures; when state
+        # is YELLOW, it records successes; when state is RED, it records
+        # when the state was changed to RED.
+        self._event_log = _EventLog(max(failure_threshold, success_threshold))
+        self._yellow_gate = locks.Gate()
+        self._num_concurrent_requests = 0
+
+    async def __aenter__(self):
+        if self._state is _States.GREEN:
+            self._num_concurrent_requests += 1
+            return self
+
+        if self._state is _States.RED:
+            if (
+                self._event_log.
+                count(time.monotonic() - self._failure_timeout) > 0
+            ):
+                raise Unavailable('circuit breaker disconnected')
+            self._change_state_yellow()
+
+        ASSERT.is_(self._state, _States.YELLOW)
+
+        while (
+            self._state is _States.YELLOW and self._num_concurrent_requests > 0
+        ):
+            await self._yellow_gate.wait()
+
+        if self._state is _States.RED:
+            raise Unavailable('circuit breaker became disconnected')
+
+        self._num_concurrent_requests += 1
+        return self
+
+    async def __aexit__(self, exc_type, exc, traceback):
+        self._num_concurrent_requests -= 1
+        self._yellow_gate.unblock()
+
+    def notify_success(self):
+        if self._state is _States.GREEN:
+            self._event_log.clear()
+        elif self._state is _States.YELLOW:
+            self._event_log.add(time.monotonic())
+            if self._event_log.count() >= self._success_threshold:
+                self._change_state_green()
+        else:
+            ASSERT.is_(self._state, _States.RED)
+            # Nothing to do here.
+
+    def notify_failure(self):
+        if self._state is _States.GREEN:
+            now = time.monotonic()
+            self._event_log.add(now)
+            if (
+                self._event_log.count(now - self._failure_period) >=
+                self._failure_threshold
+            ):
+                self._change_state_red(now)
+        elif self._state is _States.YELLOW:
+            self._change_state_red(time.monotonic())
+        else:
+            ASSERT.is_(self._state, _States.RED)
+            # Nothing to do here.
+
+    def _change_state_green(self):
+        LOG.info('TristateBreaker: change state to GREEN: %s', self._key)
+        self._state = _States.GREEN
+        self._event_log.clear()
+        self._yellow_gate.unblock()
+
+    def _change_state_yellow(self):
+        LOG.info('TristateBreaker: change state to YELLOW: %s', self._key)
+        self._state = _States.YELLOW
+        self._event_log.clear()
+        self._yellow_gate.unblock()
+
+    def _change_state_red(self, now):
+        LOG.info('TristateBreaker: change state to RED: %s', self._key)
+        self._state = _States.RED
+        self._event_log.clear()
+        self._event_log.add(now)
+        self._yellow_gate.unblock()
+
+
+class TristateBreakers(CircuitBreakers):
+
+    def __init__(self, **breaker_kwargs):
+        self._breaker_kwargs = breaker_kwargs
+        self._breakers = g1_collections.LruCache(128)
+
+    def get(self, key):
+        breaker = self._breakers.get(key)
+        if breaker is None:
+            # pylint: disable=missing-kwoa
+            breaker = self._breakers[key] = TristateBreaker(
+                key=key,
+                **self._breaker_kwargs,
+            )
+        return breaker
+
+
+class NeverBreaker(CircuitBreaker):
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, traceback):
+        pass
+
+    def notify_success(self):
+        pass
+
+    def notify_failure(self):
+        pass
+
+
+class NeverBreakers(CircuitBreakers):
+
+    def __init__(self):
+        self._no_break = NeverBreaker()
+
+    def get(self, key):
+        return self._no_break
+
+
+NO_BREAK = NeverBreakers()
 
 
 async def unlimited():
@@ -35,7 +258,7 @@ class TokenBucket:
     async def __call__(self):
         self._add_tokens()
         if self._num_tokens < 1 and self._raise_when_empty:
-            raise Unavailable
+            raise Unavailable('rate limit exceeded')
         while self._num_tokens < 1:
             await timers.sleep(self._token_period)
             self._add_tokens()
--- py/g1/http/clients/tests/test_policies.py
+++ py/g1/http/clients/tests/test_policies.py
@@ -2,7 +2,223 @@ import unittest
 import unittest.mock
 
 from g1.asyncs import kernels
+from g1.asyncs.bases import tasks
 from g1.http.clients import policies
+
+
+class EventLogTest(unittest.TestCase):
+
+    def assert_event_log(self, log, expect):
+        self.assertEqual(list(log._log), expect)
+
+    def test_event_log(self):
+        log = policies._EventLog(1)
+        self.assert_event_log(log, [])
+        self.assertEqual(log.count(), 0)
+        self.assertEqual(log.count(1), 0)
+
+        log.add(1)
+        self.assert_event_log(log, [1])
+
+        with self.assertRaisesRegex(AssertionError, 'expect x > 1, not 1'):
+            log.add(1)
+
+        log.add(2)
+        self.assert_event_log(log, [2])
+
+        self.assertEqual(log.count(), 1)
+        self.assertEqual(log.count(2), 1)
+        self.assertEqual(log.count(3), 0)
+
+        log.clear()
+        self.assert_event_log(log, [])
+
+    def test_count(self):
+        log = policies._EventLog(3)
+        log.add(1)
+        log.add(3)
+        log.add(5)
+        self.assert_event_log(log, [1, 3, 5])
+        self.assertEqual(log.count(), 3)
+        self.assertEqual(log.count(0), 3)
+        self.assertEqual(log.count(1), 3)
+        self.assertEqual(log.count(2), 2)
+        self.assertEqual(log.count(3), 2)
+        self.assertEqual(log.count(4), 1)
+        self.assertEqual(log.count(5), 1)
+        self.assertEqual(log.count(6), 0)
+
+
+class TristateBreakerTest(unittest.TestCase):
+
+    def assert_breaker(self, breaker, state, log, num_concurrent_requests):
+        self.assertIs(breaker._state, state)
+        self.assertEqual(list(breaker._event_log._log), log)
+        self.assertEqual(
+            breaker._num_concurrent_requests, num_concurrent_requests
+        )
+
+    @staticmethod
+    def make(
+        failure_threshold, failure_period, failure_timeout, success_threshold
+    ):
+        return policies.TristateBreaker(
+            key='test',
+            failure_threshold=failure_threshold,
+            failure_period=failure_period,
+            failure_timeout=failure_timeout,
+            success_threshold=success_threshold,
+        )
+
+    @kernels.with_kernel
+    def test_context_green(self):
+        breaker = self.make(2, 1, 1, 2)
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+
+        self.assertIs(kernels.run(breaker.__aenter__()), breaker)
+        self.assert_breaker(breaker, policies._States.GREEN, [], 1)
+
+        self.assertIsNone(kernels.run(breaker.__aexit__(None, None, None)))
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+
+    @kernels.with_kernel
+    def test_context_yellow_one_at_a_time(self):
+        breaker = self.make(2, 1, 1, 2)
+        breaker._change_state_yellow()
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 0)
+
+        self.assertIs(kernels.run(breaker.__aenter__()), breaker)
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 1)
+
+        aenter_task = tasks.spawn(breaker.__aenter__())
+        with self.assertRaises(kernels.KernelTimeout):
+            kernels.run(timeout=0.01)
+        self.assertFalse(aenter_task.is_completed())
+
+        aexit_task = tasks.spawn(breaker.__aexit__(None, None, None))
+        kernels.run(timeout=0.01)
+
+        self.assertTrue(aexit_task.is_completed())
+        self.assertIsNone(aexit_task.get_result_nonblocking())
+
+        self.assertTrue(aenter_task.is_completed())
+        self.assertIs(aenter_task.get_result_nonblocking(), breaker)
+
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 1)
+
+    @kernels.with_kernel
+    def test_context_yellow_change_state_red(self):
+        breaker = self.make(2, 1, 1, 2)
+        breaker._change_state_yellow()
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 0)
+
+        self.assertIs(kernels.run(breaker.__aenter__()), breaker)
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 1)
+
+        aenter_task = tasks.spawn(breaker.__aenter__())
+        with self.assertRaises(kernels.KernelTimeout):
+            kernels.run(timeout=0.01)
+        self.assertFalse(aenter_task.is_completed())
+
+        breaker._change_state_red(99)
+        kernels.run(breaker.__aexit__(None, None, None), timeout=0.01)
+
+        self.assertTrue(aenter_task.is_completed())
+        with self.assertRaisesRegex(
+            policies.Unavailable, r'circuit breaker became disconnected'
+        ):
+            aenter_task.get_result_nonblocking()
+
+        self.assert_breaker(breaker, policies._States.RED, [99], 0)
+
+    @kernels.with_kernel
+    @unittest.mock.patch.object(policies, 'time')
+    def test_context_red(self, mock_time):
+        mock_monotonic = mock_time.monotonic
+        breaker = self.make(2, 1, 1, 2)
+
+        mock_monotonic.side_effect = [100]
+        breaker._change_state_red(99)
+        self.assert_breaker(breaker, policies._States.RED, [99], 0)
+
+        with self.assertRaisesRegex(
+            policies.Unavailable, r'circuit breaker disconnected'
+        ):
+            kernels.run(breaker.__aenter__())
+        self.assert_breaker(breaker, policies._States.RED, [99], 0)
+
+    @kernels.with_kernel
+    @unittest.mock.patch.object(policies, 'time')
+    def test_context_red_timeout(self, mock_time):
+        mock_monotonic = mock_time.monotonic
+        breaker = self.make(2, 1, 1, 2)
+
+        mock_monotonic.side_effect = [101]
+        breaker._change_state_red(99)
+        self.assert_breaker(breaker, policies._States.RED, [99], 0)
+
+        self.assertIs(kernels.run(breaker.__aenter__()), breaker)
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 1)
+
+    @unittest.mock.patch.object(policies, 'time')
+    def test_notify_success(self, mock_time):
+        mock_monotonic = mock_time.monotonic
+        breaker = self.make(2, 1, 1, 2)
+
+        breaker._event_log.add(1)
+        self.assert_breaker(breaker, policies._States.GREEN, [1], 0)
+        breaker.notify_success()
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+
+        mock_monotonic.side_effect = [99, 100]
+        breaker._change_state_yellow()
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 0)
+        breaker.notify_success()
+        self.assert_breaker(breaker, policies._States.YELLOW, [99], 0)
+        breaker.notify_success()
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+
+        breaker._change_state_red(2)
+        self.assert_breaker(breaker, policies._States.RED, [2], 0)
+        breaker.notify_success()
+        self.assert_breaker(breaker, policies._States.RED, [2], 0)
+
+    @unittest.mock.patch.object(policies, 'time')
+    def test_notify_failure(self, mock_time):
+        mock_monotonic = mock_time.monotonic
+        breaker = self.make(2, 1, 1, 2)
+
+        mock_monotonic.side_effect = [99, 100]
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+        breaker.notify_failure()
+        self.assert_breaker(breaker, policies._States.GREEN, [99], 0)
+        breaker.notify_failure()
+        self.assert_breaker(breaker, policies._States.RED, [100], 0)
+
+        mock_monotonic.side_effect = [101]
+        breaker._change_state_yellow()
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 0)
+        breaker.notify_failure()
+        self.assert_breaker(breaker, policies._States.RED, [101], 0)
+
+        breaker.notify_failure()
+        self.assert_breaker(breaker, policies._States.RED, [101], 0)
+
+    def test_change_state(self):
+        breaker = self.make(1, 1, 1, 1)
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
+
+        breaker._event_log.add(1)
+        self.assert_breaker(breaker, policies._States.GREEN, [1], 0)
+
+        breaker._change_state_yellow()
+        self.assert_breaker(breaker, policies._States.YELLOW, [], 0)
+
+        breaker._change_state_red(2)
+        self.assert_breaker(breaker, policies._States.RED, [2], 0)
+
+        breaker._change_state_green()
+        self.assert_breaker(breaker, policies._States.GREEN, [], 0)
 
 
 class RateLimitTest(unittest.TestCase):
@@ -42,7 +258,9 @@ class RateLimitTest(unittest.TestCase):
     def test_token_bucket_raise_when_empty(self):
         self.monotonic_mock.return_value = 99
         tb = policies.TokenBucket(1, 1, True)
-        with self.assertRaises(policies.Unavailable):
+        with self.assertRaisesRegex(
+            policies.Unavailable, r'rate limit exceeded'
+        ):
             kernels.run(tb)
         self.assertEqual(tb._num_tokens, 0)
 
