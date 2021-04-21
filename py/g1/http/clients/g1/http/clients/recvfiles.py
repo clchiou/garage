4261ec1370091391fc597625b4b5827de35e16c6
Che-Liang Chiou <clchiou@gmail.com>
2021-04-21 00:44:59 +0000

    Make recvfile support chunk encoding and decompression
    This should make recvfile on par with requests iter_content.

--- py/g1/http/clients/g1/http/clients/recvfiles.py
+++ py/g1/http/clients/g1/http/clients/recvfiles.py
@@ -2,8 +2,10 @@ __all__ = [
     'recvfile',
 ]
 
+import contextlib
 import http.client
 import logging
+import re
 import socket
 
 from g1.asyncs.bases import adapters
@@ -26,59 +28,71 @@ async def recvfile(response, file):
 
     The caller must set ``stream`` to true when make the request.
 
-    NOTE:
-
-    * DANGER! This breaks the multiple levels of encapsulation, from
-      requests.Response all the way down to http.client.HTTPResponse.
-      As a result, the response object is most likely unusable after a
-      recvfile call, and you should probably close it immediately.
-
-    * For now, no Content-Encoding nor Transfer-Encoding are supported.
+    DANGER! This breaks the multiple levels of encapsulation, from
+    requests.Response all the way down to http.client.HTTPResponse.
+    As a result, the response object is most likely unusable after a
+    recvfile call, and you should probably close it immediately.
     """
     # requests sets _content to False initially.
     ASSERT.is_(response._content, False)
     ASSERT.false(response._content_consumed)
 
-    for header in ['Content-Encoding', 'Transfer-Encoding']:
-        encoding = response.headers.get(header)
-        if encoding:
-            raise ValueError(
-                '%s is not supported: %r %s' %
-                (header, encoding, response.url)
-            )
-
     urllib3_response = ASSERT.not_none(response.raw)
-    ASSERT.false(urllib3_response.chunked)
+    chunked = urllib3_response.chunked
 
     httplib_response = ASSERT.isinstance(
         urllib3_response._fp, http.client.HTTPResponse
     )
     ASSERT.false(httplib_response.closed)
-
     sock = ASSERT.isinstance(httplib_response.fp.raw._sock, socket.socket)
-    num_to_read = ASSERT.greater(ASSERT.not_none(httplib_response.length), 0)
 
-    src = adapters.FileAdapter(httplib_response.fp)
-    try:
+    output = DecoderChain(file)
+
+    if chunked:
+        chunk_decoder = ChunkDecoder()
+        output.add(chunk_decoder)
+        num_to_read = 0
+        eof = lambda: chunk_decoder.eof
+    else:
+        num_to_read = ASSERT.greater(
+            ASSERT.not_none(httplib_response.length), 0
+        )
+        eof = lambda: num_to_read <= 0
+
+    # Use urllib3's decoder code.
+    urllib3_response._init_decoder()
+    if urllib3_response._decoder is not None:
+        output.add(ContentDecoder(urllib3_response._decoder))
+
+    with contextlib.ExitStack() as stack:
+        src = adapters.FileAdapter(httplib_response.fp)
+        stack.callback(src.disown)
+
         sock.setblocking(False)
+        stack.callback(sock.setblocking, True)
 
-        with _BUFFER_POOL.using() as buffer:
-            buffer = memoryview(buffer)
-            while num_to_read > 0:
+        buffer = memoryview(stack.enter_context(_BUFFER_POOL.using()))
+        while not eof():
+            if chunked:
+                # TODO: If server sends more data at the end, like
+                # response of the next request, for now recvfile might
+                # read them, and then err out.  Maybe recvfile should
+                # check this, and not read more than it should instead?
+                num_read = await src.readinto1(buffer)
+            else:
                 num_read = await src.readinto1(
                     buffer[:min(num_to_read, _CHUNK_SIZE)]
                 )
-                if num_read == 0:
-                    break
-                file.write(buffer[:num_read])
-                num_to_read -= num_read
+            if num_read == 0:
+                break
+            output.write(buffer[:num_read])
+            num_to_read -= num_read
 
-        # Sanity check.
+        output.flush()
+
+    # Sanity check.
+    if not chunked:
         ASSERT.equal(num_to_read, 0)
-
-    finally:
-        src.disown()
-        sock.setblocking(True)
 
     # Trick requests to release the connection back to the connection
     # pool, rather than closing/discarding it.
@@ -94,3 +108,141 @@ async def recvfile(response, file):
     loggings.ONCE_PER(
         1000, LOG.info, 'buffer pool stats: %r', _BUFFER_POOL.get_stats()
     )
+
+
+class DecoderChain:
+
+    def __init__(self, file):
+        self._file = file
+        self._decoders = []
+
+    def add(self, decoder):
+        self._decoders.append(decoder)
+
+    def write(self, data):
+        pieces = [data]
+        for decoder in self._decoders:
+            pieces = decoder.decode(pieces)
+        self._write(pieces)
+
+    def flush(self):
+        for i, decoder in enumerate(self._decoders):
+            pieces = decoder.flush()
+            for d in self._decoders[i + 1:]:
+                pieces = d.decode(pieces)
+            self._write(pieces)
+        self._file.flush()
+
+    def _write(self, pieces):
+        for data in pieces:
+            if data:
+                self._file.write(data)
+
+
+class ChunkDecoder:
+
+    _CRLF_PATTERN = re.compile(br'\r\n')
+
+    def __init__(self):
+        self.eof = False
+        self._chunk_remaining = -2
+        # Buffer for residual chunk size data from the last `_decode`.
+        # It is fairly small for now because we do not expect big chunk
+        # parameter.
+        self._buffer = memoryview(bytearray(64))
+        self._pos = 0
+
+    def decode(self, pieces):
+        ASSERT.false(self.eof)
+        output = []
+        for data in pieces:
+            if data:
+                self._decode(data, output)
+        return output
+
+    def _decode(self, data, output):
+
+        def move(n):
+            """Move ``n`` bytes from ``data`` to ``output``."""
+            nonlocal data
+            ASSERT.greater_or_equal(self._chunk_remaining, n)
+            output.append(data[:n])
+            data = data[n:]
+            self._chunk_remaining -= n
+
+        def expect(pattern):
+            """Drop ``pattern`` prefix from ``data``."""
+            nonlocal data
+            n = min(len(pattern), len(data))
+            ASSERT.equal(pattern[:n], data[:n])
+            data = data[n:]
+            return n
+
+        while data:
+            if self._chunk_remaining > 0:
+                move(min(self._chunk_remaining, len(data)))
+                continue
+
+            if self._chunk_remaining == 0:
+                self._chunk_remaining -= expect(b'\r\n')
+                continue
+
+            if self._chunk_remaining == -1:
+                self._chunk_remaining -= expect(b'\n')
+                continue
+
+            match = self._CRLF_PATTERN.search(data)
+            if not match:
+                self._append(data)
+                match = self._CRLF_PATTERN.search(self._buffer[:self._pos])
+                if not match:
+                    break
+                data = self._reset()
+
+            chunk_size = data[:match.start()]
+            if self._pos > 0:
+                self._append(chunk_size)
+                chunk_size = self._reset()
+            # TODO: Handle parameters (stuff after ';').
+            chunk_size = int(
+                bytes(chunk_size).split(b';', maxsplit=1)[0],
+                base=16,
+            )
+            if chunk_size == 0:
+                # TODO: Handle trailers.
+                self.eof = True
+            else:
+                ASSERT.false(self.eof)
+
+            data = data[match.end():]
+            self._chunk_remaining = chunk_size
+
+        if self.eof:
+            ASSERT.empty(data)
+
+    def _append(self, data):
+        end = ASSERT.less_or_equal(self._pos + len(data), len(self._buffer))
+        self._buffer[self._pos:end] = data
+        self._pos = end
+
+    def _reset(self):
+        data = self._buffer[:self._pos]
+        self._pos = 0
+        return data
+
+    def flush(self):
+        ASSERT.true(self.eof)
+        ASSERT.equal(self._chunk_remaining, -2)
+        return []
+
+
+class ContentDecoder:
+
+    def __init__(self, urllib3_decoder):
+        self._decoder = urllib3_decoder
+
+    def decode(self, pieces):
+        return [self._decoder.decompress(data) for data in pieces if data]
+
+    def flush(self):
+        return [self._decoder.flush()]
---
+++ py/g1/http/clients/tests/test_recvfiles.py
@@ -0,0 +1,165 @@
+import unittest
+import unittest.mock
+
+from pathlib import Path
+
+from g1.http.clients import recvfiles
+
+
+class RecvfilesTest(unittest.TestCase):
+
+    def assert_chunk_decoder(
+        self,
+        decoder,
+        expect_eof,
+        expect_chunk_remaining,
+    ):
+        self.assertEqual(decoder.eof, expect_eof)
+        self.assertEqual(decoder._chunk_remaining, expect_chunk_remaining)
+
+    def test_decoder_chain(self):  # pylint: disable=no-self-use
+        mock_file = unittest.mock.Mock(spec_set=['write', 'flush'])
+        d0 = unittest.mock.Mock(spec_set=['decode', 'flush'])
+        d1 = unittest.mock.Mock(spec_set=['decode', 'flush'])
+        d2 = unittest.mock.Mock(spec_set=['decode', 'flush'])
+        d0.decode.side_effect = lambda pieces: pieces + [b'd0-decode']
+        d1.decode.side_effect = lambda pieces: pieces + [b'd1-decode']
+        d2.decode.side_effect = lambda pieces: pieces + [b'd2-decode']
+        d0.flush.return_value = [b'd0-flush']
+        d1.flush.return_value = [b'd1-flush']
+        d2.flush.return_value = [b'd2-flush']
+
+        chain = recvfiles.DecoderChain(mock_file)
+        chain.add(d0)
+        chain.add(d1)
+        chain.add(d2)
+        chain.write(b'spam')
+        chain.write(b'egg')
+        chain.flush()
+
+        mock_file.assert_has_calls([
+            unittest.mock.call.write(b'spam'),
+            unittest.mock.call.write(b'd0-decode'),
+            unittest.mock.call.write(b'd1-decode'),
+            unittest.mock.call.write(b'd2-decode'),
+            unittest.mock.call.write(b'egg'),
+            unittest.mock.call.write(b'd0-decode'),
+            unittest.mock.call.write(b'd1-decode'),
+            unittest.mock.call.write(b'd2-decode'),
+            unittest.mock.call.write(b'd0-flush'),
+            unittest.mock.call.write(b'd1-decode'),
+            unittest.mock.call.write(b'd2-decode'),
+            unittest.mock.call.write(b'd1-flush'),
+            unittest.mock.call.write(b'd2-decode'),
+            unittest.mock.call.write(b'd2-flush'),
+            unittest.mock.call.flush(),
+        ])
+        d0.assert_has_calls([
+            unittest.mock.call.decode([b'spam']),
+            unittest.mock.call.decode([b'egg']),
+            unittest.mock.call.flush(),
+        ])
+        d1.assert_has_calls([
+            unittest.mock.call.decode([b'spam', b'd0-decode']),
+            unittest.mock.call.decode([b'egg', b'd0-decode']),
+            unittest.mock.call.decode([b'd0-flush']),
+            unittest.mock.call.flush(),
+        ])
+        d2.assert_has_calls([
+            unittest.mock.call.decode([b'spam', b'd0-decode', b'd1-decode']),
+            unittest.mock.call.decode([b'egg', b'd0-decode', b'd1-decode']),
+            unittest.mock.call.decode([b'd0-flush', b'd1-decode']),
+            unittest.mock.call.decode([b'd1-flush']),
+            unittest.mock.call.flush(),
+        ])
+
+    def test_chunk_decoder(self):
+        d = recvfiles.ChunkDecoder()
+        self.assert_chunk_decoder(d, False, -2)
+
+        self.assertEqual(d.decode([]), [])
+        self.assert_chunk_decoder(d, False, -2)
+
+        output = []
+        d._decode(b'4;foo bar\r\nab', output)
+        self.assertEqual(output, [b'ab'])
+        self.assert_chunk_decoder(d, False, 2)
+        d._decode(b'cd', output)
+        self.assertEqual(output, [b'ab', b'cd'])
+        self.assert_chunk_decoder(d, False, 0)
+        d._decode(b'\r', output)
+        self.assertEqual(output, [b'ab', b'cd'])
+        self.assert_chunk_decoder(d, False, -1)
+        d._decode(b'\n', output)
+        self.assertEqual(output, [b'ab', b'cd'])
+        self.assert_chunk_decoder(d, False, -2)
+
+        output = []
+        d._decode(b'0;some parameters...\r\n', output)
+        self.assertEqual(output, [])
+        self.assert_chunk_decoder(d, True, 0)
+        d._decode(b'\r\n', output)
+        self.assertEqual(output, [])
+        self.assert_chunk_decoder(d, True, -2)
+
+        self.assertEqual(d.flush(), [])
+
+    def test_chunk_decoder_more_data_at_the_end(self):
+        d = recvfiles.ChunkDecoder()
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'expect empty collection, not b\'some more data\'',
+        ):
+            d.decode([b'0\r\n\r\nsome more data'])
+
+        d = recvfiles.ChunkDecoder()
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'expect empty collection, not b\'some more data\'',
+        ):
+            d.decode([b'0\r\n\r\n', b'some more data'])
+
+        d = recvfiles.ChunkDecoder()
+        with self.assertRaisesRegex(
+            AssertionError,
+            r'expect false-value, not True',
+        ):
+            d.decode([b'0\r\n\r\n1\r\nx\r\n'])
+
+    def test_chunk_decoder_one_byte_at_a_time(self):
+        for content in [
+            b'hello world',
+            b'0123456789abcdeffedcba9876543210',
+            Path(__file__).read_bytes(),
+        ]:
+            with self.subTest(content):
+                # Two chunks, one byte at a time.
+                output = []
+                d = recvfiles.ChunkDecoder()
+                self.assert_chunk_decoder(d, False, -2)
+                for chunk_data in [
+                    content[:len(content) // 2],
+                    content[len(content) // 2:],
+                    b'',
+                ]:
+                    chunk = b'%x\r\n%s\r\n' % (len(chunk_data), chunk_data)
+                    for i in range(len(chunk)):
+                        d._decode(memoryview(chunk[i:i + 1]), output)
+                    self.assert_chunk_decoder(d, chunk_data == b'', -2)
+                self.assertEqual(b''.join(output), content)
+
+                # One byte per chunk.
+                output = []
+                d = recvfiles.ChunkDecoder()
+                self.assert_chunk_decoder(d, False, -2)
+                for i in range(len(content) + 1):
+                    chunk_data = content[i:i + 1]
+                    chunk = b'%x\r\n%s\r\n' % (len(chunk_data), chunk_data)
+                    d._decode(memoryview(chunk), output)
+                    self.assert_chunk_decoder(d, chunk_data == b'', -2)
+                    self.assertEqual(b''.join(output), content[:i + 1])
+                self.assertEqual(b''.join(output), content)
+
+
+if __name__ == '__main__':
+    unittest.main()
