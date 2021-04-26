clchiou/garage

commit 0603bef0549c065b21f99f43c08f8c548b625508

Tweak supervise_server logging

py/g1/asyncs/bases/g1/asyncs/bases/servers.py
@@ -23,6 +23,8 @@ __all__ = [
 
 import logging
 
+from . import tasks
+
 LOG = logging.getLogger(__name__)
 
 
@@ -48,7 +50,14 @@ async def supervise_server(queue, server_tasks):
             exc = task.get_exception_nonblocking()
             if task in server_tasks:
                 if exc:
-                    raise ServerError('server task error: %r' % task) from exc
+                    if isinstance(exc, tasks.Cancelled):
+                        # Log at DEBUG rather than INFO level for the
+                        # same reason below.
+                        LOG.debug('server task is cancelled: %r', task)
+                    else:
+                        raise ServerError(
+                            'server task error: %r' % task
+                        ) from exc
                 else:
                     # Log at DEBUG rather than INFO level because
                     # supervise_server could actually be called in a
@@ -59,6 +68,9 @@ async def supervise_server(queue, server_tasks):
                     break
             else:
                 if exc:
-                    LOG.error('handler task error: %r', task, exc_info=exc)
+                    if isinstance(exc, tasks.Cancelled):
+                        LOG.debug('handler task is cancelled: %r', task)
+                    else:
+                        LOG.error('handler task error: %r', task, exc_info=exc)
                 else:
                     LOG.debug('handler task exit: %r', task)

commit bfa3a1b0175a2282d1208075f75ad6a7149a4d67

Fix not breaking when a server task gets cancelled
We tweaked log level in commit 0603bef0549c065b21f99f43c08f8c548b625508,
which introduced a bug that causes supervise_server not breaking when a
server task gets cancelled.

Also, change log level back to INFO so that we will not miss any server
task (accidental) completion in the future.

py/g1/asyncs/bases/g1/asyncs/bases/servers.py
@@ -48,29 +48,23 @@ async def supervise_server(queue, server_tasks):
     async with queue:
         async for task in queue:
             exc = task.get_exception_nonblocking()
+
             if task in server_tasks:
                 if exc:
                     if isinstance(exc, tasks.Cancelled):
-                        # Log at DEBUG rather than INFO level for the
-                        # same reason below.
-                        LOG.debug('server task is cancelled: %r', task)
+                        LOG.info('server task is cancelled: %r', task)
                     else:
                         raise ServerError(
                             'server task error: %r' % task
                         ) from exc
                 else:
-                    # Log at DEBUG rather than INFO level because
-                    # supervise_server could actually be called in a
-                    # request handler (when the handler is sufficiently
-                    # complex), and we do not want to over-log at INFO
-                    # level.
-                    LOG.debug('server task exit: %r', task)
-                    break
-            else:
-                if exc:
-                    if isinstance(exc, tasks.Cancelled):
-                        LOG.debug('handler task is cancelled: %r', task)
-                    else:
-                        LOG.error('handler task error: %r', task, exc_info=exc)
+                    LOG.info('server task exit: %r', task)
+                break
+
+            if exc:
+                if isinstance(exc, tasks.Cancelled):
+                    LOG.debug('handler task is cancelled: %r', task)
                 else:
-                    LOG.debug('handler task exit: %r', task)
+                    LOG.error('handler task error: %r', task, exc_info=exc)
+            else:
+                LOG.debug('handler task exit: %r', task)