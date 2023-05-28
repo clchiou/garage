638ffb38584af508ff20adef516a85fdc7224796
Che-Liang Chiou <clchiou@gmail.com>
2023-05-28 06:57:54 +0000

    Set up Cargo workspace

---
+++ rust/g1/base/src/lib.rs
@@ -0,0 +1,7 @@
+//! Base Library
+//!
+//! In general, the base library provides two kinds of features:
+//! * Generic utility functions that are not in the Rust stdlib.
+//! * Opinionated extension to the Rust stdlib.
+//!
+//! NOTE: The base library should only depend on the Rust stdlib.
