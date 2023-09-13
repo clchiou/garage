3d3a492b8e62b19e49cb7e25c77eef6f9d696efc
Che-Liang Chiou <clchiou@gmail.com>
2023-09-13 07:17:18 +0000

    Prototype bittorrent_base::{PieceIndex, BlockOffset, BlockDesc}

--- rust/bittorrent/base/src/lib.rs
+++ rust/bittorrent/base/src/lib.rs
@@ -52,3 +52,138 @@ impl Borrow<[u8]> for InfoHash {
         self.0.as_slice()
     }
 }
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct PieceIndex(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct BlockOffset(pub PieceIndex, pub u64);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct BlockDesc(pub BlockOffset, pub u64);
+
+impl From<usize> for PieceIndex {
+    fn from(index: usize) -> Self {
+        Self(index)
+    }
+}
+
+impl From<PieceIndex> for usize {
+    fn from(PieceIndex(index): PieceIndex) -> Self {
+        index
+    }
+}
+
+impl PieceIndex {
+    pub fn to_scalar(self, piece_size: u64) -> u64 {
+        u64::try_from(usize::from(self)).unwrap() * piece_size
+    }
+}
+
+impl From<(usize, u64)> for BlockOffset {
+    fn from((index, offset): (usize, u64)) -> Self {
+        Self(index.into(), offset)
+    }
+}
+
+impl From<(PieceIndex, u64)> for BlockOffset {
+    fn from((index, offset): (PieceIndex, u64)) -> Self {
+        Self(index, offset)
+    }
+}
+
+impl From<BlockOffset> for (usize, u64) {
+    fn from(BlockOffset(PieceIndex(index), offset): BlockOffset) -> Self {
+        (index, offset)
+    }
+}
+
+impl BlockOffset {
+    fn check(offset: u64, piece_size: u64) {
+        assert!(
+            offset < piece_size,
+            "expect block offset < {}: {}",
+            piece_size,
+            offset,
+        );
+    }
+
+    pub fn add_scalar(self, scalar: u64, piece_size: u64) -> Self {
+        let (index, offset) = self.into();
+        Self::check(offset, piece_size);
+        let offset = offset + scalar;
+        let index = index + usize::try_from(offset / piece_size).unwrap();
+        (index, offset % piece_size).into()
+    }
+
+    pub fn to_scalar(self, piece_size: u64) -> u64 {
+        let Self(index, offset) = self;
+        Self::check(offset, piece_size);
+        index.to_scalar(piece_size) + offset
+    }
+}
+
+impl From<(usize, u64, u64)> for BlockDesc {
+    fn from((index, offset, size): (usize, u64, u64)) -> Self {
+        Self((index, offset).into(), size)
+    }
+}
+
+impl From<(BlockOffset, u64)> for BlockDesc {
+    fn from((offset, size): (BlockOffset, u64)) -> Self {
+        Self(offset, size)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn piece_index_to_scalar() {
+        assert_eq!(PieceIndex::from(0).to_scalar(1), 0);
+        assert_eq!(PieceIndex::from(1).to_scalar(1), 1);
+        assert_eq!(PieceIndex::from(2).to_scalar(1), 2);
+        assert_eq!(PieceIndex::from(0).to_scalar(2), 0);
+        assert_eq!(PieceIndex::from(1).to_scalar(2), 2);
+        assert_eq!(PieceIndex::from(2).to_scalar(2), 4);
+        assert_eq!(PieceIndex::from(0).to_scalar(3), 0);
+        assert_eq!(PieceIndex::from(1).to_scalar(3), 3);
+        assert_eq!(PieceIndex::from(2).to_scalar(3), 6);
+    }
+
+    #[test]
+    fn block_offset_add_scalar() {
+        let x = BlockOffset::from((0, 0));
+        assert_eq!(x.add_scalar(0, 1), (0, 0).into());
+        assert_eq!(x.add_scalar(1, 1), (1, 0).into());
+        assert_eq!(x.add_scalar(2, 1), (2, 0).into());
+        assert_eq!(x.add_scalar(3, 1), (3, 0).into());
+
+        assert_eq!(x.add_scalar(0, 2), (0, 0).into());
+        assert_eq!(x.add_scalar(1, 2), (0, 1).into());
+        assert_eq!(x.add_scalar(2, 2), (1, 0).into());
+        assert_eq!(x.add_scalar(3, 2), (1, 1).into());
+
+        let x = BlockOffset::from((2, 1));
+        assert_eq!(x.add_scalar(0, 3), (2, 1).into());
+        assert_eq!(x.add_scalar(1, 3), (2, 2).into());
+        assert_eq!(x.add_scalar(2, 3), (3, 0).into());
+        assert_eq!(x.add_scalar(3, 3), (3, 1).into());
+    }
+
+    #[test]
+    fn block_offset_to_scalar() {
+        assert_eq!(BlockOffset::from((0, 0)).to_scalar(1), 0);
+        assert_eq!(BlockOffset::from((1, 0)).to_scalar(1), 1);
+        assert_eq!(BlockOffset::from((2, 0)).to_scalar(1), 2);
+        assert_eq!(BlockOffset::from((3, 0)).to_scalar(1), 3);
+
+        assert_eq!(BlockOffset::from((0, 0)).to_scalar(2), 0);
+        assert_eq!(BlockOffset::from((0, 1)).to_scalar(2), 1);
+        assert_eq!(BlockOffset::from((1, 0)).to_scalar(2), 2);
+        assert_eq!(BlockOffset::from((1, 1)).to_scalar(2), 3);
+
+        assert_eq!(BlockOffset::from((2, 1)).to_scalar(5), 11);
+    }
+}
