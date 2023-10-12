f3760d8d0e8ebbfc1a919070f11928f015928e5e
Che-Liang Chiou <clchiou@gmail.com>
2023-10-12 15:42:11 +0000

    Rewrite CoordSys::to_file_descs to return an iterator

--- rust/bittorrent/storage/src/coord.rs
+++ rust/bittorrent/storage/src/coord.rs
@@ -1,4 +1,6 @@
 use std::cmp;
+use std::iter;
+use std::sync::Arc;
 
 use snafu::prelude::*;
 
@@ -11,7 +13,9 @@ use crate::{error, FileBlockDesc, FileBlockOffset};
 pub(crate) struct CoordSys {
     pub(crate) dim: Dimension,
     /// Maps file indexes to block offsets.
-    file_ends: Vec<BlockOffset>,
+    // Use `Arc<[_]>` instead of `Vec<_>` so that `to_file_descs` can return an iterator without
+    // borrowing `self`.
+    file_ends: Arc<[BlockOffset]>,
 }
 
 impl CoordSys {
@@ -34,7 +38,10 @@ impl CoordSys {
         ensure!(!file_ends.is_empty(), error::ExpectNonEmptyFileListSnafu);
         assert_eq!(dim.size, size);
         assert_eq!(&dim.end, file_ends.last().unwrap());
-        Ok(Self { dim, file_ends })
+        Ok(Self {
+            dim,
+            file_ends: file_ends.into(),
+        })
     }
 
     fn check_block_offset(&self, offset: BlockOffset) -> Result<BlockOffset, error::Error> {
@@ -52,46 +59,58 @@ impl CoordSys {
     pub(crate) fn to_file_descs(
         &self,
         desc: BlockDesc,
-    ) -> Result<Vec<FileBlockDesc>, error::Error> {
-        let mut file_descs = Vec::new();
+    ) -> Result<impl Iterator<Item = FileBlockDesc>, error::Error> {
         let BlockDesc(mut offset, mut size) = self.check_block_desc(desc)?;
-        while size > 0 {
-            let file_offset = self.to_file_offset(offset).unwrap().unwrap();
+        let file_ends = self.file_ends.clone();
+        let piece_size = self.dim.piece_size;
+        Ok(iter::from_fn(move || {
+            if size == 0 {
+                return None;
+            }
+            let file_offset = Self::to_file_offset_impl(offset, &file_ends, piece_size).unwrap();
             let file_size = cmp::min(
-                self.file_ends[usize::from(file_offset.0)].to_scalar(self.dim.piece_size)
-                    - offset.to_scalar(self.dim.piece_size),
+                file_ends[usize::from(file_offset.0)].to_scalar(piece_size)
+                    - offset.to_scalar(piece_size),
                 size,
             );
-            file_descs.push((file_offset, file_size).into());
-            offset = offset.add_scalar(file_size, self.dim.piece_size);
+            offset = offset.add_scalar(file_size, piece_size);
             size -= file_size;
-        }
-        Ok(file_descs)
+            Some((file_offset, file_size).into())
+        }))
     }
 
     pub(crate) fn to_file_offset(
         &self,
         offset: BlockOffset,
     ) -> Result<Option<FileBlockOffset>, error::Error> {
-        self.check_block_offset(offset)?;
+        Ok(Self::to_file_offset_impl(
+            self.check_block_offset(offset)?,
+            &self.file_ends,
+            self.dim.piece_size,
+        ))
+    }
 
-        let file_index = match self.file_ends.binary_search(&offset) {
+    fn to_file_offset_impl(
+        offset: BlockOffset,
+        file_ends: &[BlockOffset],
+        piece_size: u64,
+    ) -> Option<FileBlockOffset> {
+        let file_index = match file_ends.binary_search(&offset) {
             Ok(i) => i + 1,
             Err(i) => i,
         };
-        if file_index == self.file_ends.len() {
-            return Ok(None);
+        if file_index == file_ends.len() {
+            return None;
         }
 
         let start = if file_index == 0 {
             (0, 0).into()
         } else {
-            self.file_ends[file_index - 1]
+            file_ends[file_index - 1]
         };
-        let file_offset =
-            offset.to_scalar(self.dim.piece_size) - start.to_scalar(self.dim.piece_size);
+        let file_offset = offset.to_scalar(piece_size) - start.to_scalar(piece_size);
 
-        Ok(Some((file_index, file_offset).into()))
+        Some((file_index, file_offset).into())
     }
 }
 
@@ -118,14 +137,14 @@ mod tests {
             new_coord_sys(1, 7, &[7]),
             Ok(CoordSys {
                 dim: Dimension::new(1, 7, 7, BLOCK_SIZE),
-                file_ends: vec![(1, 0).into()],
+                file_ends: vec![(1, 0).into()].into(),
             }),
         );
         assert_eq!(
             new_coord_sys(2, 7, &[1, 2, 7, 2]),
             Ok(CoordSys {
                 dim: Dimension::new(2, 7, 12, BLOCK_SIZE),
-                file_ends: vec![(0, 1).into(), (0, 3).into(), (1, 3).into(), (1, 5).into()],
+                file_ends: vec![(0, 1).into(), (0, 3).into(), (1, 3).into(), (1, 5).into()].into(),
             }),
         );
         assert_eq!(
@@ -210,15 +229,17 @@ mod tests {
     fn to_file_descs() {
         fn test_ok(coord_sys: &CoordSys, desc: (usize, u64, u64), expect: &[(usize, u64, u64)]) {
             let expect: Vec<_> = expect.iter().copied().map(FileBlockDesc::from).collect();
-            assert_eq!(coord_sys.to_file_descs(desc.into()), Ok(expect));
+            let file_descs: Vec<_> = coord_sys.to_file_descs(desc.into()).unwrap().collect();
+            assert_eq!(file_descs, expect);
         }
 
         fn test_err(coord_sys: &CoordSys, desc: (usize, u64, u64)) {
             let desc = BlockDesc::from(desc);
-            assert_eq!(
-                coord_sys.to_file_descs(desc),
-                Err(error::Error::InvalidBlockDesc { desc }),
-            );
+            // We cannot use `assert_matches!` because the iterator cannot be `Debug`-formatted.
+            match coord_sys.to_file_descs(desc) {
+                Ok(_) => std::panic!("expect to_file_descs to return error"),
+                Err(e) => assert_eq!(e, error::Error::InvalidBlockDesc { desc }),
+            }
         }
 
         let coord_sys = new_coord_sys(1, 7, &[4]).unwrap();
