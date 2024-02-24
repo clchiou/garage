3654104d28062c1c57dcbef2fca42a55b9d60f06
Che-Liang Chiou <clchiou@gmail.com>
2024-02-24 18:48:03 +0000

    Prototype g1_base::iter::product

--- rust/g1/base/src/iter.rs
+++ rust/g1/base/src/iter.rs
@@ -36,6 +36,19 @@ pub trait IteratorExt: Iterator {
 
 impl<T> IteratorExt for T where T: Iterator {}
 
+pub fn product<'a, XS, YS, X, Y>(xs: XS, ys: YS) -> impl Iterator<Item = (&'a X, &'a Y)>
+where
+    XS: IntoIterator<Item = &'a X>,
+    YS: IntoIterator<Item = &'a Y>,
+    <YS as IntoIterator>::IntoIter: Clone,
+    X: 'a,
+    Y: 'a,
+{
+    let xs = xs.into_iter();
+    let ys = ys.into_iter();
+    xs.flat_map(move |x| ys.clone().map(move |y| (x, y)))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,5 +64,23 @@ mod tests {
             [3, 1, 2].into_iter().collect_then_sort_by_key(|x| *x),
             vec![1, 2, 3],
         );
+    }
+
+    #[test]
+    fn test_product() {
+        fn test<const N: usize>(xs: &[usize], ys: &[usize], expect: [(usize, usize); N]) {
+            assert_eq!(
+                product(xs, ys).map(|(x, y)| (*x, *y)).collect::<Vec<_>>(),
+                expect,
+            );
+        }
+
+        test(&[], &[], []);
+        test(&[1], &[], []);
+        test(&[], &[10], []);
+        test(&[1], &[10], [(1, 10)]);
+        test(&[1, 2], &[10], [(1, 10), (2, 10)]);
+        test(&[1], &[10, 20], [(1, 10), (1, 20)]);
+        test(&[1, 2], &[10, 20], [(1, 10), (1, 20), (2, 10), (2, 20)]);
     }
 }
