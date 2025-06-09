34b5e61547ffc0a9940f77dfe448ffd9f8605dd6
Che-Liang Chiou <clchiou@gmail.com>
2025-06-09 18:00:32 +0800

    Rewrite bt_base::Features

--- rust/bt/base/src/lib.rs
+++ rust/bt/base/src/lib.rs
@@ -19,3 +19,10 @@ pub use crate::layout::{BlockRange, Layout, PieceIndex};
 pub use crate::magnet_uri::MagnetUri;
 pub use crate::node_id::{NodeDistance, NodeId};
 pub use crate::peer_id::PeerId;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Features {
+    pub dht: bool,       // BEP 5
+    pub fast: bool,      // BEP 6
+    pub extension: bool, // BEP 10
+}
