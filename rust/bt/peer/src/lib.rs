d96c95ada1822de397cbf92e593c1b0546174735
Che-Liang Chiou <clchiou@gmail.com>
2025-08-14 17:23:17 +0800

    Add skeleton bt_peer

---
+++ rust/bt/peer/src/lib.rs
@@ -0,0 +1,62 @@
+use std::collections::{HashMap, HashSet};
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+use g1_base::iter::IteratorExt;
+use g1_base::sync::MutexExt;
+
+use bt_base::InfoHash;
+
+#[derive(Clone, Debug)]
+pub struct Peers(Arc<Mutex<PeersInner>>);
+
+#[derive(Debug)]
+struct PeersInner {
+    peers: HashMap<InfoHash, HashSet<PeerInfo>>,
+}
+
+pub type PeerInfo = SocketAddr;
+
+impl Default for Peers {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Peers {
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(PeersInner::new())))
+    }
+
+    pub fn get_peers(&self, info_hash: InfoHash) -> Option<Vec<PeerInfo>> {
+        self.0.must_lock().get_peers(info_hash)
+    }
+
+    pub fn insert_peers<I>(&self, info_hash: InfoHash, peers: I)
+    where
+        I: IntoIterator<Item = PeerInfo>,
+    {
+        self.0.must_lock().insert_peers(info_hash, peers);
+    }
+}
+
+impl PeersInner {
+    fn new() -> Self {
+        Self {
+            peers: HashMap::new(),
+        }
+    }
+
+    fn get_peers(&self, info_hash: InfoHash) -> Option<Vec<PeerInfo>> {
+        self.peers
+            .get(&info_hash)
+            .map(|peers| peers.iter().copied().collect_then_sort())
+    }
+
+    fn insert_peers<I>(&mut self, info_hash: InfoHash, peers: I)
+    where
+        I: IntoIterator<Item = PeerInfo>,
+    {
+        self.peers.entry(info_hash).or_default().extend(peers);
+    }
+}
