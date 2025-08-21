a40e3dc67d74d07077b980b73333fb114195e39b
Che-Liang Chiou <clchiou@gmail.com>
2025-08-21 15:08:44 +0800

    Rewrite g1_url::UrlExt::update_query

--- rust/g1/url/src/lib.rs
+++ rust/g1/url/src/lib.rs
@@ -1,6 +1,9 @@
-use std::borrow::Cow;
+use std::borrow::{Borrow, Cow};
 use std::collections::BTreeMap;
+use std::collections::btree_map;
+use std::mem;
 
+use form_urlencoded::Serializer;
 use url::Url;
 
 pub trait UrlExt {
@@ -12,12 +15,12 @@ pub trait UrlExt {
         T: ParseQuery;
 
     // We do not preserve either the order or duplicated URL query parameters.
-    fn update_query(self, vars: &[&dyn ToQuery]) -> Self;
+    fn update_query(self, updates: &[&dyn UpdateQuery]) -> Self;
 }
 
 //
-// NOTE: Implementers of `ParseQuery` and `ToQuery` must maintain backward compatibility, as query
-// parameters are generally part of the public interface.
+// NOTE: Implementers of `ParseQuery` and `UpdateQuery` must maintain backward compatibility, as
+// query parameters are generally part of the public interface.
 //
 
 // NOTE: I did a few experiments to make an implementer `T` borrow from `query_pairs`, but they
@@ -33,11 +36,31 @@ pub trait ParseQuery: Sized {
         I: Iterator<Item = (Cow<'a, str>, Cow<'a, str>)>;
 }
 
-pub trait ToQuery {
-    // For now, we return a `Vec` because it is much easier than returning an iterator trait
-    // object.
-    fn to_query(&self) -> Vec<(&str, Option<Cow<'_, str>>)>;
+pub trait UpdateQuery {
+    // Its signature is a bit unusual in order to keep the trait `dyn`-compatible.
+    fn update_query<'a: 'b, 'b>(&'a self, builder: &mut QueryBuilder<'b>);
+
+    // NOTE: It skips raw query pairs.
+    fn query_pairs(&self) -> QueryPairs
+    where
+        Self: Sized,
+    {
+        let mut builder = QueryBuilder::new();
+        self.update_query(&mut builder);
+        builder.pairs.into_iter()
+    }
 }
+
+// Use `BTreeMap` because it is nice to have a deterministic output.
+#[derive(Debug)]
+pub struct QueryBuilder<'a> {
+    pairs: BTreeMap<Cow<'a, str>, Cow<'a, str>>,
+    // It is weird that we split query parameters into two independent groups, but given that
+    // `pairs` should be the norm and `raw_pairs` the exception, it is probably fine.
+    raw_pairs: BTreeMap<Cow<'a, str>, Cow<'a, str>>,
+}
+
+pub type QueryPairs<'a> = btree_map::IntoIter<Cow<'a, str>, Cow<'a, str>>;
 
 impl UrlExt for Url {
     fn ensure_trailing_slash(mut self) -> Self {
@@ -55,24 +78,90 @@ impl UrlExt for Url {
         T::parse_query(self.query_pairs())
     }
 
-    fn update_query(mut self, vars: &[&dyn ToQuery]) -> Self {
-        // It is nice to have a deterministic output.
-        let mut query: BTreeMap<_, _> = self.query_pairs().into_owned().collect();
-        for var in vars {
-            for (name, value) in var.to_query() {
-                match value {
-                    Some(value) => query.insert(name.into(), value.into()),
-                    None => query.remove(name),
-                };
-            }
+    fn update_query(mut self, updates: &[&dyn UpdateQuery]) -> Self {
+        let mut builder = QueryBuilder::from_url(&self);
+        for update in updates {
+            update.update_query(&mut builder);
         }
-        if query.is_empty() {
+        let QueryBuilder { pairs, raw_pairs } = builder;
+
+        if pairs.is_empty() && raw_pairs.is_empty() {
             self.set_query(None);
-        } else {
-            let mut ps = self.query_pairs_mut();
-            ps.clear();
-            ps.extend_pairs(query);
+            return self;
         }
+
+        let mut serializer = Serializer::new(String::new());
+        for (key, value) in pairs {
+            serializer.append_pair(&key, &value);
+        }
+
+        let mut serializer = serializer.finish();
+        let mut first = serializer.is_empty();
+        for (raw_key, raw_value) in raw_pairs {
+            if !mem::take(&mut first) {
+                serializer.push('&');
+            }
+            serializer.push_str(&raw_key);
+            serializer.push('=');
+            serializer.push_str(&raw_value);
+        }
+
+        self.set_query(Some(&serializer));
+        self
+    }
+}
+
+impl<'a> QueryBuilder<'a> {
+    fn from_url(url: &'a Url) -> Self {
+        Self {
+            pairs: url.query_pairs().collect(),
+            raw_pairs: BTreeMap::new(),
+        }
+    }
+
+    fn new() -> Self {
+        Self {
+            pairs: BTreeMap::new(),
+            raw_pairs: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert<K, V>(&mut self, key: K, value: V) -> &mut Self
+    where
+        Cow<'a, str>: From<K>,
+        Cow<'a, str>: From<V>,
+    {
+        self.pairs.insert(key.into(), value.into());
+        self
+    }
+
+    pub fn remove<Q>(&mut self, key: &Q) -> &mut Self
+    where
+        Cow<'a, str>: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.pairs.remove(key);
+        self
+    }
+
+    /// Inserts a raw query parameter.
+    ///
+    /// NOTE: The caller has to properly percent-encode both the key and the value.
+    pub fn insert_raw<K, V>(&mut self, key: K, value: V) -> &mut Self
+    where
+        Cow<'a, str>: From<K>,
+        Cow<'a, str>: From<V>,
+    {
+        self.raw_pairs.insert(key.into(), value.into());
+        self
+    }
+
+    pub fn remove_raw<Q>(&mut self, key: &Q) -> &mut Self
+    where
+        Cow<'a, str>: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.raw_pairs.remove(key);
         self
     }
 }
@@ -93,8 +182,8 @@ mod tests {
         {
             let mut x = None;
             let mut y = None;
-            for (name, value) in query_pairs {
-                match &*name {
+            for (key, value) in query_pairs {
+                match &*key {
                     "x" => x = Some(value),
                     "y" => y = Some(value),
                     _ => {}
@@ -108,12 +197,12 @@ mod tests {
         }
     }
 
-    impl ToQuery for Option<Param> {
-        fn to_query(&self) -> Vec<(&str, Option<Cow<'_, str>>)> {
+    impl UpdateQuery for Option<Param> {
+        fn update_query<'a: 'b, 'b>(&'a self, builder: &mut QueryBuilder<'b>) {
             match self {
-                Some(this) => vec![("x", Some((&this.0).into())), ("y", Some((&this.1).into()))],
-                None => vec![("x", None), ("y", None)],
-            }
+                Some(this) => builder.insert("x", &this.0).insert("y", &this.1),
+                None => builder.remove("x").remove("y"),
+            };
         }
     }
 
@@ -222,6 +311,46 @@ mod tests {
             u("http://127.0.0.1/a/b/c?a=3")
                 .update_query(&[&Some(p("1", "2")), &Some(p("foo", "bar"))]),
             u("http://127.0.0.1/a/b/c?a=3&x=foo&y=bar"),
+        );
+    }
+
+    #[test]
+    fn insert_raw() {
+        struct Query(bool, bool);
+
+        impl UpdateQuery for Query {
+            fn update_query<'a: 'b, 'b>(&'a self, builder: &mut QueryBuilder<'b>) {
+                if self.0 {
+                    builder.insert("spam egg", "Hello, World!");
+                }
+                if self.1 {
+                    builder
+                        .insert_raw("spam%20egg", "Hello%2C%20World%21")
+                        .insert_raw("foo", "bar");
+                }
+            }
+        }
+
+        assert_eq!(
+            u("http://127.0.0.1/").update_query(&[&Query(false, false)]),
+            u("http://127.0.0.1/"),
+        );
+
+        assert_eq!(
+            u("http://127.0.0.1/").update_query(&[&Query(true, false)]),
+            u("http://127.0.0.1/?spam+egg=Hello%2C+World%21"),
+        );
+
+        assert_eq!(
+            u("http://127.0.0.1/").update_query(&[&Query(false, true)]),
+            u("http://127.0.0.1/?foo=bar&spam%20egg=Hello%2C%20World%21"),
+        );
+
+        assert_eq!(
+            u("http://127.0.0.1/").update_query(&[&Query(true, true)]),
+            u(
+                "http://127.0.0.1/?spam+egg=Hello%2C+World%21&foo=bar&spam%20egg=Hello%2C%20World%21"
+            ),
         );
     }
 }
